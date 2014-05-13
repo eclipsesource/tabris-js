def3b4cf93618300bcf44df14b13f0286eb22755
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-05-13 23:59:58 +0200

    Add example app for tabris UI

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 
 Sources and tests for *tabris.js*
 
+Copy tabris.js to tabris-android
+
     cp src/js/tabris.js ../tabris-android/tabris-android/tabris-android/src/main/res/raw/tabris.js
+
+Copy example app to tabris-android
+
+    rm -f ../tabris-android/tabris-android/tabris-android-launcher/src/main/assets/js
+    cp -a examples/js/tabris-ui ../tabris-android/tabris-android/tabris-android-launcher/src/main/assets/js
---
+++ examples/js/tabris-ui/tabris-ui-draft.js
@@ -0,0 +1,20 @@
+/*global Tabris */
+
+var createPage = function( name, bgColor, topLevel ) {
+  var page = Tabris.create( "tabris.Page", {
+    title: name,
+    topLevel: topLevel
+  });
+  page.parent.set( "background", bgColor );
+  var label = page.parent.append( "rwt.widgets.Label", {
+    bounds: [20, 85, 185, 35],
+    text: name
+  });
+  return page;
+}
+
+var page1 = createPage( "Page One", [255, 200, 200], true );
+var page2 = createPage( "Page Two", [200, 255, 200], true );
+var page3 = createPage( "Page Three", [200, 200, 255], true );
+
+Tabris.UI.set( "activePage", page1.id );
