6be9a14d16a03de9a5afafbda1271735691357d6
Christian Petrov <Christian.Petrov@outlook.com>
2014-07-03 20:01:45 +0200

    Add gallery and keyboard demo
    Change-Id: I80891e73c2569ded50b63deb42ec571efd256bc9

---
+++ examples/js/gallery/gallery.js
@@ -0,0 +1,114 @@
+Tabris.load(function() {
+
+  var imageHolder;
+
+  var imageNames = [
+    "catseye",
+    "heic0305a",
+    "heic0401a",
+    "heic0407a",
+    "heic0407b",
+    "heic0409a",
+    "heic0414a",
+    "heic0502a",
+    "heic0514a",
+    "heic0515a",
+    "heic0604a",
+    "heic0910e",
+    "IRS46_nasa",
+    "ngc4414",
+    "opo0110a",
+    "opo0505a",
+    "opo9901a",
+    "orion-nebula"
+  ];
+
+  var page = Tabris.createPage({
+    title: "The Big Bang Theory",
+    topLevel: true
+  });
+
+  var mainComposite = page.append("Composite", {
+    background: [0, 0, 0],
+    layoutData: {left: 0, top: 0, right: 0, bottom: 0}
+  });
+
+  var scrolledCompositeLayoutData = {left: 0, right: 0, bottom: 0, height: 164};
+
+  var scrolledComposite = mainComposite.append("ScrolledComposite", {
+    style: ["H_SCROLL"],
+    layoutData: scrolledCompositeLayoutData,
+    background: [32, 32, 32]
+  });
+
+  scrolledComposite.append("rwt.widgets.ScrollBar", {
+    style: ["HORIZONTAL"]
+  });
+
+  var composite = scrolledComposite.append("Composite", {
+    // only works with bounds
+    bounds: [0, 0, 2929, 150]
+  });
+
+  function createImageThumbPath(imageName) {
+    return "images/".concat(imageName).concat("_thumb.jpg");
+  }
+
+  function createImageBigPath(imageName) {
+    return "images/".concat(imageName).concat(".jpg");
+  }
+
+  function bindShowToSelection(image, path) {
+    image.on("Selection", function() {
+      updateCentralImage(path);
+    });
+  }
+
+  for (var i = 0; i < imageNames.length; i++) {
+    var imageThumbPath = createImageThumbPath(imageNames[i]);
+    var imageBigPath = createImageBigPath(imageNames[i]);
+    var image = composite.append("Label", {
+      layoutData: {top: 7, left: i * 150 + i * 7, width: 150, height: 150},
+      image: [imageThumbPath, 150, 150],
+      data: {
+        showTouch: true
+      }
+    });
+    bindShowToSelection(image, imageBigPath);
+  }
+
+  function updateCentralImage(path) {
+    if (imageHolder) {
+      imageHolder.dispose();
+    }
+    imageHolder = mainComposite.append("Label", {
+      layoutData: {top: 0, bottom: 0, left: 0, right: 0},
+      image: [path, 0, 0]
+    });
+  }
+
+  var toggleAction = function() {
+    if (!scrolledComposite.isHidden) {
+      recreateThumbnailAction("Thumbnails");
+      scrolledComposite.set("layoutData", {height: 0});
+      scrolledComposite.isHidden = true;
+    } else {
+      recreateThumbnailAction("Fullscreen");
+      scrolledComposite.set("layoutData", scrolledCompositeLayoutData);
+      scrolledComposite.isHidden = false;
+    }
+  };
+
+  var recreateThumbnailAction = function(actionTitle) {
+    action.dispose();
+    action = Tabris.createAction({
+      title: actionTitle
+    }, toggleAction);
+  };
+
+  var action = Tabris.createAction({
+    title: "Fullscreen"
+  }, toggleAction);
+
+  page.open();
+});
---
+++ examples/js/keyboard/keyboard.js
@@ -0,0 +1,173 @@
+Tabris.load(function() {
+
+  var page = Tabris.createPage({
+    title: "Keyboard Types",
+    topLevel: true
+  });
+
+  var scrolledComposite = page.append("ScrolledComposite", {
+    style: ["V_SCROLL"],
+    layoutData: {left: 0, right: 0, top: 0, bottom: 0}
+  });
+
+  scrolledComposite.append("ScrollBar", {
+    style: ["VERTICAL"]
+  });
+
+  var composite = scrolledComposite.append("Composite", {});
+
+  var capitalizeOnLabel = composite.append("Label", {
+    layoutData: {left: 10, top: 10, width: 120},
+    alignment: "left",
+    text: "Capitalize On"
+  });
+
+  var capitalizeOnInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [capitalizeOnLabel, 10], right: 10, top: 10},
+    data: {
+      "autoCapitalize": true
+    }
+  });
+
+  var capitalizeOffLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [capitalizeOnInput, 10], width: 120},
+    alignment: "left",
+    text: "Capitalize Off"
+  });
+
+  var capitalizeOffInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [capitalizeOffLabel, 10], right: 10, top: [capitalizeOnInput, 10]},
+    data: {
+      "autoCapitalize": false
+    }
+  });
+
+  var autoCorrectOnLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [capitalizeOffInput, 10], width: 120},
+    alignment: "left",
+    text: "AutoCorrect On"
+  });
+
+  var autoCorrectOnInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [autoCorrectOnLabel, 10], right: 10, top: [capitalizeOffInput, 10]},
+    data: {
+      "autoCorrect": true
+    }
+  });
+
+  var autoCorrectOffLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [autoCorrectOnInput, 10], width: 120},
+    alignment: "left",
+    text: "AutoCorrect Off"
+  });
+
+  var autoCorrectOffInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [autoCorrectOffLabel, 10], right: 10, top: [autoCorrectOnInput, 10]},
+    data: {
+      "autoCorrect": false
+    }
+  });
+
+  var asciiLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [autoCorrectOffInput, 10], width: 120},
+    alignment: "left",
+    text: "ASCII"
+  });
+
+  var asciiInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [asciiLabel, 10], right: 10, top: [autoCorrectOffInput, 10]},
+    data: {
+      "keyboard": "ascii"
+    }
+  });
+
+  var decimalLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [asciiInput, 10], width: 120},
+    alignment: "left",
+    text: "Decimal"
+  });
+
+  var decimalInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [decimalLabel, 10], right: 10, top: [asciiInput, 10]},
+    data: {
+      "keyboard": "decimal"
+    }
+  });
+
+  var emailLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [decimalInput, 10], width: 120},
+    alignment: "left",
+    text: "E-Mail"
+  });
+
+  var emailInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [emailLabel, 10], right: 10, top: [decimalInput, 10]},
+    data: {
+      "keyboard": "email"
+    }
+  });
+
+  var numbersLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [emailInput, 10], width: 120},
+    alignment: "left",
+    text: "Numbers"
+  });
+
+  var numbersInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [numbersLabel, 10], right: 10, top: [emailInput, 10]},
+    data: {
+      "keyboard": "number"
+    }
+  });
+
+  var numbersPunctuationLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [numbersInput, 10], width: 120},
+    alignment: "left",
+    text: "Numbers\nPunctuation"
+  });
+
+  var numbersPunctuationInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [numbersPunctuationLabel, 10], right: 10, top: [numbersInput, 10]},
+    data: {
+      "keyboard": "numbersAndPunctuation"
+    }
+  });
+
+  var phoneLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [numbersPunctuationInput, 10], width: 120},
+    alignment: "left",
+    text: "Phone"
+  });
+
+  var phoneInput = composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [phoneLabel, 10], right: 10, top: [numbersPunctuationInput, 10]},
+    data: {
+      "keyboard": "phone"
+    }
+  });
+
+  var urlLabel = composite.append("Label", {
+    layoutData: {left: 10, top: [phoneInput, 10], width: 120},
+    alignment: "left",
+    text: "URL"
+  });
+
+  composite.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: [urlLabel, 10], right: 10, top: [phoneInput, 10]},
+    keyboard: "url",
+  });
+
+  page.open();
+
+});
