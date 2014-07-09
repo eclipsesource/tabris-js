dc03204fabdcdf2af74a0f591c3182dc5854df39
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-07-09 11:07:46 +0200

    Add demo for geo location
    Change-Id: I2261111abeebb51aadc02da2b69d5632250ddead

---
+++ examples/js/geo-location/geo.js
@@ -0,0 +1,61 @@
+Tabris.load(function() {
+
+  var page = Tabris.createPage({
+    title: "Geo Location Example",
+    topLevel: true
+  });
+
+  var location = null;
+
+  var geo = new Tabris.Proxy( "tabris.Geolocation" );
+  geo.on( "LocationUpdate", function( event ) {
+    location = event;
+    showLocation();
+  });
+
+  var launcher = Tabris.create( "tabris.AppLauncher" );
+
+  function showLocation() {
+    locationLabel.set( "text", locationToString( location ) );
+    mapButton.set( "enabled", location !== null );
+  }
+
+  function locationToString( location ) {
+    if( location === null ) {
+      return "Location unknown";
+    }
+    return "Your location: " + location.latitude.toFixed( 3 ) + ", " + location.longitude.toFixed( 3 );
+  }
+
+  var locationLabel = page.append( "Label", {
+    layoutData: { left: 20, right: 20, top: 20, height: 80 }
+  });
+
+  var locationButton = page.append( "Button", {
+    text: "Where am I?",
+    style: [ "PUSH" ],
+    layoutData: { left: 10, right: 10, top: [locationLabel, 10] }
+  }).on( "Selection", function() {
+    geo.set({
+      "needsPosition": "ONCE",
+      "frequency": 10000,
+      "maximumAge": -1,
+      "highAccuracy": true
+    });
+  });
+
+  var mapButton = page.append( "Button", {
+    text: "Show on map",
+    layoutData: { left: 10, right: 10, top: [locationButton, 10] }
+  }).on( "Selection", function() {
+    launcher.call( "open", {
+      "app": "MAPS",
+      "latitude": location.latitude.toString(),
+      "longitude": location.longitude.toString()
+    });
+  });
+
+  showLocation();
+  page.open();
+
+});
