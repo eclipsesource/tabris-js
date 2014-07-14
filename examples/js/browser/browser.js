1f4820ff59fba307dba11abfe4ce21ca580ceaa6
Moritz Post <mpost@eclipsesource.com>
2014-07-14 16:56:48 +0200

    Adds browser example app.
    Change-Id: Ib5eb4dc89e4e1e94102a5607940dde1e68639985

---
+++ examples/js/browser/browser.js
@@ -0,0 +1,31 @@
+tabris.load(function() {
+
+  var page = tabris.createPage({
+    title: "Browser",
+    topLevel: true
+  });
+
+  var goButton = page.append("Button", {
+    layoutData: {right: 8, top: 8, width: 64},
+    text: "Go"
+  });
+
+  var urlInput = page.append("Text", {
+    style: ["BORDER"],
+    layoutData: {left: 8, right: [goButton, 8], top: 8},
+    message: "URL",
+    text: "http://eclipsesource.com/"
+  });
+
+  var browser = page.append("rwt.widgets.Browser", {
+    layoutData: {left: 0, top: [goButton, 8], right: 0, bottom: 0},
+    url: "http://eclipsesource.com/"
+  });
+
+  goButton.on("Selection", function() {
+    browser.set("url", urlInput.get("text"));
+  });
+
+  page.open();
+
+});
