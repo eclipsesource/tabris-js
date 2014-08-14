6a3709788f5ed1bbf9f6f1c2b551bac61f3e8b31
Moritz Post <mpost@eclipsesource.com>
2014-08-14 12:02:31 +0200

    Add console log example
    Adds an example on how to use the console log.
    
    Change-Id: I06d5d1e2a1a50db79f6a2715e68338505447e122

---
+++ examples/js/console/console.js
@@ -0,0 +1,60 @@
+tabris.load(function() {
+
+  var MARGIN = 8;
+  var MARGIN_LARGE = 16;
+
+  var page = tabris.createPage({
+    title: "Console",
+    topLevel: true
+  });
+
+  var levelCombo = page.append("Combo", {
+    layoutData: { right: MARGIN, top: MARGIN_LARGE},
+    items: ["Error", "Warning", "Info", "Log", "Debug"],
+    selectionIndex: 2
+  });
+
+  var logText = page.append("Text", {
+    text: "Message",
+    message: "Log message",
+    layoutData: {left: MARGIN, top: MARGIN_LARGE, right: [levelCombo, MARGIN]}
+  });
+
+  var logButton = page.append("Button", {
+    text: "Log",
+    layoutData: {left: MARGIN, right: MARGIN, top: [levelCombo, MARGIN_LARGE]}
+  });
+
+  logButton.on("Selection", function() {
+    var text = logText.get("text");
+    var selection = levelCombo.get("selectionIndex");
+    if (selection === 0) {
+      console.error(text);
+    } else if (selection === 1) {
+      console.warn(text);
+    } else if (selection === 2) {
+      console.info(text);
+    } else if (selection === 3) {
+      console.log(text);
+    } else if (selection === 4) {
+      console.debug(text);
+    }
+  });
+
+  var undefinedVar;
+  var nullVar = null;
+  var func = function() {
+    console.log("hallo");
+  };
+
+  console.error("Error string", "This is not an error");
+  console.warn("Warn array", ["a", 1, false]);
+  console.info(func);
+  console.log("Log date", new Date());
+  console.log("Log object", logButton);
+  console.debug("Debug undefined", undefinedVar);
+  console.debug("Debug null", nullVar);
+
+  page.open();
+
+});
