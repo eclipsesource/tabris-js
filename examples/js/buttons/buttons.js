1e6e53f67de4c6a719b5be0d8d3a755932f1f848
David Weiss <dweiss93@live.com>
2014-08-18 16:11:57 +0200

    Implement a button demo (Tabris-js)
    The quiz is using different buttons, like Push Buttons, Radio Buttons
    and Checkboxes. However, poor creativity for MC-questions.
    
    Change-Id: I2c3b18da9cb992ff190ca8e011995b0d96abd371

---
+++ examples/js/buttons/buttons.js
@@ -0,0 +1,109 @@
+/* global questionsArray:false, shuffleArray:false */
+tabris.load(function() {
+  var createQuizPage = function(questionObject) {
+    function createAnswerButton(type, answer, refWidget) {
+      return answerComposite.append(type, {
+        font: [["arial"], 25, true, false],
+        layoutData: { left: [40, 0], right: [10, 0], top: refWidget ? [refWidget, 20] : 0 },
+        text: answer
+        }).on("Selection", function() {
+          activateNextButton();
+          statusLabel.set("text", "");
+        });
+    }
+
+    function createAnswerButtons(type, questionObject) {
+      var answerButtons = [];
+      for (var i = 0; i < questionObject.answers.length; i++){
+        answerButtons.push(createAnswerButton(type, questionObject.answers[i], answerButtons[i-1]));
+      }
+      return answerButtons;
+    }
+
+    function activateNextButton() {
+      nextButton.set("enabled", true);
+    }
+
+    function correctAnswer(questionObject) {
+      for (var i = 0; i < questionObject.answers.length; i++) {
+        if (answerButtons[i].get("selection") != isInArray(questionObject.correctAnswers, i)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function isMultipleChoice(questionObject) {
+      return questionObject.correctAnswers.length > 1;
+    }
+
+    function isInArray(array, search) {
+      return array.indexOf(search) >= 0;
+    }
+
+    function questionsLeft() {
+      return currentQuestionIndex+1 < questionsArray.length;
+    }
+
+    var page = tabris.createPage({
+      title: "You don't know that?",
+      topLevel: true
+    });
+
+    var statusComposite = page.append("Composite", {
+        layoutData: { left: 0, right: 0, top: [80, 0], bottom: 0 }
+    });
+
+    var answerComposite = page.append("Composite", {
+        layoutData: { left: 0, right: 0, top: [50, 0], bottom: [statusComposite, 0] }
+    });
+
+    var questionComposite = page.append("Composite", {
+        layoutData: { left: 0, right: 0, top: 0, bottom: [answerComposite, 0] }
+    });
+
+    questionComposite.append("Label", {
+        layoutData: { left: 10, right: 10, top: 10 },
+        alignment: "center",
+        text: questionObject.question,
+        font: [["arial"], 25, true, false],
+        style: ["WRAP"]
+    });
+
+    var type = (isMultipleChoice(questionObject)) ? "CheckBox" : "RadioButton";
+
+    var answerButtons = createAnswerButtons(type, questionObject);
+
+    var statusLabel = statusComposite.append("Label", {
+        text: "",
+        font: [["arial"], 25, true, false],
+        layoutData: { left: 13, bottom: 13, width: 200, height: 50 }  // iOS needs this width/heigth
+    });
+
+    var nextButton = statusComposite.append("Button", {
+        text: "Next",
+        font: [["arial"], 25, true, false],
+        layoutData: { right: 10, bottom: 10, width: 150, heigth: 25 },
+        enabled: false
+    }).on("Selection", function() {
+        if (!correctAnswer(questionObject)) {
+            statusLabel.set("text", "Sorry, try again!");
+            nextButton.set("enabled", false);
+        } else if (!questionsLeft()) {
+            statusLabel.set("text", "Congratulations!");
+            nextButton.set("text", "Restart");
+            currentQuestionIndex = -1;
+            shuffleArray(questionsArray);
+        } else {
+            page.close();
+            page = createQuizPage(questionsArray[++currentQuestionIndex]);
+            page.open();
+        }
+    });
+    return page;
+  };
+
+  var currentQuestionIndex = 0;
+  shuffleArray(questionsArray);
+  createQuizPage(questionsArray[currentQuestionIndex]).open();
+});
---
+++ examples/js/buttons/questions.js
@@ -0,0 +1,41 @@
+/* exported questionsArray, shuffleArray */
+var questionsArray = [
+  {
+    "question" : "How many friends are there in Friends?",
+    "answers" : [ "Six", "Five", "6"],
+    "correctAnswers" : [0, 2]
+  },
+  {
+    "question" : "How many fingers does Homer Simpson have?",
+    "answers" : [ "Eight","Nine","Ten"],
+    "correctAnswers" : [0]
+  },
+  {
+    "question" : "What's Fran's last name on The Nanny?",
+    "answers" : [ "Cline", "Fine", "Zine" ],
+    "correctAnswers" : [1]
+  },
+  {
+    "question" : "Which of the following integers are multiples of both 2 and 3?",
+    "answers" : [ "21","18","32" ],
+    "correctAnswers" : [1]
+  },
+  {
+    "question" : "What TV star did 500,000 people show up to watch sing at the Berlin Wall?",
+    "answers" : [ "David Hasselhoff","Michael Jackson","William Shatner" ],
+    "correctAnswers" : [0]
+  }
+];
+
+function shuffleArray(array) {
+  var remainingElements = array.length,
+    randomIndexInRange,
+    temp;
+  while ( remainingElements > 0 ){
+    randomIndexInRange = Math.floor(Math.random() * remainingElements--);
+    temp = array[remainingElements];
+    array[remainingElements] = array[randomIndexInRange];
+    array[randomIndexInRange] = temp;
+  }
+  return array;
+}
