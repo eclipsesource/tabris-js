91e5859c06855e59cb6e8345f357995ccd1e6f17
Moritz Post <mpost@eclipsesource.com>
2014-08-29 15:37:25 +0200

    Add spaces in text canvas demo

--- examples/js/canvas/text.js
+++ examples/js/canvas/text.js
@@ -11,38 +11,38 @@ tabris.load(function() {
 
   var ctx = tabris.getContext(canvas, 500, 700);
 
-  ctx.strokeStyle="red";
-  ctx.moveTo(5,100);
-  ctx.lineTo(500,100);
+  ctx.strokeStyle = "red";
+  ctx.moveTo(5, 100);
+  ctx.lineTo(500, 100);
   ctx.stroke();
 
-  ctx.textBaseline="top";
-  ctx.fillText("Top",5,100);
-  ctx.textBaseline="bottom";
-  ctx.fillText("Bottom",50,100);
-  ctx.textBaseline="middle";
-  ctx.fillText("Middle",120,100);
-  ctx.textBaseline="alphabetic";
-  ctx.fillText("Alphabetic",190,100);
-  ctx.textBaseline="ideographic";
-  ctx.fillText("Ideographic",390,100);
-  ctx.textBaseline="hanging";
-  ctx.fillText("Hanging",290,100);
+  ctx.textBaseline = "top";
+  ctx.fillText("Top", 5, 100);
+  ctx.textBaseline = "bottom";
+  ctx.fillText("Bottom", 50, 100);
+  ctx.textBaseline = "middle";
+  ctx.fillText("Middle", 120, 100);
+  ctx.textBaseline = "alphabetic";
+  ctx.fillText("Alphabetic", 190, 100);
+  ctx.textBaseline = "ideographic";
+  ctx.fillText("Ideographic", 390, 100);
+  ctx.textBaseline = "hanging";
+  ctx.fillText("Hanging", 290, 100);
 
-  ctx.strokeStyle="red";
-  ctx.moveTo(150,120);
-  ctx.lineTo(150,270);
+  ctx.strokeStyle = "red";
+  ctx.moveTo(150, 120);
+  ctx.lineTo(150, 270);
   ctx.stroke();
 
-  ctx.textAlign="start";
-  ctx.fillText("start",150,160);
-  ctx.textAlign="end";
-  ctx.fillText("end",150,180);
-  ctx.textAlign="left";
-  ctx.fillText("left",150,200);
-  ctx.textAlign="center";
-  ctx.fillText("center",150,220);
-  ctx.textAlign="right";
-  ctx.fillText("right",150,240);
+  ctx.textAlign = "start";
+  ctx.fillText("start", 150, 160);
+  ctx.textAlign = "end";
+  ctx.fillText("end", 150, 180);
+  ctx.textAlign = "left";
+  ctx.fillText("left", 150, 200);
+  ctx.textAlign = "center";
+  ctx.fillText("center", 150, 220);
+  ctx.textAlign = "right";
+  ctx.fillText("right", 150, 240);
 
 });
