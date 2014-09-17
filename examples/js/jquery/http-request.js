8625b9c955eba1c098dbeeaa16722d2572d839fd
Christian Petrov <cpetrov@eclipsesource.com>
2014-09-17 17:23:03 +0200

    Add an example for jQuery Ajax
    Change-Id: Iaf35d3091ec4e350dbcd856b625f00dc08189619

---
+++ examples/js/jquery/http-request.js
@@ -0,0 +1,39 @@
+/*global $:true */
+// jQuery built with "grunt custom:-ajax/script,-ajax/jsonp,-css,-deprecated,-dimensions,-effects,-event,-event/alias,-offset,-wrap,-ready,-deferred,-exports/amd,-sizzle"
+// https://github.com/jquery/jquery#how-to-build-your-own-jquery
+tabris.load(function() {
+
+  var MARGIN = 12;
+  var lastLabel;
+
+  var page = tabris.createPage({
+    title: "XMLHttpRequest",
+    topLevel: true
+  });
+
+  var createLabel = function(labelText) {
+    lastLabel = page.append("Label", {
+      style: ["WRAP"],
+      text: labelText,
+      markupEnabled: true,
+      layoutData: {left: MARGIN, right: MARGIN, top: [lastLabel, MARGIN]}
+    });
+  };
+
+  $.getJSON("http://www.telize.com/geoip", function(json) {
+    createLabel("The IP address is: " + json.ip);
+    createLabel("Latitude: " + json.latitude);
+    createLabel("Longitude: " + json.longitude);
+  });
+
+  $.getJSON("https://data.itpir.wm.edu/deflate/api.php?val=100USD1986USA&json=true", function (json) {
+    createLabel("Value of 1986 100$ today: " + json.deflated_amount + "$");
+  });
+
+  $.getJSON("http://api.automeme.net/text.json", function (json) {
+    createLabel("Meme: " + json[0]);
+  });
+
+  page.open();
+
+});
