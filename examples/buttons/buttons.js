c23694e8839e468f00c75ca37107aebf078c544c
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-10 15:33:45 +0200

    Integrate tabris.js syntax changes in buttons demo
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Remove second quiz page as it is superfluous.
    
    Change-Id: I8e1de55c77d3fbeafd15145a6c4b78571f823a6d

--- examples/buttons/buttons.js
+++ examples/buttons/buttons.js
@@ -2,10 +2,90 @@
 
 tabris.load(function() {
 
-  var createQuizPage = function(questionObject) {
+  var currentQuestionIndex = 0;
+  shuffleArray(questions);
+  createQuizPage(questions[currentQuestionIndex]).open();
+
+  function createQuizPage(question) {
+
+    var nextButton, statusLabel, answerButtons;
+
+    var page = tabris.create("Page", {
+      title: "You don't know that?",
+      topLevel: true
+    });
+    var statusComposite = createStatusComposite({
+      layoutData: {left: 0, right: 0, top: [80, 0], bottom: 0}
+    });
+    var answerComposite = createAnswerComposite({
+      layoutData: {left: 0, right: 0, top: [50, 0], bottom: [statusComposite, 0]}
+    });
+    var questionComposite = createQuestionComposite(question, {
+      layoutData: {left: 0, right: 0, top: 0, bottom: [answerComposite, 0]}
+    });
+    return page.append(statusComposite, answerComposite, questionComposite);
+
+    function createStatusComposite(properties) {
+      var statusComposite = tabris.create("Composite", properties);
+      statusLabel = tabris.create("Label", {
+        text: "",
+        font: "25px sans-serif",
+        layoutData: {left: 13, bottom: 13, width: 200, height: 35} // iOS needs this width/heigth
+      });
+      nextButton = tabris.create("Button", {
+        text: "Next",
+        font: "25px sans-serif",
+        layoutData: {right: 10, bottom: 10, width: 150, heigth: 25},
+        enabled: false
+      }).on("selection", function() {
+        if (!correctAnswer()) {
+          statusLabel.set("text", "Sorry, try again!");
+          nextButton.set("enabled", false);
+        } else if (!questionsLeft()) {
+          statusLabel.set("text", "Congratulations!");
+          nextButton.set("text", "Restart");
+          currentQuestionIndex = -1;
+          shuffleArray(questions);
+        } else {
+          page.close();
+          page = createQuizPage(questions[++currentQuestionIndex]);
+          page.open();
+        }
+      });
+      statusComposite.append(statusLabel, nextButton);
+      return statusComposite;
+    }
+
+    function createQuestionComposite(properties) {
+      return tabris.create("Composite", properties).append(tabris.create("Label", {
+        layoutData: {left: 10, right: 10, top: 10},
+        alignment: "center",
+        text: question.text,
+        font: "25px sans-serif",
+        style: ["WRAP"]
+      }));
+    }
+
+    function createAnswerComposite(properties) {
+      var composite = tabris.create("Composite", properties);
+      createAnswerButtons().forEach(function(button) {
+        composite.append(button);
+      });
+      return composite;
+    }
+
+    function createAnswerButtons() {
+      var type = (isMultipleChoice(question)) ? "CheckBox" : "RadioButton";
+      answerButtons = [];
+      for (var i = 0; i < question.answers.length; i++) {
+        var button = createAnswerButton(type, question.answers[i], answerButtons[i - 1]);
+        answerButtons.push(button);
+      }
+      return answerButtons;
+    }
 
     function createAnswerButton(type, answer, refWidget) {
-      return answerComposite.append(type, {
+      return tabris.create(type, {
         font: "25px sans-serif",
         layoutData: {left: [40, 0], right: [10, 0], top: refWidget ? [refWidget, 20] : 0},
         text: answer
@@ -15,106 +95,35 @@ tabris.load(function() {
       });
     }
 
-    function createAnswerButtons(type, questionObject) {
-      var answerButtons = [];
-      for (var i = 0; i < questionObject.answers.length; i++) {
-        answerButtons.push(createAnswerButton(type, questionObject.answers[i],
-                                              answerButtons[i - 1]));
-      }
-      return answerButtons;
-    }
-
     function activateNextButton() {
       nextButton.set("enabled", true);
     }
 
-    function correctAnswer(questionObject) {
-      for (var i = 0; i < questionObject.answers.length; i++) {
-        if (answerButtons[i].get("selection") !== isInArray(questionObject.correctAnswers, i)) {
+    function correctAnswer() {
+      for (var i = 0; i < question.answers.length; i++) {
+        if (answerButtons[i].get("selection") !== isInArray(question.correctAnswers, i)) {
           return false;
         }
       }
       return true;
     }
 
-    function isMultipleChoice(questionObject) {
-      return questionObject.correctAnswers.length > 1;
-    }
+  }
 
-    function isInArray(array, search) {
-      return array.indexOf(search) >= 0;
-    }
+  function questionsLeft() {
+    return currentQuestionIndex + 1 < questions.length;
+  }
 
-    function questionsLeft() {
-      return currentQuestionIndex + 1 < questions.length;
-    }
+  function isMultipleChoice(question) {
+    return question.correctAnswers.length > 1;
+  }
 
-    var page = tabris.createPage({
-                                   title: "You don't know that?",
-                                   topLevel: true
-                                 });
-
-    var statusComposite = page.append("Composite", {
-      layoutData: {left: 0, right: 0, top: [80, 0], bottom: 0}
-    });
-
-    var answerComposite = page.append("Composite", {
-      layoutData: {left: 0, right: 0, top: [50, 0], bottom: [statusComposite, 0]}
-    });
-
-    var questionComposite = page.append("Composite", {
-      layoutData: {left: 0, right: 0, top: 0, bottom: [answerComposite, 0]}
-    });
-
-    questionComposite.append("Label", {
-      layoutData: {left: 10, right: 10, top: 10},
-      alignment: "center",
-      text: questionObject.question,
-      font: "25px sans-serif",
-      style: ["WRAP"]
-    });
-
-    var type = (isMultipleChoice(questionObject)) ? "CheckBox" : "RadioButton";
-
-    var answerButtons = createAnswerButtons(type, questionObject);
-
-    var statusLabel = statusComposite.append("Label", {
-      text: "",
-      font: "25px sans-serif",
-      layoutData: {left: 13, bottom: 13, width: 200, height: 35}  // iOS needs this width/heigth
-    });
-
-    var nextButton = statusComposite.append("Button", {
-      text: "Next",
-      font: "25px sans-serif",
-      layoutData: {right: 10, bottom: 10, width: 150, heigth: 25},
-      enabled: false
-    }).on("selection", function() {
-      if (!correctAnswer(questionObject)) {
-        statusLabel.set("text", "Sorry, try again!");
-        nextButton.set("enabled", false);
-      } else if (!questionsLeft()) {
-        statusLabel.set("text", "Congratulations!");
-        nextButton.set("text", "Restart");
-        currentQuestionIndex = -1;
-        shuffleArray(questions);
-      } else {
-        page.close();
-        page = createQuizPage(questions[++currentQuestionIndex]);
-        page.open();
-      }
-    });
-    return page;
-  };
-
-  var currentQuestionIndex = 0;
-  shuffleArray(questions);
-  createQuizPage(questions[currentQuestionIndex]).open();
+  function isInArray(array, search) {
+    return array.indexOf(search) >= 0;
+  }
 
   function shuffleArray(array) {
-    var remainingElements = array.length,
-        randomIndexInRange,
-        temp;
+    var remainingElements = array.length, randomIndexInRange, temp;
     while (remainingElements > 0) {
       randomIndexInRange = Math.floor(Math.random() * remainingElements--);
       temp = array[remainingElements];
@@ -123,8 +132,5 @@ tabris.load(function() {
     }
     return array;
   }
-
-  shuffleArray(questions);
-  createQuizPage(questions[currentQuestionIndex]).open();
 
 });
--- examples/buttons/questions.js
+++ examples/buttons/questions.js
@@ -2,27 +2,27 @@
 
 var questions = [
   {
-    "question" : "How many friends are there in Friends?",
+    "text" : "How many friends are there in Friends?",
     "answers" : [ "Six", "Five", "6"],
     "correctAnswers" : [0, 2]
   },
   {
-    "question" : "How many fingers does Homer Simpson have?",
+    "text" : "How many fingers does Homer Simpson have?",
     "answers" : [ "Eight","Nine","Ten"],
     "correctAnswers" : [0]
   },
   {
-    "question" : "What's Fran's last name on The Nanny?",
+    "text" : "What's Fran's last name on The Nanny?",
     "answers" : [ "Cline", "Fine", "Zine" ],
     "correctAnswers" : [1]
   },
   {
-    "question" : "Which of the following integers are multiples of both 2 and 3?",
+    "text" : "Which of the following integers are multiples of both 2 and 3?",
     "answers" : [ "21","18","32" ],
     "correctAnswers" : [1]
   },
   {
-    "question" : "What TV star did 500,000 people show up to watch sing at the Berlin Wall?",
+    "text" : "What TV star did 500,000 people show up to watch sing at the Berlin Wall?",
     "answers" : [ "David Hasselhoff","Michael Jackson","William Shatner" ],
     "correctAnswers" : [0]
   }
