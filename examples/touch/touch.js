da48edaff92fb17b60b42aba34d8b3624919ee40
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-10 18:34:57 +0200

    Integrate tabris.js syntax changes in touch demo
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Retrieve x and y coordinates from the touches array from the touch event as
    coordinates are not event properties.
    
    Change-Id: Ie2e444e50ac733b26d8cbe2e016e6ec87c2b94e2

--- examples/touch/touch.js
+++ examples/touch/touch.js
@@ -7,61 +7,67 @@ tabris.load(function() {
   };
 
   var printXY = function(label, prefix, event) {
-    label.set("text", prefix + ": " + event.x + " X " + event.y);
+    label.set("text", prefix + ": " + event.touches[0].x + " X " + event.touches[0].y);
   };
 
-  var blue = tabris.createPage({
+  var bluePage = tabris.create("Page", {
     title: "Touch Events",
     topLevel: true,
     background: colors.blue[0]
   });
 
-  var red = blue.append("Composite", {
+  var redComposite = tabris.create("Composite", {
     layoutData: {left: 30, right: 30, top: 60, bottom: [50, 0]},
     background: colors.red[0]
-  }).on("touchstart", function(event) {
-    printXY(redLabel, "touchstart", event);
-    red.set("background", colors.red[1]);
-  }).on("touchmove", function(event) {
-    printXY(redLabel, "touchmove", event);
-  }).on("touchend", function(event) {
-    printXY(redLabel, "touchend", event);
-    red.set("background", colors.red[0]);
-  }).on("longpress", function(event) {
-    printXY(redLabel, "longpress", event);
   });
 
-  var green = blue.append("Composite", {
+  var greenComposite = tabris.create("Composite", {
     layoutData: {left: 30, right: 30, top: [50, 30], bottom: 30},
     background: colors.green[0]
-  }).on("touchstart", function(event) {
-    printXY(greenLabel, "touchstart", event);
-    green.set("background", colors.green[1]);
-  }).on("touchmove", function(event) {
-    printXY(greenLabel, "touchmove", event);
-  }).on("touchend", function(event) {
-    printXY(greenLabel, "touchend", event);
-    green.set("background", colors.green[0]);
-  }).on("longpress", function(event) {
-    printXY(greenLabel, "longpress", event);
   });
 
-  blue.on("touchstart", function(event) {
+  var blueLabel = tabris.create("Label", {font: "20px sans-serif"});
+  var redLabel = tabris.create("Label", {font: "20px sans-serif"});
+  var greenLabel = tabris.create("Label", {font: "20px sans-serif"});
+
+  redComposite.append(redLabel);
+  greenComposite.append(greenLabel);
+  bluePage.append(redComposite, greenComposite, blueLabel);
+
+  bluePage.on("touchstart", function(event) {
     printXY(blueLabel, "touchstart", event);
-    blue.set("background", colors.blue[1]);
+    bluePage.set("background", colors.blue[1]);
   }).on("touchmove", function(event) {
     printXY(blueLabel, "touchmove", event);
   }).on("touchend", function(event) {
     printXY(blueLabel, "touchend", event);
-    blue.set("background", colors.blue[0]);
+    bluePage.set("background", colors.blue[0]);
   }).on("longpress", function(event) {
     printXY(blueLabel, "longpress", event);
   });
+  redComposite.on("touchstart", function(event) {
+    printXY(redLabel, "touchstart", event);
+    redComposite.set("background", colors.red[1]);
+  }).on("touchmove", function(event) {
+    printXY(redLabel, "touchmove", event);
+  }).on("touchend", function(event) {
+    printXY(redLabel, "touchend", event);
+    redComposite.set("background", colors.red[0]);
+  }).on("longpress", function(event) {
+    printXY(redLabel, "longpress", event);
+  });
+  greenComposite.on("touchstart", function(event) {
+    printXY(greenLabel, "touchstart", event);
+    greenComposite.set("background", colors.green[1]);
+  }).on("touchmove", function(event) {
+    printXY(greenLabel, "touchmove", event);
+  }).on("touchend", function(event) {
+    printXY(greenLabel, "touchend", event);
+    greenComposite.set("background", colors.green[0]);
+  }).on("longpress", function(event) {
+    printXY(greenLabel, "longpress", event);
+  });
 
-  var blueLabel = blue.append("Label", {font: "20px sans-serif"});
-  var redLabel = red.append("Label", {font: "20px sans-serif"});
-  var greenLabel = green.append("Label", {font: "20px sans-serif"});
-
-  blue.open();
+  bluePage.open();
 
 });
