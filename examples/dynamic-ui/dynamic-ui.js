235d67f606097a47b198cb1ee22aabdd461238ec
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-10 19:43:34 +0200

    Integrate tabris.js syntax changes in dynamic ui
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    
    Change-Id: Ia92e6009858601f75a1d2cb881c1186b38ea789d

--- examples/dynamic-ui/dynamic-ui.js
+++ examples/dynamic-ui/dynamic-ui.js
@@ -10,48 +10,42 @@ tabris.load(function() {
 
   var createPage = function(pageTitle, pageIsTopLevel) {
 
-    var page = tabris.createPage({
+    var page = tabris.create("Page", {
       title: pageTitle,
       topLevel: pageIsTopLevel
     });
 
-    var addRootPageButton = page.append("Button", {
+    var addRootPageButton = tabris.create("Button", {
       layoutData: {left: 5, right: 5, top: 5},
       text: "Add Root Page",
       background: "green",
       foreground: "white",
       image: ["images/add_root_page.png", 24, 24]
-    });
-
-    addRootPageButton.on("Selection", function() {
+    }).on("Selection", function() {
       var page = createPage("Root Page: " + createRandomPageId(), true);
       topLevelPages.push(page);
       page.open();
     });
 
-    var addPageButton = page.append("Button", {
+    var addPageButton = tabris.create("Button", {
       layoutData: {left: 5, right: 5, top: [addRootPageButton, 5]},
       text: "Add Page",
       background: "green",
       foreground: "white",
       image: ["images/add_page.png", 24, 24]
-    });
-
-    addPageButton.on("Selection", function() {
+    }).on("Selection", function() {
       var page = createPage("Page: " + createRandomPageId(), false);
       pages.push(page);
       page.open();
     });
 
-    var addGlobalActionButton = page.append("Button", {
+    var addGlobalActionButton = tabris.create("Button", {
       layoutData: {left: 5, right: 5, top: [addPageButton, 5]},
       text: "Add Global Action",
       background: "green",
       foreground: "white",
       image: ["images/global_action.png", 24, 24]
-    });
-
-    addGlobalActionButton.on("Selection", function() {
+    }).on("Selection", function() {
       var action = tabris.createAction({
         title: "Share",
         image: ["images/action_share.png", 24, 24]
@@ -62,38 +56,37 @@ tabris.load(function() {
     });
 
     // TODO: add page actions to demo when implemented. See tabris-js issue #8.
-
-    var removeLastGlobalActionButton = page.append("Button", {
+    var removeLastGlobalActionButton = tabris.create("Button", {
       layoutData: {left: 5, right: 5, bottom: 5},
       text: "Remove Last Global Action",
       background: "red",
       foreground: "white",
       image: ["images/global_action.png", 24, 24]
-    });
-
-    removeLastGlobalActionButton.on("Selection", function() {
+    }).on("Selection", function() {
       if (actions.length - 1 >= 0) {
         actions[actions.length - 1].dispose();
         actions.pop();
       }
     });
 
-    var removeLastRootPageButton = page.append("Button", {
+    var removeLastRootPageButton = tabris.create("Button", {
       layoutData: {left: 5, right: 5, bottom: [removeLastGlobalActionButton, 5]},
       text: "Remove Last Root Page",
       background: "red",
       foreground: "white",
       image: ["images/remove_root_page.png", 24, 24]
-    });
-
-    removeLastRootPageButton.on("Selection", function() {
+    }).on("Selection", function() {
       if (topLevelPages.length - 1 >= 0) {
         topLevelPages[topLevelPages.length - 1].close();
         topLevelPages.pop();
       }
     });
 
+    page.append(addRootPageButton, addPageButton, addGlobalActionButton, removeLastGlobalActionButton,
+      removeLastRootPageButton);
+
     return page;
+
   };
 
   var topLevelPage = createPage("Dynamic UI Start", true);
