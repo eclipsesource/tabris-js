e009f4a4474c93c458449068be71f120035e3983
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-13 23:54:38 +0200

    Integrate layout changes in browser demo
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Format the demo.
    
    Change-Id: Ied5b1d1762c42d84f1f77993d458d129aebb182e

--- examples/browser/browser.js
+++ examples/browser/browser.js
@@ -1,25 +1,27 @@
 tabris.load(function() {
 
-  var page = tabris.createPage({
+  var page = tabris.create("Page", {
     title: "Browser",
     topLevel: true
   });
 
-  var goButton = page.append("Button", {
+  var goButton = tabris.create("Button", {
     layoutData: {right: 8, top: 8, width: 64},
     text: "Go"
   });
 
-  var urlInput = page.append("Text", {
+  var urlInput = tabris.create("Text", {
     layoutData: {left: 8, right: [goButton, 8], top: 8},
     message: "URL",
     text: "http://eclipsesource.com/"
   });
 
-  var browser = page.append("rwt.widgets.Browser", {
+  var browser = tabris.create("rwt.widgets.Browser", {
     layoutData: {left: 0, top: [goButton, 8], right: 0, bottom: 0},
     url: "http://eclipsesource.com/"
   });
+
+  page.append(goButton, urlInput, browser);
 
   goButton.on("Selection", function() {
     browser.set("url", urlInput.get("text"));
