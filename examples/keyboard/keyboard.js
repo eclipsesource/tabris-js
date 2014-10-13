cdb70a14ec6232f7a72b8e557b2e436fe4069ee7
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-13 23:26:04 +0200

    Integrate layout changes in keyboard demo
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Remove object key quotes.
    
    Change-Id: Ib0276268866861c3b281204836095c1061332e78

--- examples/keyboard/keyboard.js
+++ examples/keyboard/keyboard.js
@@ -1,155 +1,161 @@
 tabris.load(function() {
 
-  var page = tabris.createPage({
+  var page = tabris.create("Page", {
     title: "Keyboard Types",
     topLevel: true
   });
 
-  var composite = page.append("ScrollComposite", {
+  var composite = tabris.create("ScrollComposite", {
     scroll: "vertical",
     layoutData: {left: 0, right: 0, top: 0, bottom: 0}
   });
 
-  var capitalizeOnLabel = composite.append("Label", {
+  var capitalizeOnLabel = tabris.create("Label", {
     layoutData: {left: 10, top: 10, width: 120},
     alignment: "left",
     text: "Capitalize On"
   });
 
-  var capitalizeOnInput = composite.append("Text", {
+  var capitalizeOnInput = tabris.create("Text", {
     layoutData: {left: [capitalizeOnLabel, 10], right: 10, top: 10},
     data: {
-      "autoCapitalize": true
+      autoCapitalize: true
     }
   });
 
-  var capitalizeOffLabel = composite.append("Label", {
+  var capitalizeOffLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [capitalizeOnInput, 10], width: 120},
     alignment: "left",
     text: "Capitalize Off"
   });
 
-  var capitalizeOffInput = composite.append("Text", {
+  var capitalizeOffInput = tabris.create("Text", {
     layoutData: {left: [capitalizeOffLabel, 10], right: 10, top: [capitalizeOnInput, 10]},
     data: {
-      "autoCapitalize": false
+      autoCapitalize: false
     }
   });
 
-  var autoCorrectOnLabel = composite.append("Label", {
+  var autoCorrectOnLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [capitalizeOffInput, 10], width: 120},
     alignment: "left",
     text: "AutoCorrect On"
   });
 
-  var autoCorrectOnInput = composite.append("Text", {
+  var autoCorrectOnInput = tabris.create("Text", {
     layoutData: {left: [autoCorrectOnLabel, 10], right: 10, top: [capitalizeOffInput, 10]},
     data: {
-      "autoCorrect": true
+      autoCorrect: true
     }
   });
 
-  var autoCorrectOffLabel = composite.append("Label", {
+  var autoCorrectOffLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [autoCorrectOnInput, 10], width: 120},
     alignment: "left",
     text: "AutoCorrect Off"
   });
 
-  var autoCorrectOffInput = composite.append("Text", {
+  var autoCorrectOffInput = tabris.create("Text", {
     layoutData: {left: [autoCorrectOffLabel, 10], right: 10, top: [autoCorrectOnInput, 10]},
     data: {
-      "autoCorrect": false
+      autoCorrect: false
     }
   });
 
-  var asciiLabel = composite.append("Label", {
+  var asciiLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [autoCorrectOffInput, 10], width: 120},
     alignment: "left",
     text: "ASCII"
   });
 
-  var asciiInput = composite.append("Text", {
+  var asciiInput = tabris.create("Text", {
     layoutData: {left: [asciiLabel, 10], right: 10, top: [autoCorrectOffInput, 10]},
     data: {
-      "keyboard": "ascii"
+      keyboard: "ascii"
     }
   });
 
-  var decimalLabel = composite.append("Label", {
+  var decimalLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [asciiInput, 10], width: 120},
     alignment: "left",
     text: "Decimal"
   });
 
-  var decimalInput = composite.append("Text", {
+  var decimalInput = tabris.create("Text", {
     layoutData: {left: [decimalLabel, 10], right: 10, top: [asciiInput, 10]},
     data: {
-      "keyboard": "decimal"
+      keyboard: "decimal"
     }
   });
 
-  var emailLabel = composite.append("Label", {
+  var emailLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [decimalInput, 10], width: 120},
     alignment: "left",
     text: "E-Mail"
   });
 
-  var emailInput = composite.append("Text", {
+  var emailInput = tabris.create("Text", {
     layoutData: {left: [emailLabel, 10], right: 10, top: [decimalInput, 10]},
     data: {
-      "keyboard": "email"
+      keyboard: "email"
     }
   });
 
-  var numbersLabel = composite.append("Label", {
+  var numbersLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [emailInput, 10], width: 120},
     alignment: "left",
     text: "Numbers"
   });
 
-  var numbersInput = composite.append("Text", {
+  var numbersInput = tabris.create("Text", {
     layoutData: {left: [numbersLabel, 10], right: 10, top: [emailInput, 10]},
     data: {
-      "keyboard": "number"
+      keyboard: "number"
     }
   });
 
-  var numbersPunctuationLabel = composite.append("Label", {
+  var numbersPunctuationLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [numbersInput, 10], width: 120},
     alignment: "left",
     text: "Numbers\nPunctuation"
   });
 
-  var numbersPunctuationInput = composite.append("Text", {
+  var numbersPunctuationInput = tabris.create("Text", {
     layoutData: {left: [numbersPunctuationLabel, 10], right: 10, top: [numbersInput, 10]},
     data: {
-      "keyboard": "numbersAndPunctuation"
+      keyboard: "numbersAndPunctuation"
     }
   });
 
-  var phoneLabel = composite.append("Label", {
+  var phoneLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [numbersPunctuationInput, 10], width: 120},
     alignment: "left",
     text: "Phone"
   });
 
-  var phoneInput = composite.append("Text", {
+  var phoneInput = tabris.create("Text", {
     layoutData: {left: [phoneLabel, 10], right: 10, top: [numbersPunctuationInput, 10]},
     data: {
-      "keyboard": "phone"
+      keyboard: "phone"
     }
   });
 
-  var urlLabel = composite.append("Label", {
+  var urlLabel = tabris.create("Label", {
     layoutData: {left: 10, top: [phoneInput, 10], width: 120},
     alignment: "left",
     text: "URL"
   });
 
-  composite.append("Text", {
+  var urlInput = tabris.create("Text", {
     layoutData: {left: [urlLabel, 10], right: 10, top: [phoneInput, 10]},
     keyboard: "url"
   });
+
+  composite.append(capitalizeOnLabel, capitalizeOnInput, capitalizeOffLabel, capitalizeOffInput,
+    autoCorrectOnLabel, autoCorrectOnInput, autoCorrectOffLabel, autoCorrectOffInput, asciiLabel,
+    asciiInput, decimalLabel, decimalInput, emailLabel, emailInput, numbersLabel, numbersInput,
+    numbersPunctuationLabel, numbersPunctuationInput, phoneLabel, phoneInput, urlLabel, urlInput);
+  page.append(composite);
 
   page.open();
 
