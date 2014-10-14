f8bd163f258448291a0f76561f42e5e25fa58cff
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-14 20:10:29 +0200

    Integrate recent changes in TabrisBackbone
    * Make use of the new append syntax which allows appending of multiple
    widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Format the code.
    
    Change-Id: I6c39a8ac0a09856ac371c04d82c85264aa791b54

--- examples/backbone/TabrisBackbone.js
+++ examples/backbone/TabrisBackbone.js
@@ -18,7 +18,7 @@
 (function() {
 
   var View = Backbone.View = function(options) {
-    this.cid = _.uniqueId('view');
+    this.cid = _.uniqueId("view");
     options = options || {};
     _.extend(this, _.pick(options, viewOptions));
     this._ensureWidget(options);
@@ -31,26 +31,25 @@
 
   // List of view options to be merged as properties.
   var viewOptions = [
-    'model',
-    'collection',
-    'widget',
-    'parentWidget',
-    'parentView',
-    'attributes',
-    'widgetType',
-    'events'
+    "model",
+    "collection",
+    "widget",
+    "parentWidget",
+    "parentView",
+    "attributes",
+    "widgetType",
+    "events"
   ];
 
   // Set up all inheritable **Backbone.View** properties and methods.
   _.extend(View.prototype, Backbone.Events, {
 
-    // The default `type` of a View's widget is `"Composite"`.
-    widgetType: 'Composite',
+    // The default `type` of a View's widget is `Composite`.
+    widgetType: "Composite",
 
     // Initialize is an empty function by default. Override it with your own
     // initialization logic.
-    initialize: function() {
-    },
+    initialize: function() {},
 
     // **render** is the core function that your view should override, in order
     // to populate its widget (`this.widget`), with the appropriate content. The
@@ -62,7 +61,7 @@
     // Remove this view by taking the element out of the DOM, and removing any
     // applicable Backbone.Events listeners.
     remove: function() {
-      if(this.widget.close) {
+      if (this.widget.close) {
         this.widget.close();
       }
       this.widget.dispose();
@@ -73,16 +72,15 @@
     // Change the view's widget (`this.widget` property), including event
     // re-delegation.
     setWidget: function(widget, delegate) {
-      if(this.widget) {
+      if (this.widget) {
         this.undelegateEvents();
       }
       this.widget = widget;
-      if(delegate !== false) {
+      if (delegate !== false) {
         this.delegateEvents();
       }
       return this;
     },
-
 
     // NOTE [TABRIS]: could the selector be supported somehow? Comment unchanged:
     // Set callbacks, where `this.events` is a hash of
@@ -90,9 +88,9 @@
     // *{"event selector": "callback"}*
     //
     //     {
-    //       'mousedown .title':  'edit',
-    //       'click .button':     'save',
-    //       'click .open':       function(e) { ... }
+    //       "mousedown .title":  "edit",
+    //       "click .button":     "save",
+    //       "click .open":       function(e) { ... }
     //     }
     //
     // pairs. Callbacks will be bound to the view, with `this` set properly.
@@ -101,23 +99,23 @@
     // This only works for delegate-able events: not `focus`, `blur`, and
     // not `change`, `submit`, and `reset` in Internet Explorer.
     delegateEvents: function(events) {
-      if(!(events || (events = _.result(this, 'events')))) {
+      if (!(events || (events = _.result(this, "events")))) {
         return this;
       }
       this.undelegateEvents();
       var listener = [];
-      for(var key in events) {
+      for (var key in events) {
         var method = events[key];
-        if(!_.isFunction(method)) {
+        if (!_.isFunction(method)) {
           method = this[events[key]];
         }
-        if(!method) {
+        if (!method) {
           continue;
         }
         var match = key.match(delegateEventSplitter);
         var eventName = match[1];
-        var selector = match[2] || 'widget';
-        if(this[selector]) {
+        var selector = match[2] || "widget";
+        if (this[selector]) {
           method = _.bind(method, this);
           this[selector].on(eventName, method);
           listener.push([this[selector], eventName, method]);
@@ -129,11 +127,11 @@
 
     // removes all given listener. Overwritten on instance by delegateEvents
     undelegateEvents: function(arr) {
-      if(arr instanceof Array) {
-        for(var i = 0; i < arr.length; i++) {
-          try{
+      if (arr instanceof Array) {
+        for (var i = 0; i < arr.length; i++) {
+          try {
             arr[i][0].off(arr[i][1], arr[i][2]);
-          }catch(ex) {
+          } catch (ex) {
             // widget is probably disposed
           }
         }
@@ -145,23 +143,24 @@
     // Ensure that the View has a widget to render into.
     // Create a widget from the `widgetType`, `attributes` and `layoutData` properties.
     // If no parent is present, create and open a page;
-    _ensureWidget: function( options ) {
-      if(!this.widget) {
-        var attrs = _.extend({}, _.result(this, 'attributes'));
-        if(!this.parentWidget && this.parentView) {
+    _ensureWidget: function(options) {
+      if (!this.widget) {
+        var attrs = _.extend({}, _.result(this, "attributes"));
+        if (!this.parentWidget && this.parentView) {
           this.parentWidget = this.parentView.container || this.parentView.widget;
         }
-        if( this.parentWidget ) {
-          this.setWidget(this.parentWidget.append(_.result(this, 'widgetType'), attrs), false);
+        if (this.parentWidget) {
+          var widget = tabris.create(_.result(this, "widgetType"), attrs);
+          this.setWidget(this.parentWidget.append(widget), false);
         } else {
-          this.setWidget(tabris.createPage(attrs), false);
+          this.setWidget(tabris.create("Page", attrs), false);
           this.widget.open();
         }
       } else {
-        this.setWidget(_.result(this, 'widget'), false);
+        this.setWidget(_.result(this, "widget"), false);
       }
-      if(options.layoutData) {
-        this.widget.set('layoutData', options.layoutData);
+      if (options.layoutData) {
+        this.widget.set("layoutData", options.layoutData);
       }
     }
 
