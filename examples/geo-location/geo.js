0b6f3af559c1b36e40677e3c67f171f022ec0e4c
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-10-15 12:05:48 +0200

    Integrate recent changes in geolocation
    * Make use of the new append syntax which allows appending of multiple
      widgets.
    * Change obsolete "createPage" method invokation to "create".
    * Move layout-related code to the top.
    * Format the code.
    
    Change-Id: Ie93f9f345dba312964a35a6718135211113a2dbd

--- examples/geo-location/geo.js
+++ examples/geo-location/geo.js
@@ -1,58 +1,64 @@
 tabris.load(function() {
 
-  var page = tabris.createPage({
+  var page = tabris.create("Page", {
     title: "Geo Location Example",
     topLevel: true
   });
 
+  var locationLabel = tabris.create("Label", {
+    layoutData: {left: 20, right: 20, top: 20, height: 80}
+  });
+
+  var locationButton = tabris.create("Button", {
+    text: "Where am I?",
+    layoutData: {left: 10, right: 10, top: [locationLabel, 10]}
+  });
+
+  var mapButton = tabris.create("Button", {
+    text: "Show on map",
+    layoutData: {left: 10, right: 10, top: [locationButton, 10]}
+  });
+
+  page.append(locationLabel, locationButton, mapButton);
+
+  locationButton.on("selection", function() {
+    geo.set({
+      needsPosition: "ONCE",
+      frequency: 10000,
+      maximumAge: -1,
+      highAccuracy: true
+    });
+  });
+
+  mapButton.on("selection", function() {
+    launcher.call("open", {
+      app: "MAPS",
+      latitude: location.latitude.toString(),
+      longitude: location.longitude.toString()
+    });
+  });
+
   var location = null;
 
-  var geo = tabris( "tabris.Geolocation" );
-  geo.on( "LocationUpdate", function( event ) {
+  var geo = tabris("tabris.Geolocation");
+  geo.on("LocationUpdate", function(event) {
     location = event;
     showLocation();
   });
 
-  var launcher = tabris.create( "tabris.AppLauncher" );
+  var launcher = tabris.create("tabris.AppLauncher");
 
   function showLocation() {
-    locationLabel.set( "text", locationToString( location ) );
-    mapButton.set( "enabled", location !== null );
+    locationLabel.set("text", locationToString(location));
+    mapButton.set("enabled", location !== null);
   }
 
-  function locationToString( location ) {
-    if( location === null ) {
+  function locationToString(location) {
+    if (location === null) {
       return "Location unknown";
     }
-    return "Your location: " + location.latitude.toFixed( 3 ) + ", " + location.longitude.toFixed( 3 );
+    return "Your location: " + location.latitude.toFixed(3) + ", " + location.longitude.toFixed(3);
   }
-
-  var locationLabel = page.append( "Label", {
-    layoutData: { left: 20, right: 20, top: 20, height: 80 }
-  });
-
-  var locationButton = page.append( "Button", {
-    text: "Where am I?",
-    layoutData: { left: 10, right: 10, top: [locationLabel, 10] }
-  }).on( "selection", function() {
-    geo.set({
-      "needsPosition": "ONCE",
-      "frequency": 10000,
-      "maximumAge": -1,
-      "highAccuracy": true
-    });
-  });
-
-  var mapButton = page.append( "Button", {
-    text: "Show on map",
-    layoutData: { left: 10, right: 10, top: [locationButton, 10] }
-  }).on("selection", function() {
-    launcher.call( "open", {
-      "app": "MAPS",
-      "latitude": location.latitude.toString(),
-      "longitude": location.longitude.toString()
-    });
-  });
 
   showLocation();
   page.open();
