2ce4293b747de23e29d0801a089607ef4df18b97
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-22 17:28:00 +0200

    Improve the console demo
    Change the console demo to show an input for the message and buttons
    for every console type to make the UI easier to use. Remove the console
    log messages at startup as they are confusing.
    
    Change-Id: I3c01460f112161d06f9030a52dfabcfbf2e34405

--- examples/console/console.js
+++ examples/console/console.js
@@ -3,59 +3,44 @@ tabris.load(function() {
   var MARGIN = 8;
   var MARGIN_LARGE = 16;
 
+  var lastWidget;
+  var logText;
+
   var page = tabris.create("Page", {
     title: "Console",
     topLevel: true
   });
 
-  var levelCombo = tabris.create("Combo", {
-    layoutData: {right: MARGIN, top: MARGIN_LARGE},
-    items: ["Error", "Warning", "Info", "Log", "Debug"],
-    selectionIndex: 2
-  });
-
-  var logText = tabris.create("Text", {
-    text: "Message",
-    message: "Log message",
-    layoutData: {left: MARGIN, top: MARGIN_LARGE, right: [levelCombo, MARGIN]}
-  });
-
-  var logButton = tabris.create("Button", {
-    text: "Log",
-    layoutData: {left: MARGIN, right: MARGIN, top: [levelCombo, MARGIN_LARGE]}
-  });
-
-  page.append(levelCombo, logText, logButton);
-
-  logButton.on("selection", function() {
-    var text = logText.get("text");
-    var selection = levelCombo.get("selectionIndex");
-    if (selection === 0) {
-      console.error(text);
-    } else if (selection === 1) {
-      console.warn(text);
-    } else if (selection === 2) {
-      console.info(text);
-    } else if (selection === 3) {
-      console.log(text);
-    } else if (selection === 4) {
-      console.debug(text);
-    }
-  });
-
-  var undefinedVar;
-  var nullVar = null;
-  var func = function() {
-    console.log("hallo");
+  var consoleFunctions = {
+    error: console.error,
+    warn: console.warn,
+    info: console.info,
+    log: console.log,
+    debug: console.debug
   };
 
-  console.error("Error string", "This is not an error");
-  console.warn("Warn array", ["a", 1, false]);
-  console.info(func);
-  console.log("Log date", new Date());
-  console.log("Log object", logButton);
-  console.debug("Debug undefined", undefinedVar);
-  console.debug("Debug null", nullVar);
+  logText = lastWidget = tabris.create("Text", {
+    text: "Message",
+    message: "Log message",
+    layoutData: {left: MARGIN, top: MARGIN_LARGE, right: MARGIN}
+  }).appendTo(page);
+
+  var toUpperCase = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  var getConsoleHandler = function(type) {
+    return function() {
+      consoleFunctions[type](logText.get("text"));
+    };
+  };
+
+  for (var type in consoleFunctions) {
+    lastWidget = tabris.create("Button", {
+      text: toUpperCase(type),
+      layoutData: {left: MARGIN, right: MARGIN, top: [lastWidget, MARGIN_LARGE]}
+    }).on("selection", getConsoleHandler(type)).appendTo(page);
+  }
 
   page.open();
 
