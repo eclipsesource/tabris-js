1f0bb8365dfc9de0b87f06b25a157b5634a1dd01
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-10-23 09:49:31 +0200

    Code cleanup in timer demo
    - Use appendTo
    - Move functions to the bottom
    - Better var names
    
    Change-Id: I32c802e89023dd08ab17db720fc16d9b81bed229

--- examples/timer/timer.js
+++ examples/timer/timer.js
@@ -6,7 +6,6 @@ tabris.load(function() {
 
   var cpsCount = 0;
   var startTime = new Date().getTime();
-
   var taskId;
 
   var page = tabris.create("Page", {
@@ -14,66 +13,40 @@ tabris.load(function() {
     topLevel: true
   });
 
-  var labelStatus = tabris.create("Label", {
+  var statusLabel = tabris.create("Label", {
     text: "Last update: <none>",
     layoutData: {left: MARGIN, top: MARGIN_LARGE, right: MARGIN},
     font: LARGE_FONT
-  });
+  }).appendTo(page);
 
-  var labelCps = tabris.create("Label", {
+  var cpsLabel = tabris.create("Label", {
     text: "Calls per second: <none>",
-    layoutData: {left: MARGIN, top: [labelStatus, MARGIN], right: MARGIN},
+    layoutData: {left: MARGIN, top: [statusLabel, MARGIN], right: MARGIN},
     font: LARGE_FONT
-  });
+  }).appendTo(page);
 
-  var labelDelay = tabris.create("Label", {
+  var delayLabel = tabris.create("Label", {
     text: "Delay (ms)",
-    layoutData: {left: MARGIN, top: [labelCps, MARGIN_LARGE]}
-  });
+    layoutData: {left: MARGIN, top: [cpsLabel, MARGIN_LARGE]}
+  }).appendTo(page);
 
-  var textDelay = tabris.create("Text", {
+  var delayText = tabris.create("Text", {
     text: "1000",
     message: "Delay (ms)",
-    layoutData: {left: [labelDelay, MARGIN], top: [labelCps, MARGIN_LARGE]}
-  });
+    layoutData: {left: [delayLabel, MARGIN], top: [cpsLabel, MARGIN_LARGE]}
+  }).appendTo(page);
 
-  var checkRepeat = tabris.create("CheckBox", {
+  var repeatCheckbox = tabris.create("CheckBox", {
     text: "Repeat",
-    layoutData: {left: MARGIN, top: [textDelay, MARGIN]}
-  });
+    layoutData: {left: MARGIN, top: [delayText, MARGIN]}
+  }).appendTo(page);
 
-  var buttonStart = tabris.create("Button", {
+  var startButton = tabris.create("Button", {
     text: "Start timer",
-    layoutData: {left: [50, MARGIN / 4], top: [checkRepeat, MARGIN_LARGE], right: MARGIN}
-  });
-
-  var buttonCancelTimer = tabris.create("Button", {
-    text: "Cancel timer",
-    layoutData: {left: MARGIN, top: [checkRepeat, MARGIN_LARGE], right: [50, MARGIN / 4]}
-  });
-
-  page.append(labelStatus, labelCps, labelDelay, textDelay, checkRepeat, buttonStart, buttonCancelTimer);
-
-  var updateStatusLabels = function() {
-    cpsCount++;
-    var curTime = new Date().getTime();
-    var diff = curTime - startTime;
-    if (diff >= 1000) {
-      labelCps.set("text", "Calls per second: " + cpsCount);
-      cpsCount = 0;
-      startTime = curTime;
-    }
-    labelStatus.set("text", "Last update: " + new Date().getTime().toString());
-  };
-
-  function enableTimerStart(available) {
-    buttonStart.set("enabled", available);
-    buttonCancelTimer.set("enabled", !available);
-  }
-
-  buttonStart.on("selection", function() {
-    var delay = parseInt(textDelay.get("text"));
-    if (checkRepeat.get("selection")) {
+    layoutData: {left: [50, MARGIN / 4], top: [repeatCheckbox, MARGIN_LARGE], right: MARGIN}
+  }).on("selection", function() {
+    var delay = parseInt(delayText.get("text"));
+    if (repeatCheckbox.get("selection")) {
       taskId = setInterval(updateStatusLabels, delay);
     } else {
       taskId = setTimeout(function() {
@@ -82,15 +55,35 @@ tabris.load(function() {
       }, delay);
     }
     enableTimerStart(false);
-  });
+  }).appendTo(page);
 
-  buttonCancelTimer.on("selection", function() {
+  var cancelButton = tabris.create("Button", {
+    text: "Cancel timer",
+    layoutData: {left: MARGIN, top: [repeatCheckbox, MARGIN_LARGE], right: [50, MARGIN / 4]}
+  }).on("selection", function() {
     clearTimeout(taskId);
     enableTimerStart(true);
-  });
+  }).appendTo(page);
 
   enableTimerStart(true);
 
   page.open();
+
+  function updateStatusLabels() {
+    cpsCount++;
+    var curTime = new Date().getTime();
+    var diff = curTime - startTime;
+    if (diff >= 1000) {
+      cpsLabel.set("text", "Calls per second: " + cpsCount);
+      cpsCount = 0;
+      startTime = curTime;
+    }
+    statusLabel.set("text", "Last update: " + new Date().getTime().toString());
+  }
+
+  function enableTimerStart(available) {
+    startButton.set("enabled", available);
+    cancelButton.set("enabled", !available);
+  }
 
 });
