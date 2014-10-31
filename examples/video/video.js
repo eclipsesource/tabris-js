86f09933217bf3f2fb8694263f6cea1e9f8ab860
Christian Petrov <cpetrov@eclipsesource.com>
2014-10-31 18:34:04 +0100

    Refactor video demo and fix some issues
    * Remove workaround for tabris issue #14 as it is already solved.
    * Use appendTo to append widgets.
    * Update checkbox selection event name.
    * Make video widget bottom relative to the controls composite.
    * Change button enabled state depending on the current video playback state.
    
    See tabris-android issues #659 and #660 and tabris-ios issue #559.
    
    Change-Id: I50a4c2761c3774935c9f96def16740f9f6cbe889

--- examples/video/video.js
+++ examples/video/video.js
@@ -9,98 +9,106 @@ tabris.load(function() {
     topLevel: true
   });
 
+  var controls = tabris.create("Composite", {
+    layoutData: {left: 0, right: 0, bottom: 0, height: 90}
+  }).appendTo(page);
+
   var video = tabris.create("Video", {
     enabled: true,
     controls_visible: true,
     repeat: false,
     playback: "play",
-    // TODO: workaround for tabris-ios bug #451
-    layoutData: {left: 0, right: 0, top: 0, height: 500},
+    layoutData: {left: 0, right: 0, top: 0, bottom: [controls, 0]},
     url: "http://mirrorblender.top-ix.org/movies/sintel-1024-surround.mp4"
-  });
-
-  var controls = tabris.create("Composite", {
-    layoutData: {left: 0, right: 0, bottom: 0, height: 90}
-  });
+  }).appendTo(page);
 
   var playButton = tabris.create("Button", {
     layoutData: {top: 5, left: 5, width: 60},
     text: "play"
-  });
-
-  playButton.on("selection", function() {
+  }).on("selection", function() {
     video.set("playback", "play");
-  });
+  }).appendTo(controls);
 
   var pauseButton = tabris.create("Button", {
     layoutData: {top: 5, left: [playButton, 5], width: 70},
-    text: "pause"
-  });
-
-  pauseButton.on("selection", function() {
+    text: "pause",
+    enabled: false
+  }).on("selection", function() {
     video.set("playback", "pause");
-  });
+  }).appendTo(controls);
 
   var stopButton = tabris.create("Button", {
     layoutData: {top: 5, left: [pauseButton, 5], width: 60},
-    text: "stop"
-  });
-
-  stopButton.on("selection", function() {
+    text: "stop",
+    enabled: false
+  }).on("selection", function() {
     video.set("playback", "stop");
-  });
+  }).appendTo(controls);
 
   var backwardButton = tabris.create("Button", {
     layoutData: {top: 5, left: [stopButton, 5], width: 100},
     text: "backward"
-  });
-
-  backwardButton.on("selection", function() {
+  }).on("selection", function() {
     video.set("playback", "fast_backward");
-  });
+  }).appendTo(controls);
 
   var forwardButton = tabris.create("Button", {
     layoutData: {top: 5, left: [backwardButton, 5], width: 90},
     text: "forward"
-  });
-
-  forwardButton.on("selection", function() {
+  }).on("selection", function() {
     video.set("playback", "fast_forward");
-  });
+  }).appendTo(controls);
 
-  var fullscreenButton = tabris.create("Button", {
+  tabris.create("Button", {
     layoutData: {top: 5, left: [forwardButton, 5], width: 100},
     text: "fullscreen"
-  });
-
-  fullscreenButton.on("selection", function() {
+  }).on("selection", function() {
     video.set("presentation", "full_screen");
-  });
+  }).appendTo(controls);
 
   var controlsCheckbox = tabris.create("CheckBox", {
     layoutData: {bottom: 5, left: 5, width: 100},
     text: "Controls",
     selection: true
-  });
+  }).on("change:selection", function() {
+    video.set("controls_visible", this.get("selection"));
+  }).appendTo(controls);
 
-  controlsCheckbox.on("selection", function() {
-    // TODO: workaround for tabris-js bug #14
-    video.set("controls_visible", !video.get("controls_visible"));
-  });
-
-  var repeatCheckbox = tabris.create("CheckBox", {
+  tabris.create("CheckBox", {
     layoutData: {bottom: 5, left: [controlsCheckbox, 5], width: 100},
     text: "Repeat"
-  });
+  }).on("change:selection", function() {
+    video.set("repeat", this.get("selection"));
+  }).appendTo(controls);
 
-  repeatCheckbox.on("selection", function() {
-    // TODO: workaround for tabris-js bug #14
-    video.set("repeat", !video.get("repeat"));
-  });
+  var setButtonsEnabledState = function(buttons, state) {
+    buttons.forEach(function(button) {
+      button.set("enabled", state);
+    });
+  };
 
-  controls.append(playButton, pauseButton, stopButton, backwardButton, forwardButton,
-    fullscreenButton, controlsCheckbox, repeatCheckbox);
-  page.append(video, controls);
+  video.on("Playback", function() {
+    // TODO: toLowerCase workaround for tabris-android issue #659
+    switch (this.get("playback").toLowerCase()) {
+      case "play":
+        playButton.set("enabled", false);
+        setButtonsEnabledState([pauseButton, stopButton, forwardButton], true);
+        break;
+      case "pause":
+      case "stop":
+        setButtonsEnabledState([playButton, forwardButton, backwardButton], true);
+        setButtonsEnabledState([pauseButton, stopButton], false);
+        break;
+      case "fast_forward":
+        setButtonsEnabledState([playButton, pauseButton, stopButton, backwardButton], true);
+        forwardButton.set("enabled", false);
+        break;
+      case "fast_backward":
+        setButtonsEnabledState([playButton, pauseButton, stopButton, forwardButton], true);
+        backwardButton.set("enabled", false);
+        break;
+    }
+  });
 
   page.open();
 
