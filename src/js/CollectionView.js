eclipsesource/tabris-js

commit 89b35d0a76ef3a4b3bb39a5d65d4ebbf36ffacf5

Implement checks for children support
Throw an error when a widget is appended to a parent that does not
support children, or only supports children of another type. These are
severe problems in the application and we should not try to continue.

Support a new field "_supportsChildren" in registerType. This field
defaults to false, container widgets need to set it to true. It can
also be set to a function to check children selectively
(e.g. TabFolder only supports childs of type Tab).

Fix #129

Change-Id: Iaefb973a7a4ce39d6c2c09826e34fe86b4eaf642

src/js/TabFolder.js
@@ -19,6 +19,10 @@
       selection: true
     },
 
+    _supportsChildren: function(child) {
+      return child.type === "Tab" || child.type === "_TabItem";
+    },
+
     _listen: {"change:selection": "Selection"},
     _trigger: {Selection: "change:selection"},
 

src/js/Page.js
@@ -16,6 +16,8 @@
 
     _type: "rwt.widgets.Composite",
 
+    _supportsChildren: true,
+
     _checkProperty: {image: true, title: true, topLevel: true},
 
     _create: function(properties) {

src/js/Proxy.js
@@ -146,6 +146,13 @@
     },
 
     _addChild: function(child) {
+      var check = this.constructor && this.constructor._supportsChildren;
+      if (check === false) {
+        throw new Error(this.type + " cannot contain children");
+      }
+      if (typeof check === "function" && !check(child)) {
+        throw new Error(this.type + " cannot contain children of type " + child.type);
+      }
       if (!this._children) {
         this._children = [];
       }

src/js/ScrollComposite.js
@@ -15,6 +15,8 @@
 
     _type: "rwt.widgets.ScrolledComposite",
 
+    _supportsChildren: true,
+
     _listen: {
       scroll: function(listen) {
         if (listen) {

src/js/Tabris.js
@@ -39,10 +39,10 @@
       tabris[type] = function() {
         tabris.Proxy.apply(this, arguments);
       };
-      staticMembers.forEach(function(member) {
-        tabris[type][member] = members[member] || (member === "_type" ? null : {});
-      });
-      var superProto = util.omit(members, staticMembers);
+      for (var member in staticMembers) {
+        tabris[type][member] = members[member] || getDefault(member);
+      }
+      var superProto = util.omit(members, Object.keys(staticMembers));
       superProto.type = type;
       superProto.constructor = tabris[type]; // util.extendPrototype can not provide the original
       tabris[type].prototype = util.extendPrototype(tabris.Proxy, superProto);
@@ -72,8 +72,20 @@
 
   });
 
-  var staticMembers = [
-    "_trigger", "_listen", "_setProperty", "_getProperty", "_properties", "_type", "_checkProperty"
-  ];
+  function getDefault(member) {
+    var value = staticMembers[member];
+    return value instanceof Object ? util.clone(value) : value;
+  }
+
+  var staticMembers = {
+    "_trigger": {},
+    "_listen": {},
+    "_setProperty": {},
+    "_getProperty": {},
+    "_properties": {},
+    "_type": null,
+    "_checkProperty": {},
+    "_supportsChildren": false
+  };
 
 })();

src/js/Widgets.js
@@ -49,7 +49,8 @@
   });
 
   tabris.registerWidget("Canvas", {
-    _type: "rwt.widgets.Canvas"
+    _type: "rwt.widgets.Canvas",
+    _supportsChildren: true
   });
 
   tabris.registerWidget("CheckBox", {
@@ -68,7 +69,8 @@
   });
 
   tabris.registerWidget("Composite", {
-    _type: "rwt.widgets.Composite"
+    _type: "rwt.widgets.Composite",
+    _supportsChildren: true
   });
 
   tabris.registerWidget("ImageView", {

test/js/Proxy.spec.js
@@ -15,7 +15,9 @@ describe("Proxy", function() {
     log = [];
     tabris._reset();
     tabris._start(nativeBridge);
-    tabris.registerType("TestType", {});
+    tabris.registerType("TestType", {
+      _supportsChildren: true
+    });
   });
 
   afterEach(function() {
@@ -175,6 +177,34 @@ describe("Proxy", function() {
         expect(function() {
           proxy.append({});
         }).toThrowError("Cannot append non-widget");
+      });
+
+    });
+
+    describe("append when children are not supported", function() {
+
+      it("throws an error", function() {
+        tabris.TestType._supportsChildren = false;
+        var child = tabris.create("Label", {});
+
+        expect(function() {
+          proxy.append(child);
+        }).toThrowError("TestType cannot contain children");
+        expect(proxy.children()).not.toContain(child);
+      });
+
+    });
+
+    describe("append children of unsupported type", function() {
+
+      it("logs an error", function() {
+        tabris.TestType._supportsChildren = function() { return false; };
+        var child = tabris.create("Label", {});
+
+        expect(function() {
+          proxy.append(child);
+        }).toThrowError("TestType cannot contain children of type Label");
+        expect(proxy.children()).not.toContain(child);
       });
 
     });

commit ffbcd45f99bdc4f9c65fbfc9c7a852588ff09711

Add missing _supportsChildren entries
CollectionView and TabFolder were broken by commit 89b35d0a76ef3.

Change-Id: Ia9823f8ad107715329076cc45c08420dc5c0969c

src/js/CollectionView.js
@@ -104,6 +104,8 @@
 
     _listen: {itemchange: true},
 
+    _supportsChildren: true,
+
     dispose: function() {
       console.warn("CollectionView cells are container-managed, they cannot be disposed of");
     }

src/js/TabFolder.js
@@ -64,6 +64,8 @@
       badge: true
     },
 
+    _supportsChildren: true,
+
     _create: function(properties) {
       this._itemProps = {};
       return this.super("_create", properties);