eclipsesource/tabris-js

commit 77771d6315fe2e06ca99c5ade13dea999ccfd78f

Refactor Page.js, take advantage of new tabris.registerWidget features
Change-Id: I2e337cd3bb15a331dbdc2c4fea351b66bd07de01

src/js/Page.js
@@ -3,78 +3,50 @@
  * All rights reserved.
  */
 
-tabris.registerWidget("Page", {
+(function() {
 
-  _type: "tabris.Page",
+  var pageProperties = ["title", "image", "style", "topLevel"];
 
-  _PAGE_PROPS: ["title", "image", "style", "topLevel"],
+  tabris.registerType("_Page", {
+    _type: "tabris.Page"
+  });
 
-  _create: function(properties) {
-    var compositeProperties = util.extend(util.omit(properties, this._PAGE_PROPS), {
-      parent: tabris._shell,
-      layoutData: {left: 0, right: 0, top: 0, bottom: 0}
-    });
-    this._composite = tabris.create("Composite", compositeProperties);
-    var pageProperties = util.extend(util.pick(properties, this._PAGE_PROPS), {
-      parent: tabris._uiProxy._ui,
-      control: this._composite
-    });
-    this.super("_create", pageProperties);
-    return this;
-  },
+  tabris.registerWidget("Page", {
 
-  get: function(property) {
-    if (this._PAGE_PROPS.indexOf(property) !== -1) {
-      return this.super("get", property);
+    _type: "rwt.widgets.Composite",
+
+    _create: function(properties) {
+      this.super("_create",  util.extend(util.omit(properties, pageProperties), {
+        parent: tabris._shell,
+        layoutData: {left: 0, right: 0, top: 0, bottom: 0}
+      }));
+      this._page = tabris.create("_Page", util.extend(util.pick(properties, pageProperties), {
+        parent: tabris._uiProxy._ui,
+        control: this
+      }));
+      return this;
+    },
+
+    dispose: function() {
+      this.super("dispose");
+      this._page.dispose();
+    },
+
+    open: function() {
+      tabris._uiProxy.setActivePage(this);
+    },
+
+    close: function() {
+      this.dispose();
+      tabris._uiProxy.setLastActivePage();
+      this._page.dispose();
     }
-    return this._composite.get(property);
-  },
 
-  set: function(arg1, arg2) {
-    var properties;
-    if (typeof arg1 === "string") {
-      properties = {};
-      properties[arg1] = arg2;
-    } else {
-      properties = arg1;
-    }
-    this.super("set", util.pick(properties, this._PAGE_PROPS));
-    this._composite.set(util.omit(properties, this._PAGE_PROPS));
-    return this;
-  },
+  });
 
-  call: function(method, parameters) {
-    this._composite.call(method, parameters);
-    return this;
-  },
+  pageProperties.forEach(function(property) {
+    tabris.Page._setProperty[property] = function(value) {this._page.set(property, value);};
+    tabris.Page._getProperty[property] = function() {return this._page.get(property);};
+  });
 
-  on: function(event, listener) {
-    this._composite.on(event, listener);
-    return this;
-  },
-
-  off: function(event, listener) {
-    this._composite.off(event, listener);
-    return this;
-  },
-
-  dispose: function() {
-    this._composite.dispose();
-    this.super("dispose");
-  },
-
-  open: function() {
-    tabris._uiProxy.setActivePage(this);
-  },
-
-  close: function() {
-    this._composite.dispose();
-    tabris._uiProxy.setLastActivePage();
-    this.dispose();
-  },
-
-  _getContainer: function() {
-    return this._composite;
-  }
-
-});
+}());

src/js/UIProxy.js
@@ -52,7 +52,7 @@ tabris.UIProxy.prototype = {
 
   setActivePage: function(page) {
     this._pages.push(page);
-    this._ui.set("activePage", page);
+    this._ui.set("activePage", page._page);
   },
 
   getActivePage: function() {
@@ -63,7 +63,7 @@ tabris.UIProxy.prototype = {
     this._pages.pop();
     var page = this.getActivePage();
     if (page) {
-      this._ui.set("activePage", page);
+      this._ui.set("activePage", page._page);
     }
   }
 

test/js/Page.spec.js
@@ -7,7 +7,7 @@ describe("Page", function() {
 
   var nativeBridge;
   var page;
-  var uiId = "uiId";
+  var uiId;
 
   beforeEach(function() {
     nativeBridge = new NativeBridgeSpy();
@@ -15,23 +15,30 @@ describe("Page", function() {
     tabris._start(nativeBridge);
     tabris._shell = new tabris.Proxy("shellId");
     tabris._uiProxy = jasmine.createSpyObj("uiProxy", ["setActivePage", "setLastActivePage"]);
-    tabris._uiProxy._ui = new tabris.Proxy(uiId);
-    page = new tabris.Page();
+    tabris._uiProxy._ui = tabris.create("_UI", {
+      shell: tabris._shell
+    });
+    uiId = tabris._uiProxy._ui.id;
+    nativeBridge.resetCalls();
   });
 
   afterEach(function() {
     delete tabris._uiProxy;
   });
 
-  it("is instance of Proxy", function() {
-    expect(page).toEqual(jasmine.any(tabris.Proxy));
-  });
-
   describe("create", function() {
 
-    it("creates a Composite and a Page", function() {
-      page._create({});
+    beforeEach(function() {
+      page = tabris.create("Page", {
+        title: "title",
+        image: {src: "image"},
+        style: "fullscreen",
+        topLevel: true,
+        background: "red"
+      });
+    });
 
+    it("creates a Composite and a Page", function() {
       var createCalls = nativeBridge.calls({op: "create"});
       expect(createCalls.length).toBe(2);
       expect(createCalls[0].type).toBe("rwt.widgets.Composite");
@@ -40,74 +47,60 @@ describe("Page", function() {
 
     describe("created Composite", function() {
 
-      var createCall;
+      var properties;
 
       beforeEach(function() {
-        page._create({
-          title: "title",
-          image: "image",
-          style: "fullscreen",
-          topLevel: true,
-          background: "red"
-        });
-        createCall = nativeBridge.calls({op: "create", type: "rwt.widgets.Composite"})[0];
+        var createCall = nativeBridge.calls({op: "create", type: "rwt.widgets.Composite"})[0];
+        properties = createCall.properties;
       });
 
       it("parent is shell", function() {
-        expect(createCall.properties.parent).toEqual(tabris._shell.id);
+        expect(properties.parent).toEqual(tabris._shell.id);
       });
 
       it("is full-size", function() {
-        expect(createCall.properties.layoutData).toEqual({left: 0, right: 0, top: 0, bottom: 0});
+        expect(properties.layoutData).toEqual({left: 0, right: 0, top: 0, bottom: 0});
       });
 
       it("does not inherit page properties", function() {
-        expect(createCall.properties.title).not.toBeDefined();
-        expect(createCall.properties.image).not.toBeDefined();
-        expect(createCall.properties.style).not.toBeDefined();
-        expect(createCall.properties.topLevel).not.toBeDefined();
+        expect(properties.title).not.toBeDefined();
+        expect(properties.image).not.toBeDefined();
+        expect(properties.style).not.toBeDefined();
+        expect(properties.topLevel).not.toBeDefined();
       });
 
       it("has non-page properties", function() {
-        expect(createCall.properties.background).toEqual([255, 0, 0, 255]);
+        expect(properties.background).toEqual([255, 0, 0, 255]);
       });
 
     });
 
     describe("created Page", function() {
 
-      var createCall;
-      var compositeId;
+      var properties;
 
       beforeEach(function() {
-        page._create({
-          title: "title",
-          image: {src: "image"},
-          style: "fullscreen",
-          topLevel: true,
-          background: "red"
-        });
-        createCall = nativeBridge.calls({op: "create", type: "tabris.Page"})[0];
-        compositeId = nativeBridge.calls({op: "create", type: "rwt.widgets.Composite"})[0].id;
+        var createCall = nativeBridge.calls({op: "create", type: "tabris.Page"})[0];
+        properties = createCall.properties;
       });
 
       it("parent is set to tabris.UI", function() {
-        expect(createCall.properties.parent).toBe(uiId);
+        expect(properties.parent).toBe(uiId);
       });
 
       it("control is set to composite", function() {
-        expect(createCall.properties.control).toBe(compositeId);
+        expect(properties.control).toBe(page.id);
       });
 
       it("has title, image and topLevel properties", function() {
-        expect(createCall.properties.title).toBe("title");
-        expect(createCall.properties.image).toEqual(["image", null, null, null]);
-        expect(createCall.properties.style).toBe("fullscreen");
-        expect(createCall.properties.topLevel).toBe(true);
+        expect(properties.title).toBe("title");
+        expect(properties.image).toEqual(["image", null, null, null]);
+        expect(properties.style).toBe("fullscreen");
+        expect(properties.topLevel).toBe(true);
       });
 
       it("does not inherit non-page properties", function() {
-        expect(createCall.properties.background).not.toBeDefined();
+        expect(properties.background).not.toBeDefined();
       });
 
     });
@@ -119,7 +112,7 @@ describe("Page", function() {
     var compositeCreateCall;
 
     beforeEach(function() {
-      page._create({});
+      page = tabris.create("Page");
       pageCreateCall = nativeBridge.calls({op: "create", type: "tabris.Page"})[0];
       compositeCreateCall = nativeBridge.calls({op: "create", type: "rwt.widgets.Composite"})[0];
       nativeBridge.resetCalls();
@@ -202,7 +195,7 @@ describe("Page", function() {
 
       it("uses page's composite in 'set'", function() {
         var call = nativeBridge.calls({op: "set", id: child.id})[0];
-        expect(call.properties.parent).toBe(page._composite.id);
+        expect(call.properties.parent).toBe(page.id);
       });
 
     });

commit 9d692d9414a8f4b66bf0e579834e0ed416979500

Fix page navigation
On Android pages need to be made visible by a set "activePage"
operation. Commit 77771d63 broke this operation by setting undefined
instead of the page id.

Change-Id: Iaefa6e7420133f618059e48d2bd54ad0a5da8a81

src/js/Page.js
@@ -27,6 +27,7 @@
         parent: tabris._uiProxy._ui,
         control: this
       }));
+      this._page.widget = this;
       return this;
     },
 

src/js/UIProxy.js
@@ -41,7 +41,7 @@ tabris.UIProxy.prototype = {
     });
     this._ui.on("ShowPage", function(properties) {
       var page = tabris._proxies[properties.pageId];
-      self.setActivePage(page);
+      self.setActivePage(page.widget);
     });
     this._ui.on("ShowPreviousPage", function() {
       self.getActivePage().close();

test/js/UIProxy.spec.js
@@ -52,7 +52,7 @@ describe("UIProxy", function() {
 
   });
 
-  describe("after creation", function() {
+  describe("instance", function() {
 
     beforeEach(function() {
       uiProxy._create();
@@ -79,7 +79,7 @@ describe("UIProxy", function() {
 
     });
 
-    describe("when a page is created", function() {
+    describe("with a page", function() {
 
       var page;
 
@@ -92,6 +92,12 @@ describe("UIProxy", function() {
       it("ShowPreviousPage event closes page", function() {
         tabris._notify(uiId, "ShowPreviousPage", {});
         expect(page.close).toHaveBeenCalled();
+      });
+
+      it("ShowPage sets activePage", function() {
+        tabris._notify(uiId, "ShowPage", {pageId: page._page.id});
+        var setCall = nativeBridge.calls({op: "set", id: uiId}).pop();
+        expect(setCall.properties.activePage).toBe(page._page.id);
       });
 
     });