91074369b38d3dc46a8b176cb9ed90443c806352
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-11-06 16:21:33 +0100

    Revise WebView snippet
    Add a default page to avoid having to type "http://..."
    
    Alternativly, the url input field could be pre-filled with "http://".
    
    Change-Id: Ic264175e1c318a2bf91c0916a8b1f60cc9047039

--- snippets/webview/webview.js
+++ snippets/webview/webview.js
@@ -1,20 +1,25 @@
 tabris.load(function() {
 
   var page = tabris.create("Page", {
-    title: "Creating a web view",
+    title: "Web View",
     topLevel: true
   });
 
-  var text = tabris.create("Text", {
+  var urlInput = tabris.create("Text", {
     layoutData: {left: 8, right: 8, top: 8},
-    message: "Enter URL..."
-  }).on("accept", function() {
-    webview.set("url", this.get("text"));
-  }).appendTo(page);
+    message: "Enter URL...",
+    text: "http://en.wikipedia.org"
+  }).on("accept", loadUrl).appendTo(page);
 
   var webview = tabris.create("WebView", {
-    layoutData: {left: 0, top: [text, 8], right: 0, bottom: 0}
+    layoutData: {left: 0, top: [urlInput, 8], right: 0, bottom: 0}
   }).appendTo(page);
+
+  function loadUrl() {
+    webview.set("url", urlInput.get("text"));
+  }
+
+  loadUrl();
 
   page.open();
 
