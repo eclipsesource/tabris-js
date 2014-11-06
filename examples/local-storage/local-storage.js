56451163b112e3fb81939d17a11cefc31b41a258
Tim Buschtöns <tbuschto@eclipsesource.com>
2014-11-06 18:12:07 +0100

    Move revised local-storage snippet to examples, replace with new one
    The example showcases all feature of the store, the snippet just set and
    get.
    
    Change-Id: I67a0f2da3db599ec9595238f49c18662a7d550bc

---
+++ examples/local-storage/local-storage.js
@@ -0,0 +1,61 @@
+tabris.load(function() {
+
+  var page = tabris.create("Page", {
+    title: "Storing data on the device",
+    topLevel: true
+  });
+
+  var keyLabel = tabris.create("Label", {
+    layoutData: {left: 10, top: 20},
+    text: "Key:"
+  }).appendTo(page);
+
+  var keyField = tabris.create("Text", {
+    layoutData: {left: [keyLabel, 20], baseline: keyLabel, right: 10},
+    text: "foo"
+  }).appendTo(page);
+
+  var valueLabel = tabris.create("Label", {
+    layoutData: {left: 10, top: [keyField, 20]},
+    text: "Value:"
+  }).appendTo(page);
+
+  var valueField = tabris.create("Text", {
+    layoutData: {left: [valueLabel, 20], baseline: valueLabel, right: 10},
+    text: localStorage.getItem("foo") || "bar"
+  }).appendTo(page);
+
+  var setButton = tabris.create("Button", {
+    layoutData: {left: 10, top: [valueField, 20]},
+    text: "set"
+  }).on("selection", function() {
+    localStorage.setItem(keyField.get("text"), valueField.get("text"));
+    valueField.set("text", "");
+  }).appendTo(page);
+
+  var getButton = tabris.create("Button", {
+    layoutData: {left: [setButton, 10], baseline: setButton},
+    text: "get"
+  }).on("selection", function() {
+    valueField.set("text", localStorage.getItem(keyField.get("text")));
+  }).appendTo(page);
+
+  var removeButton = tabris.create("Button", {
+    layoutData: {left: [getButton, 10], baseline: getButton},
+    text: "remove"
+  }).on("selection", function() {
+    localStorage.removeItem(keyField.get("text"));
+    valueField.set("text", "");
+  }).appendTo(page);
+
+  tabris.create("Button", {
+    layoutData: {left: [removeButton, 10], baseline: removeButton},
+    text: "clear"
+  }).on("selection", function() {
+    localStorage.clear();
+    valueField.set("text", "");
+  }).appendTo(page);
+
+  page.open();
+
+});
--- snippets/local-storage/local-storage.js
+++ snippets/local-storage/local-storage.js
@@ -1,63 +1,20 @@
 tabris.load(function() {
 
-  var MARGIN = 8;
-  var MARGIN_LARGE = 16;
-  var STORAGE_KEY = "message";
-
   var page = tabris.create("Page", {
-    title: "Storing key/value data pairs on the device",
+    title: "Local Storage",
     topLevel: true
   });
 
-  var storeText = tabris.create("Text", {
-    text: "Hallo World!",
-    message: "String to store",
-    layoutData: {left: MARGIN, top: MARGIN_LARGE, right: MARGIN}
-  });
+  var KEY = "localStorageSnippetCount";
 
-  var addButton = tabris.create("Button", {
-    text: "Add",
-    layoutData: {left: MARGIN, right: [66.6, MARGIN], top: [storeText, MARGIN_LARGE]}
-  });
-
-  var removeButton = tabris.create("Button", {
-    text: "Remove",
-    layoutData: {left: [33.3, 0], right: [33.3, 0], top: [storeText, MARGIN_LARGE]}
-  });
-
-  var clearButton = tabris.create("Button", {
-    text: "Clear",
-    layoutData: {left: [66.6, MARGIN], right: MARGIN, top: [storeText, MARGIN_LARGE]}
-  });
-
-  var storeLabel = tabris.create("Label", {
-    layoutData: {left: MARGIN, top: [addButton, MARGIN_LARGE], right: MARGIN},
-    markupEnabled: true
-  });
-
-  page.append(storeText, addButton, removeButton, clearButton, storeLabel);
-
-  function updateStoreLabel() {
-    var value = localStorage.getItem(STORAGE_KEY);
-    storeLabel.set("text", "<b>Store content:</b><br />".concat(value));
-  }
-
-  addButton.on("selection", function() {
-    localStorage.setItem(STORAGE_KEY, storeText.get("text"));
-    updateStoreLabel();
-  });
-
-  removeButton.on("selection", function() {
-    localStorage.removeItem(STORAGE_KEY);
-    updateStoreLabel();
-  });
-
-  clearButton.on("selection", function() {
-    localStorage.clear();
-    updateStoreLabel();
-  });
-
-  updateStoreLabel();
+  var startCount = parseInt(localStorage.getItem(KEY) || "0") + 1;
+  localStorage.setItem(KEY, startCount.toString());
+  tabris.create("Label", {
+    layoutData: {left: 10, right: 10, centerY: 0},
+    alignment: "center",
+    font: "22px sans-serif",
+    text: "This application was started " + startCount + " time(s)."
+  }).appendTo(page);
 
   page.open();
 
