a946960fe01c83b062815a10a759fc92989809ae
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-11-06 12:32:03 +0100

    Revise Canvas snippet
    Change-Id: I823dba7e66d53b0e0978361c3fec7b8352ca7545

--- snippets/canvas/canvas.js
+++ snippets/canvas/canvas.js
@@ -1,17 +1,21 @@
 tabris.load(function() {
 
   var page = tabris.create("Page", {
-    title: "Drawing a rectangle on a canvas",
+    title: "Canvas",
     topLevel: true
   });
 
   var canvas = tabris.create("Canvas", {
     layoutData: {left: 10, top: 10, right: 10, bottom: 10}
+  }).on("change:bounds", function() {
+    var bounds = this.get("bounds");
+    var ctx = tabris.getContext(canvas, bounds.width, bounds.height);
+    ctx.strokeStyle = "rgb(78, 154, 217)";
+    ctx.lineWidth = 10;
+    ctx.moveTo(20, 20);
+    ctx.lineTo(bounds.width - 40, bounds.height - 40);
+    ctx.stroke();
   }).appendTo(page);
-
-  var ctx = tabris.getContext(canvas, 400, 400);
-  ctx.fillStyle = "rgb(78, 154,217)";
-  ctx.fillRect(20, 20, 150, 100);
 
   page.open();
 
