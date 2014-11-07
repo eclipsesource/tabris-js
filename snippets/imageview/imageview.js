8ed551efea0d8fb43149986ab3ade507411fad4a
Tim Buschtöns <tbuschto@eclipsesource.com>
2014-11-07 10:30:16 +0100

    Revise image view snippet
    Show different scale mode in basic image view example.
    
    Change-Id: I08bf313d2f113fbd51f6a35ed102926c4ff84c17

---
+++ snippets/imageview/imageview.js
@@ -0,0 +1,23 @@
+tabris.load(function() {
+
+  var page = tabris.create("Page", {
+    title: "Image View",
+    topLevel: true
+  });
+
+  var createImageView = function(scaleMode) {
+    tabris.create("ImageView", {
+      layoutData: {left: 10, top: [page.children().pop() || 0, 10], width: 250, height: 100},
+      image: {src: "images/target_200.png"},
+      background: "#aaaaaa",
+      scaleMode: scaleMode
+    }).appendTo(page);
+  };
+
+  createImageView("fit");
+  createImageView("none");
+  createImageView("fill");
+
+  page.open();
+
+});
