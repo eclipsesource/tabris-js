907805e039851982282999a7a60a6d2472b723ea
Tim Buschtöns <tbuschto@eclipsesource.com>
2014-11-07 15:55:19 +0100

    Revise the TabFolder snippet
    - add icons
    - use appendTo
    
    Change-Id: If718d5cd0c427579ac8518b7ee0430a5296dda68

--- snippets/tabfolder/tabfolder.js
+++ snippets/tabfolder/tabfolder.js
@@ -1,28 +1,29 @@
 tabris.load(function() {
 
   var page = tabris.create("Page", {
-    title: "Creating a tab folder",
+    title: "TabFolder",
     topLevel: true
   });
 
-  page.append(
-    tabris.create("TabFolder", {
-      layoutData: {left: 0, top: 0, right: 0, bottom: 0},
-      paging: true,       // enables swiping through tabs
-      background: "#234",
-      foreground: "#9ab"
-    }).append(
-      tabris.create("Tab", {title: "Tab 1", background: "white"}).append(
-        tabris.create("Label", {text: "Content of Tab 1"})
-      ),
-      tabris.create("Tab", {title: "Tab 2", background: "white"}).append(
-        tabris.create("Label", {text: "Content of Tab 2"})
-      ),
-      tabris.create("Tab", {title: "Tab 3", background: "white"}).append(
-        tabris.create("Label", {text: "Content of Tab 3"})
-      )
-    )
-  );
+  var tabFolder = tabris.create("TabFolder", {
+    layoutData: {left: 0, top: 0, right: 0, bottom: 0},
+    paging: true // enables swiping. To still be able to open the developer console in iOS, swipe from the bottom right.
+  }).appendTo(page);
+
+  var createTab = function(title, image) {
+    var tab = tabris.create("Tab", {
+      title: title, // converted to upper-case on Android
+      image: {src: image, width: 15, height: 15} // only used by iOS
+    }).appendTo(tabFolder);
+    tabris.create("Label", {
+      layoutData: {centerX: 0, centerY: 0},
+      text: "Content of Tab " + title
+    }).appendTo(tab);
+  };
+
+  createTab("Cart", "images/cart.png");
+  createTab("Pay", "images/card.png");
+  createTab("Statistic", "images/chart.png");
 
   page.open();
 
