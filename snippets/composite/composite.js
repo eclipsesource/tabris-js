b712b0fa4d361ee2d724700bef7e875ec75dd3c1
Christian Petrov <cpetrov@eclipsesource.com>
2014-11-12 18:24:59 +0100

    Add composite snippets
    Add composite and composite-highlight-on-touch snippets.
    
    * "composite" demonstrates creating a composite and adding a label to
    it.
    * "composite-highlight-on-touch" demonstrates the highlightOnTouch
    property used on a composite.
    
    Change-Id: Ic8b3b0a4690f2158522f6c6bdd9a3e234528e51e

---
+++ snippets/composite/composite.js
@@ -0,0 +1,32 @@
+tabris.load(function() {
+
+  var page = tabris.create("Page", {
+    title: "Composite",
+    topLevel: true
+  });
+
+  var composite1 = tabris.create("Composite", {
+    layoutData: {left: 0, top: 0, bottom: 0, right: [50, 0]},
+    background: "#f3f3f3"
+  }).appendTo(page);
+
+  tabris.create("Label", {
+    alignment: "center",
+    layoutData: {left: 0, right: 0, top: [50, 0]},
+    text: "Composite 1"
+  }).appendTo(composite1);
+
+  var composite2 = tabris.create("Composite", {
+    layoutData: {left: [composite1, 0], top: 0, bottom: 0, right: 0},
+    background: "#eaeaea"
+  }).appendTo(page);
+
+  tabris.create("Label", {
+    alignment: "center",
+    layoutData: {left: 0, right: 0, top: [50, 0]},
+    text: "Composite 2"
+  }).appendTo(composite2);
+
+  page.open();
+
+});
