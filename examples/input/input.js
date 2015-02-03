57bec44aba7c7139e208debc9c4581ae78d4c3ed
Christian Petrov <cpetrov@eclipsesource.com>
2015-02-03 12:57:45 +0100

    Replace deprecated types in input example
    Done to reflect eclipsesource/tabris-js@ca0f105e82a2d27067c9674ecf71a93af2c7b7bd
    and eclipsesource/tabris-js@7380149aa7627a7fc79caff12eaf6e0d0a6e5dc4
    
    Change-Id: Ibfa87226c43a155893ebe4dc446d49afa961ac16

--- examples/input/input.js
+++ examples/input/input.js
@@ -10,66 +10,66 @@ var page = tabris.create("Page", {
   topLevel: true
 });
 
-var firstNameLabel = tabris.create("Label", {
+var firstNameTextView = tabris.create("TextView", {
   layoutData: {left: 10, top: 18, width: 120},
   alignment: "left",
   text: "First Name:"
 }).appendTo(page);
 
-var firstNameInput = tabris.create("Text", {
-  layoutData: {left: [firstNameLabel, 10], right: 10, baseline: firstNameLabel},
+var firstNameInput = tabris.create("TextInput", {
+  layoutData: {left: [firstNameTextView, 10], right: 10, baseline: firstNameTextView},
   message: "First Name"
 }).appendTo(page);
 
-var lastNameLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [firstNameLabel, 18], width: 120},
+var lastNameTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [firstNameTextView, 18], width: 120},
   alignment: "left",
   text: "Last Name:"
 }).appendTo(page);
 
-var lastNameInput = tabris.create("Text", {
-  layoutData: {left: [lastNameLabel, 10], right: 10, baseline: lastNameLabel},
+var lastNameInput = tabris.create("TextInput", {
+  layoutData: {left: [lastNameTextView, 10], right: 10, baseline: lastNameTextView},
   message: "Last Name"
 }).appendTo(page);
 
-var passphraseLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [lastNameLabel, 18], width: 120},
+var passphraseTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [lastNameTextView, 18], width: 120},
   alignment: "left",
   text: "Passphrase:"
 }).appendTo(page);
 
-tabris.create("Text", {
+tabris.create("TextInput", {
   type: "password",
-  layoutData: {left: [passphraseLabel, 10], right: 10, baseline: passphraseLabel},
+  layoutData: {left: [passphraseTextView, 10], right: 10, baseline: passphraseTextView},
   message: "Passphrase"
 }).appendTo(page);
 
-var countryLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [passphraseLabel, 18], width: 120},
+var countryTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [passphraseTextView, 18], width: 120},
   alignment: "left",
   text: "Country:"
 }).appendTo(page);
 
 tabris.create("Combo", {
-  layoutData: {left: [countryLabel, 10], right: 10, baseline: countryLabel},
+  layoutData: {left: [countryTextView, 10], right: 10, baseline: countryTextView},
   items: ["Germany", "Canada", "USA", "Bulgaria"],
   selectionIndex: 0
 }).appendTo(page);
 
-var classLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [countryLabel, 18], width: 120},
+var classTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [countryTextView, 18], width: 120},
   alignment: "left",
   text: "Class:"
 }).appendTo(page);
 
 tabris.create("Combo", {
-  layoutData: {left: [classLabel, 10], right: 10, baseline: classLabel},
+  layoutData: {left: [classTextView, 10], right: 10, baseline: classTextView},
   items: ["Business", "Economy", "Economy Plus"],
   selectionIndex: 0
 }).appendTo(page);
 
-var dateLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [classLabel, 18], width: 120},
+var dateTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [classTextView, 18], width: 120},
   alignment: "left",
   text: "Date:"
 }).appendTo(page);
@@ -77,32 +77,32 @@ var dateLabel = tabris.create("Label", {
 var current = new Date();
 var dateField = tabris.create("DateTime", {
   style: ["DATE"],
-  layoutData: {left: [dateLabel, 10], right: 10, baseline: dateLabel},
+  layoutData: {left: [dateTextView, 10], right: 10, baseline: dateTextView},
   year: current.getFullYear(),
   month: current.getMonth(),
   day: current.getDate()
 }).appendTo(page);
 
-var luggageLabel = tabris.create("Label", {
-  layoutData: {left: 10, top: [dateLabel, 18], width: 120},
+var luggageTextView = tabris.create("TextView", {
+  layoutData: {left: 10, top: [dateTextView, 18], width: 120},
   alignment: "left",
   text: "Luggage:"
 }).appendTo(page);
 
-var luggageWeight = tabris.create("Label", {
-  layoutData: {right: 10, baseline: luggageLabel, width: 50},
+var luggageWeight = tabris.create("TextView", {
+  layoutData: {right: 10, baseline: luggageTextView, width: 50},
   alignment: "left",
   text: "0 Kg"
 }).appendTo(page);
 
 var luggage = tabris.create("Slider", {
-  layoutData: {left: [luggageLabel, 10], right: [luggageWeight, 10], top: [dateLabel, 18]}
+  layoutData: {left: [luggageTextView, 10], right: [luggageWeight, 10], top: [dateTextView, 18]}
 }).on("change:selection", function() {
   luggageWeight.set("text", this.get("selection") + " Kg");
 }).appendTo(page);
 
 var checkbox = tabris.create("CheckBox", {
-  layoutData: {left: [dateLabel, 10], right: 10, top: [luggage, 10]},
+  layoutData: {left: [dateTextView, 10], right: 10, top: [luggage, 10]},
   text: "Vegetarian"
 }).appendTo(page);
 
@@ -119,7 +119,7 @@ function populateMessage() {
   if (message) {
     message.dispose();
   }
-  message = tabris.create("Label", {
+  message = tabris.create("TextView", {
     layoutData: {left: 10, right: 10, top: [button, 10]},
     alignment: "left",
     text: "Flight booked for: " + createName() + "\n" + "Departure: " + createDepartureDate()
