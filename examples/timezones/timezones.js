8b9ed045a37fe1f41426297df0bec93975b3d784
Christian Petrov <cpetrov@eclipsesource.com>
2015-02-03 13:05:20 +0100

    Rename "Label" to "TextView" in timezones example
    Done to reflect eclipsesource/tabris-js@ca0f105e82a2d27067c9674ecf71a93af2c7b7bd
    
    Change-Id: If64183b7ff166fff048c1614a0897f847d16d55a

--- examples/timezones/timezones.js
+++ examples/timezones/timezones.js
@@ -7,19 +7,19 @@ var page = tabris.create("Page", {
 
 page.open();
 
-createLabel("Europe/Berlin", "Berlin");
-createLabel("America/New_York", "New York");
-createLabel("Asia/Tokyo", "Tokyo");
+createTextView("Europe/Berlin", "Berlin");
+createTextView("America/New_York", "New York");
+createTextView("Asia/Tokyo", "Tokyo");
 
 update();
 
-function createLabel(timezone, name) {
-  var locationLabel = tabris.create("Label", {
+function createTextView(timezone, name) {
+  var locationTextView = tabris.create("TextView", {
     layoutData: {top: [page.children().last() || 0, 30], centerX: 0},
     text: name
   }).appendTo(page);
-  tabris.create("Label", {
-    layoutData: {top: [locationLabel, 10], centerX: 0},
+  tabris.create("TextView", {
+    layoutData: {top: [locationTextView, 10], centerX: 0},
     font: "bold 50px sans-serif"
   }).on("update", function() {
     this.set("text", moment.tz(timezone).format("h:mm a"));
