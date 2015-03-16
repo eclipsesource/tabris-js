0829d3a3768cb0268383ba94f7c72391165d17ae
Jochen Krause <jkrause@eclipsesource.com>
2015-03-16 09:50:34 +0100

    Introduce picker for language selection
    Enable the user to change the language dynamically.
    
    Change-Id: I5d2ba5c40fb19bfe7f6ce5620df7d26e5ca1eb9d

--- examples/i18n/i18n.js
+++ examples/i18n/i18n.js
@@ -1,17 +1,26 @@
-var data = tabris.device.get("language") === "de-DE" ? require("./de.json") : require("./en.json");
+var texts = device.language === "de-DE" ? require("./de.json") : require("./en.json");
 
 var MARGIN = 10;
 
 var page = tabris.create("Page", {
-  title: data.title,
+  id: "menuPage",
   background: "white",
   topLevel: true
 });
 
+tabris.create("Picker", {id: "langPicker", layoutData: {left: 10, top: 10, right: 10}})
+  .on("change:selection", function() {
+    var selectionIndex = this.get("selectionIndex");
+    if (selectionIndex > 0) {
+      this.set("selectionIndex", 0);
+      page.apply(require("./" + this.get("items")[selectionIndex] + ".json"));
+    }
+  }).appendTo(page);
+
 tabris.create("CollectionView", {
-  items: data.menuItems,
+  id: "menuItemsCV",
   itemHeight: 100,
-  layoutData: {left: 0, top: 0, right: 0, bottom: 0},
+  layoutData: {left: 0, top: ["#langPicker", 10], right: 0, bottom: 0},
   initializeCell: function(cell) {
     var price = tabris.create("TextView", {
       layoutData: {centerY: 0, right: MARGIN, width: 100},
@@ -37,5 +46,7 @@ tabris.create("CollectionView", {
     });
   }
 }).appendTo(page);
+
+page.apply(texts);
 
 page.open();
