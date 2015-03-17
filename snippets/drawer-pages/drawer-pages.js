3d0a5c504bc799b30552dbca945e469a849804b7
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-03-17 18:53:27 +0100

    Add a snippet for the page selector
    Change-Id: I9cfd101999c1cef6e047ca6905f8e38bbd7200fc

---
+++ snippets/drawer-pages/drawer-pages.js
@@ -0,0 +1,29 @@
+createPage("Page 1").open();
+createPage("Page 2");
+
+var drawer = tabris.create("Drawer");
+
+tabris.create("ImageView", {
+  image: "images/cover.jpg",
+  scaleMode: "fill",
+  layoutData: {left: 0, right: 0, top: 0, height: 200}
+}).appendTo(drawer);
+
+tabris.create("PageSelector", {
+  layoutData: {left: 0, top: 200, right: 0, bottom: 0}
+}).appendTo(drawer);
+
+function createPage(title) {
+  var page = tabris.create("Page", {
+    title: title,
+    image: "images/page.png",
+    topLevel: true
+  });
+  tabris.create("Button", {
+    text: "Create another page",
+    layoutData: {left: 20, right: 20, top: 20}
+  }).on("selection", function() {
+    createPage("Another Page");
+  }).appendTo(page);
+  return page;
+}
