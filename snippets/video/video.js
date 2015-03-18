3a89c7f33823d209ce71b4eb51c219397466c005
Christian Petrov <cpetrov@eclipsesource.com>
2015-03-18 16:26:11 +0100

    Update Sintel movie source
    Themazzone mirror was down.
    
    Change-Id: I98a5e4b4ccbaf57ac0f72a0afae1d8708eeb675b

--- snippets/video/video.js
+++ snippets/video/video.js
@@ -5,7 +5,7 @@ var page = tabris.create("Page", {
 
 tabris.create("Video", {
   layoutData: {left: 0, right: 0, top: 0, bottom: 0},
-  url: "http://peach.themazzone.com/durian/movies/sintel-1280-stereo.mp4"
+  url: "http://mirrorblender.top-ix.org/movies/sintel-1280-stereo.mp4"
 }).appendTo(page);
 
 page.open();
