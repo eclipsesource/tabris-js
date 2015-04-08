86f31ca31eaca8e37ca79b77b7012fdd5c0a57b7
Tim Buschtöns <tbuschto@eclipsesource.com>
2015-04-08 13:52:32 +0200

    Replace jQuery example with fetch() example
    Unlike jQuery, fetch() can be directly installed via npm. It's therefore
    better suited as a featured example.
    
    NOTE: This requires recent changes in the module system. Do not submit
    this before the clients have been updated with a new boot.js.
    
    Change-Id: Ia54b7826c170f7b36d6c7594363ca00b3724f7e5

---
+++ examples/fetch/http-request.js
@@ -0,0 +1,26 @@
+/* globals fetch: false, Promise: true*/
+Promise = require("promise");
+require("whatwg-fetch");
+
+var page = tabris.create("Page", {
+  title: "XMLHttpRequest via fetch()",
+  topLevel: true
+});
+
+var createTextView = function(text) {
+  tabris.create("TextView", {
+    text: text,
+    markupEnabled: true,
+    layoutData: {left: 12, right: 12, top: [page.children().last() || "0%", 12]}
+  }).appendTo(page);
+};
+
+fetch("http://www.telize.com/geoip").then(function(response) {
+  return response.json();
+}).then(function(json) {
+  createTextView("The IP address is: " + json.ip);
+  createTextView("Latitude: " + json.latitude);
+  createTextView("Longitude: " + json.longitude);
+});
+
+page.open();
--- examples/jquery/README.md
+++
@@ -1,9 +0,0 @@
-This example showcases how jQuery's Ajax API can be used in Tabris.js.
-Since the DOM manipulations will not work in tabris, we use a custom jQuery
-lib that only contains the Ajax API. It's built with
-
-    grunt custom:-ajax/script,-ajax/jsonp,-css,-deprecated,-dimensions,-effects,-event,-event/alias,-offset,-wrap,-ready,-deferred,-exports/amd,-sizzle
-
-and compressed using [ugilify-js](https://www.npmjs.org/package/uglify-js).
-
-For details on build custom jQuery, see https://github.com/jquery/jquery#how-to-build-your-own-jquery
