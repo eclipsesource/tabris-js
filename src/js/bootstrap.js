ea67dcd774f391d0d0d3e2bfed4d3cbdfcf095c0
Tim Buschtöns <tbuschto@eclipsesource.com>
2015-04-09 15:17:15 +0200

    Implement version check
    The check is implemented in bootstrap.js and called directly after the
    tabris module is required (before it is initialized).
    
    Different tabris.js/client majors print error.
    Bigger tabris.js minor.patch prints warning.
    Smaller/same tabris.js minor.patch is ignored.
    
    Any error during the version check would be caught and handled like
    failure to load tabris.js. This may happen if there are
    breaking changes in the nativeBridge or app service.
    
    Fix #240
    
    Change-Id: I0615b7345bcd7a1b48410dcfc39f2d3b8a850f3a

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,49 +1,65 @@
-tabris._start = function(client) {
-  try {
-    window.global = window.self = window;
-    tabris._client = client;
-    var Module = tabris.Module;
-    var rootModule = new Module();
+(function() {
+  tabris._start = function(client) {
     try {
-      rootModule.require("tabris");
-    } catch (error) {
-      console.error("Could not load tabris module: " + error);
-      console.log(error.stack);
-      return;
-    }
-    tabris._defineModule = function(id, fn) {
-      return new Module(id, rootModule, fn);
-    };
-    tabris._client = client; // required by head.append
-    var cordovaScript = document.createElement("script");
-    cordovaScript.src = "./cordova.js";
-    document.head.appendChild(cordovaScript);
-    if (tabris._init) {
-      tabris._init(client);
-    }
-    var loadMain = function() {
+      window.global = window.self = window;
+      tabris._client = client;
+      var Module = tabris.Module;
+      var rootModule = new Module();
       try {
-        rootModule.require("./");
-        tabris.trigger("flush");
+        rootModule.require("tabris");
+        tabris._client = client; // required by head.append and version check
+        checkVersion();
       } catch (error) {
-        console.error("Could not load main module: " + error);
+        console.error("Could not load tabris module: " + error);
         console.log(error.stack);
+        return;
       }
-    };
-    if (tabris._entryPoint) {
-      tabris._entryPoint(loadMain);
-      delete tabris._entryPoint;
-    } else {
-      loadMain();
+      tabris._defineModule = function(id, fn) {
+        return new Module(id, rootModule, fn);
+      };
+      var cordovaScript = document.createElement("script");
+      cordovaScript.src = "./cordova.js";
+      document.head.appendChild(cordovaScript);
+      if (tabris._init) {
+        tabris._init(client);
+      }
+      var loadMain = function() {
+        try {
+          rootModule.require("./");
+          tabris.trigger("flush");
+        } catch (error) {
+          console.error("Could not load main module: " + error);
+          console.log(error.stack);
+        }
+      };
+      if (tabris._entryPoint) {
+        tabris._entryPoint(loadMain);
+        delete tabris._entryPoint;
+      } else {
+        loadMain();
+      }
+      tabris.trigger("flush");
+    } catch (ex) {
+      console.error(ex);
+      console.log(ex.stack);
     }
-    tabris.trigger("flush");
-  } catch (ex) {
-    console.error(ex);
-    console.log(ex.stack);
-  }
-};
+  };
 
-tabris._notify = function() {
-  // client may get the reference to _notify before tabris has been loaded
-  tabris._notify.apply(this, arguments);
-};
+  tabris._notify = function() {
+    // client may get the reference to _notify before tabris has been loaded
+    tabris._notify.apply(this, arguments);
+  };
+
+  function checkVersion() {
+    var client = tabris._client.get("tabris.App", "tabrisJsVersion").split(".");
+    var tabrisJs = tabris.version.split(".");
+    var message = "Version mismatch: JavaScript module \"tabris\" (version " + tabris.version + ") ";
+    if (tabrisJs[0] !== client[0]) {
+      console.error(message + "is incompatible with this native tabris platform (different major version).");
+    }
+    if (tabrisJs[1] > client[1] || (tabrisJs[1] === client[1] && tabrisJs[2] > client[2])) {
+      console.warn(message + "is newer than the native tabris platform. Some features may not work.");
+    }
+  }
+
+}());
