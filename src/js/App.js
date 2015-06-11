ede6fa3b78e9aafd8145b04bdbf380fcc2817c71
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-06-11 10:20:39 +0200

    Introduce installPatch function on app object
    Allow to patch apps to apply critical fixes without having to wait
    for an update becoming available in the app stores. Example:
    
        tabris.app.installPatch(patchUrl, function(error, patch) {
          if (error) {
            // show error dialog
          } else {
            // confirm reload
            tabris.app.reload();
          }
        });
    
    The clients will download a zip file from the given patchUrl, extract
    it, and prepare to overlay the app's resources with the contents of
    the patch zip after the next reload.
    
    After successful installation, the callback will be called with the
    parameter `patch` containing the contents of the file `patch.json`
    if it was present in the patch. In case of an error, the callback
    will be called with the `error` parameter being set.
    
    See #479
    
    Change-Id: I8919f83ed33f7fc48d480c6123dd107b8c05cf32

--- src/js/App.js
+++ src/js/App.js
@@ -2,18 +2,51 @@ tabris.registerType("_App", {
   _type: "tabris.App",
   _events: {
     pause: {name: "Pause", trigger: triggerWithTarget},
-    resume: {name: "Resume", trigger: triggerWithTarget}
+    resume: {name: "Resume", trigger: triggerWithTarget},
+    patchInstall: {trigger: notifyPatchCallback}
   },
   dispose: function() {
     throw new Error("tabris.app can not be disposed");
   },
   reload: function() {
     this._nativeCall("reload", {});
+  },
+  installPatch: function(url, callback) {
+    if (typeof url !== "string") {
+      throw new Error("parameter url is not a string");
+    }
+    if (!this._pendingPatchCallback) {
+      this._pendingPatchCallback = callback || true;
+      this._listen("patchInstall", true);
+      this._nativeCall("installPatch", {url: url});
+    } else if (typeof callback === "function") {
+      callback(new Error("Another installPatch operation is already pending."));
+    }
   }
 });
 
-tabris.app = tabris("_App");
+tabris.load(function() {
+  tabris.app = tabris("_App");
+});
 
 function triggerWithTarget(event, name) {
   this.trigger(name, this, event);
+}
+
+function notifyPatchCallback(event) {
+  this._listen("patchInstall", false);
+  var callback = this._pendingPatchCallback;
+  delete this._pendingPatchCallback;
+  if (typeof callback === "function") {
+    if (event.error) {
+      callback(new Error(event.error));
+    } else {
+      try {
+        var patch = event.success ? JSON.parse(event.success) : null;
+        callback(null, patch);
+      } catch (error) {
+        callback(new Error("Failed to parse patch.json"));
+      }
+    }
+  }
 }
--- test/js/App.spec.js
+++ test/js/App.spec.js
@@ -4,12 +4,9 @@ describe("App", function() {
 
   beforeEach(function() {
     nativeBridge = new NativeBridgeSpy();
+    tabris._reset();
     tabris._init(nativeBridge);
-    app = tabris("_App");
-  });
-
-  it("is added to tabris object", function() {
-    expect(tabris.app).toEqual(jasmine.any(tabris._App));
+    app = tabris.create("_App");
   });
 
   it("listens for Pause event", function() {
@@ -17,14 +14,14 @@ describe("App", function() {
 
     app.on("pause", listener);
 
-    var calls = nativeBridge.calls({id: "tabris.App", op: "listen", event: "Pause"});
+    var calls = nativeBridge.calls({id: app.cid, op: "listen", event: "Pause"});
     expect(calls[0].listen).toBe(true);
   });
 
   it("triggers pause event", function() {
     var listener = jasmine.createSpy();
     app.on("pause", listener);
-    tabris._notify("tabris.App", "Pause");
+    tabris._notify(app.cid, "Pause");
 
     expect(listener).toHaveBeenCalled();
   });
@@ -34,14 +31,14 @@ describe("App", function() {
 
     app.on("resume", listener);
 
-    var calls = nativeBridge.calls({id: "tabris.App", op: "listen", event: "Resume"});
+    var calls = nativeBridge.calls({id: app.cid, op: "listen", event: "Resume"});
     expect(calls[0].listen).toBe(true);
   });
 
   it("triggers resume event", function() {
     var listener = jasmine.createSpy();
     app.on("resume", listener);
-    tabris._notify("tabris.App", "Resume");
+    tabris._notify(app.cid, "Resume");
 
     expect(listener).toHaveBeenCalled();
   });
@@ -53,13 +50,148 @@ describe("App", function() {
   });
 
   describe("reload", function() {
-    it("issues CALL to reload", function() {
+
+    it("CALLs `reload`", function() {
       spyOn(nativeBridge, "call");
 
       app.reload();
 
-      expect(nativeBridge.call).toHaveBeenCalledWith("tabris.App", "reload", {});
+      expect(nativeBridge.call).toHaveBeenCalledWith(app.cid, "reload", {});
     });
+
+  });
+
+  describe("installPatch", function() {
+
+    var callback, error;
+
+    beforeEach(function() {
+      callback = jasmine.createSpy();
+      error = {};
+    });
+
+    it("fails if parameter is not a string", function() {
+      expect(function() {
+        app.installPatch(23);
+      }).toThrow();
+    });
+
+    it("CALLs `installPatch` with URL", function() {
+      spyOn(nativeBridge, "call");
+
+      app.installPatch("http://example.com/patch");
+
+      expect(nativeBridge.call).toHaveBeenCalledWith(app.cid, "installPatch", {
+        url: "http://example.com/patch"
+      });
+    });
+
+    it("does not CALL `installPatch` when already pending", function() {
+      app.installPatch("http://example.com/patch1");
+      spyOn(nativeBridge, "call");
+
+      app.installPatch("http://example.com/patch2");
+
+      expect(nativeBridge.call).not.toHaveBeenCalled();
+    });
+
+    it("errors if install already pending", function() {
+      app.installPatch("http://example.com/patch");
+
+      app.installPatch("http://example.com/patch", callback);
+
+      expect(callback).toHaveBeenCalledWith(jasmine.any(Error));
+      var err = callback.calls.first().args[0];
+      expect(err.message).toEqual(jasmine.stringMatching("already pending"));
+    });
+
+    it("starts LISTEN on `patchInstall` event", function() {
+      spyOn(nativeBridge, "listen");
+
+      app.installPatch("http://example.com/patch");
+
+      expect(nativeBridge.listen).toHaveBeenCalledWith(app.cid, "patchInstall", true);
+    });
+
+    describe("on success event with illegal JSON", function() {
+
+      beforeEach(function() {
+        app.installPatch("http://example.com/patch", callback);
+        spyOn(nativeBridge, "listen");
+        spyOn(nativeBridge, "call");
+
+        tabris._notify(app.cid, "patchInstall", {success: "{not json}"});
+      });
+
+      it("notifies callback with error", function() {
+        expect(callback).toHaveBeenCalledWith(jasmine.any(Error));
+        expect(callback.calls.argsFor(0)).toEqual([jasmine.stringMatching("parse")]);
+      });
+
+      it("stops LISTEN on `patchInstall` event", function() {
+        expect(nativeBridge.listen).toHaveBeenCalledWith(app.cid, "patchInstall", false);
+      });
+
+      it("allows for subsequent calls", function() {
+        app.installPatch("http://example.com/patch");
+
+        expect(nativeBridge.call).toHaveBeenCalled();
+      });
+
+    });
+
+    describe("on success event", function() {
+
+      beforeEach(function() {
+        app.installPatch("http://example.com/patch", callback);
+        spyOn(nativeBridge, "listen");
+        spyOn(nativeBridge, "call");
+
+        tabris._notify(app.cid, "patchInstall", {success: '{"version": 23}'});
+      });
+
+      it("notifies callback", function() {
+        expect(callback).toHaveBeenCalledWith(null, {version: 23});
+      });
+
+      it("stops LISTEN on `patchInstall` event", function() {
+        expect(nativeBridge.listen).toHaveBeenCalledWith(app.cid, "patchInstall", false);
+      });
+
+      it("allows for subsequent calls", function() {
+        app.installPatch("http://example.com/patch");
+
+        expect(nativeBridge.call).toHaveBeenCalled();
+      });
+
+    });
+
+    describe("on error event", function() {
+
+      beforeEach(function() {
+        app.installPatch("http://example.com/patch", callback);
+        spyOn(nativeBridge, "listen");
+        spyOn(nativeBridge, "call");
+
+        tabris._notify(app.cid, "patchInstall", {error: error});
+      });
+
+      it("notifies callback", function() {
+        expect(callback).toHaveBeenCalledWith(new Error(error));
+      });
+
+      it("stops LISTEN on `patchInstall` event", function() {
+        expect(nativeBridge.listen).toHaveBeenCalledWith(app.cid, "patchInstall", false);
+      });
+
+      it("allows for subsequent calls", function() {
+        app.installPatch("http://example.com/patch");
+
+        expect(nativeBridge.call).toHaveBeenCalled();
+      });
+
+    });
+
   });
 
 });
