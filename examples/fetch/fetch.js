b3453297a8f6c4424b9f789648aaf5e9336395b9
“shaialon” <shaialon84@gmail.com>
2016-01-24 17:20:03 +0200

    code cleanup

--- examples/fetch/fetch.js
+++ examples/fetch/fetch.js
@@ -19,23 +19,26 @@ var createTextView = function(text) {
 var reloadButton;
 
 var loadData = function(){
-  // Dispose existing elements
+  // Dispose existing elements via the class selector
   page.children(".location-data").dispose();
   page.children(".reload-button").dispose();
 
   // Create loading indicator
   var activityIndicator = tabris.create("ActivityIndicator").appendTo(page);
 
-
   fetch("https://freegeoip.net/json/").then(function(response) {
     return response.json();
   }).catch(function(err){
-    console.log(err || "Error loading geo-data");
+
+    // On error show want went wrong and reload button.
+    createTextView("Failure: " + err || "Error loading geo-data");
+    createReloadButton();
   }).then(function(json) {
-    // Hide the loader
+
+    // Dispose of the activity loader via direct reference
     activityIndicator.dispose();
 
-    // Show the result data
+    // Show the result location data
     createTextView("The IP address is: " + json.ip);
     createTextView("City: " + json.city);
     createTextView("Country: " + json.country_name);
@@ -43,15 +46,19 @@ var loadData = function(){
     createTextView("Longitude: " + json.longitude);
 
     // Create the reload button
-    reloadButton = tabris.create("Button", {
-      layoutData: {left: 16, right: 16, top: "prev() 12"},
-      text: "Reload Geo-Data",
-      class: "reload-button"
-    }).on("select", function() {
-      loadData();
-    }).appendTo(page);
+    createReloadButton();
   });
 }
+
+var createReloadButton = function(){
+  reloadButton = tabris.create("Button", {
+    layoutData: {left: 16, right: 16, top: "prev() 12"},
+    text: "Reload Geo-Data",
+    class: "reload-button"
+  }).on("select", function() {
+    loadData();
+  }).appendTo(page);
+};
 
 page.open();
 loadData();
