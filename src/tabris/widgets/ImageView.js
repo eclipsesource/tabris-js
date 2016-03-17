f2ffdc899fa40307f4d64182dac84d9fb49e9d8d
Moritz Post <mpost@eclipsesource.com>
2016-03-17 16:52:52 +0100

    Add support for "load" event on ImageView
    A "load" event is fired when an image has finished loading in
    an ImageView. The callback contains an event object with a
    boolean property "error" that indicates if the image could be
    loaded successfully.
    
    Close #614.
    
    Change-Id: Icf6fe79b6b60f42e7d3759db3d865a10a578fd2d

--- src/tabris/widgets/ImageView.js
+++ src/tabris/widgets/ImageView.js
@@ -1,5 +1,12 @@
 tabris.registerWidget("ImageView", {
   _type: "tabris.ImageView",
+  _events: {
+    load: {
+      trigger: function(event) {
+        this.trigger("load", this, event);
+      }
+    }
+  },
   _properties: {
     image: {type: "image", default: null},
     scaleMode: {type: ["choice", ["auto", "fit", "fill", "stretch", "none"]], default: "auto"}
