c8159ab102a17914ebb70e240d237ccefb2a5f62
Tim Buschtoens <tbuschto@eclipsesource.com>
2016-03-24 10:12:53 +0100

    Prevent PageSelector from sending the create operation twice
    With the recent change from using tabris.create to constructors, the
    PageSelector must no longer call _create explicitly.
    
    Change-Id: Ib6ed12b716bed4a634eb7f99362ee3e5e15b6f46

--- src/tabris/widgets/PageSelector.js
+++ src/tabris/widgets/PageSelector.js
@@ -1,21 +1,13 @@
 (function() {
 
   tabris.PageSelector = function(properties) {
-    var instance = new tabris.CollectionView();
-    _create.call(instance, properties);
-    return instance;
-  };
-
-  tabris.PageSelector.prototype.type = "PageSelector";
-
-  function _create(properties) {
-    tabris.CollectionView.prototype._create.call(this, _.extend({
+    var instance = new tabris.CollectionView(_.extend({
       items: getPages(),
       initializeCell: initializeCell,
       itemHeight: device.platform === "iOS" ? 40 : 48,
       layoutData: {left: 0, top: 0, right: 0, bottom: 0}
     }, properties));
-    this.on("select", function(target, value) {
+    instance.on("select", function(target, value) {
       if (value instanceof tabris.Page) {
         if (tabris.ui.drawer) {
           tabris.ui.drawer.close();
@@ -23,13 +15,15 @@
         value.open();
       }
     });
-    tabris.ui.on("addchild", insertPage, this).on("removechild", removePage, this);
-    this.on("dispose", function() {
+    tabris.ui.on("addchild", insertPage, instance).on("removechild", removePage, instance);
+    instance.on("dispose", function() {
       tabris.ui.off("addchild", insertPage);
       tabris.ui.off("removechild", removePage);
     });
-    return this;
-  }
+    return instance;
+  };
+
+  tabris.PageSelector.prototype.type = "PageSelector";
 
   function insertPage(ui, child) {
     if (child instanceof tabris.Page && child.get("topLevel")) {
