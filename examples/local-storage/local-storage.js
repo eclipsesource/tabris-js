eclipsesource/tabris-js

commit f707517aaeda46ab25bc653bd529d0d98957dabc

Allow direct widget property access via fields
In addition to `widget.get("foo")` also allow `widget.foo`.
In addition to `widget.set("foo", 1)` also allow `widget.foo = 1`.

Replace uses of widget.get in snippets and examples. Uses of widget.set
with two arguments would also be desirable, but can not be done with
regular expressions.

Change-Id: I1ca8ef9eae89ce8461c96a017c30d302ceb41855

examples/local-storage/local-storage.js
@@ -27,12 +27,12 @@ new tabris.Button({
   layoutData: {left: 10, right: "66% 5", top: [valueTextView, 20]},
   text: "Set"
 }).on("select", function() {
-  if (!keyField.get("text")) {
+  if (!keyField.text) {
     console.error("The key cannot be empty.");
-  } else if (!valueField.get("text")) {
+  } else if (!valueField.text) {
     console.error("The value cannot be empty.");
   } else {
-    localStorage.setItem(keyField.get("text"), valueField.get("text"));
+    localStorage.setItem(keyField.text"), valueField.get("text);
     valueField.set("text", "");
   }
 }).appendTo(page);
@@ -41,11 +41,11 @@ new tabris.Button({
   layoutData: {left: "33% 5", right: "33% 5", top: [valueTextView, 20]},
   text: "Get"
 }).on("select", function() {
-  if (!keyField.get("text")) {
+  if (!keyField.text) {
     console.error("The key cannot be empty.");
   } else {
     valueField.set("text", "");
-    valueField.set("text", localStorage.getItem(keyField.get("text")));
+    valueField.set("text", localStorage.getItem(keyField.text));
   }
 }).appendTo(page);
 
@@ -53,7 +53,7 @@ new tabris.Button({
   layoutData: {left: "66% 5", right: 10, top: [valueTextView, 20]},
   text: "Remove"
 }).on("select", function() {
-  localStorage.removeItem(keyField.get("text"));
+  localStorage.removeItem(keyField.text);
   valueField.set("text", "");
 }).appendTo(page);
 

examples/animation/tray.js
@@ -64,7 +64,7 @@ new tabris.TextView({
 }).appendTo(trayContent);
 
 trayContent.on("resize", function() {
-  var bounds = trayContent.get("bounds");
+  var bounds = trayContent.bounds;
   trayHeight = bounds.height;
   if (trayState === "dragging") {
     positionTrayInRestingState(2000);
@@ -76,7 +76,7 @@ trayContent.on("resize", function() {
 strap.on("pan:vertical", function(widget, event) {
   if (event.state === "start" && (trayState === "up" || trayState === "down")) {
     trayState = "dragging";
-    dragOffset = tray.get("transform").translationY - event.translation.y;
+    dragOffset = tray.transform.translationY - event.translation.y;
   }
   if (trayState === "dragging") {
     var offsetY = Math.min(Math.max(event.translation.y + dragOffset, 0), trayHeight);

examples/gallery/gallery.js
@@ -42,7 +42,7 @@ var thumbnailsAction = new tabris.Action({
 page.open();
 
 function toggleAction() {
-  if (scrollView.get("visible")) {
+  if (scrollView.visible) {
     scrollView.set("visible", false);
     thumbnailsAction.set("visible", true);
     fullscreenAction.set("visible", false);

examples/input/input.js
@@ -161,8 +161,8 @@ function populateMessage() {
   message = new tabris.TextView({
     layoutData: {left: 10, right: 10, top: "#reservationButton 10", bottom: 10},
     text: [
-      "Flight booked for: " + scrollView.children("#nameInput").get("text"),
-      "Destination: " + scrollView.children("#countryPicker").get("selection"),
+      "Flight booked for: " + scrollView.children("#nameInput").first().text,
+      "Destination: " + scrollView.children("#countryPicker").first().selection,
       "Seating: " + createSeating(),
       "Luggage: " + createWeight(),
       "Meal: " + createMeal(),
@@ -174,27 +174,27 @@ function populateMessage() {
 function createSeating() {
   var seating = "Anywhere";
   scrollView.children("RadioButton").forEach(function(button) {
-    if (button.get("selection")) {
-      seating = button.get("text");
+    if (button.selection) {
+      seating = button.text;
     }
   });
-  seating += ", " + scrollView.children("#classPicker").get("selection");
+  seating += ", " + scrollView.children("#classPicker").first().selection;
   return seating;
 }
 
 function createWeight() {
   var panel = scrollView.children("#luggagePanel");
-  return panel.children("#luggageSlider").get("selection") + " Kg";
+  return panel.children("#luggageSlider").first().selection + " Kg";
 }
 
 function createMeal() {
-  return scrollView.children("#veggieChoice").get("selection") ? "Vegetarian" : "Standard";
+  return scrollView.children("#veggieChoice").selection ? "Vegetarian" : "Standard";
 }
 
 function createFrequentFlyerInfo() {
   var panel = scrollView.children("#milesPanel");
-  var info = panel.children("#milesSwitch").get("selection") ? "Yes" : "No";
-  info += ", acct: " + scrollView.children("#flyerNumberInput").get("text");
+  var info = panel.children("#milesSwitch").first().selection ? "Yes" : "No";
+  info += ", acct: " + scrollView.children("#flyerNumberInput").first().text;
   return info;
 }
 

examples/parallax/parallax.js
@@ -74,7 +74,7 @@ new tabris.TextView({
 scrollView.on("resize", function(widget, bounds) {
   var imageHeight = bounds.height / 2;
   imageView.set("height", imageHeight);
-  var titleCompHeight = titleComposite.get("height");
+  var titleCompHeight = titleComposite.height;
   // We need the offset of the title composite in each scroll event.
   // As it can only change on resize, we assign it here.
   titleCompY = Math.min(imageHeight - titleCompHeight, bounds.height / 2);

examples/swipe-to-dismiss/swipe-to-dismiss.js
@@ -60,7 +60,7 @@ function handlePan(event, container) {
 }
 
 function handlePanFinished(event, container) {
-  var beyondCenter = Math.abs(event.translation.x) > container.get("bounds").width / 2;
+  var beyondCenter = Math.abs(event.translation.x) > container.bounds.width / 2;
   var fling = Math.abs(event.velocity.x) > 200;
   var sameDirection = sign(event.velocity.x) === sign(event.translation.x);
   // When swiped beyond the center, trigger dismiss if flinged in the same direction or let go.
@@ -74,14 +74,14 @@ function handlePanFinished(event, container) {
 }
 
 function animateDismiss(event, container) {
-  var bounds = container.get("bounds");
+  var bounds = container.bounds;
   container.animate({
     transform: {translationX: sign(event.translation.x) * bounds.width}
   }, {
     duration: 200,
     easing: "ease-out"
   }).then(function() {
-    collectionView.remove(container.parent().get("itemIndex"));
+    collectionView.remove(container.parent().itemIndex);
   });
 }
 

examples/timer/timer.js
@@ -41,8 +41,8 @@ var startButton = new tabris.Button({
   text: "Start timer",
   layoutData: {left: ["50%", MARGIN / 4], top: [repeatCheckbox, MARGIN_LARGE], right: MARGIN}
 }).on("select", function() {
-  var delay = parseInt(delayTextInput.get("text"));
-  if (repeatCheckbox.get("selection")) {
+  var delay = parseInt(delayTextInput.text);
+  if (repeatCheckbox.selection) {
     taskId = setInterval(updateStatusTextViews, delay);
   } else {
     taskId = setTimeout(function() {

snippets/collectionview-scroll/collectionview-scroll.js
@@ -34,7 +34,7 @@ new tabris.CollectionView({
   }
 }).on("scroll", function(collectionView, event) {
   scrollPosition += event.deltaY;
-  var firstVisibleItem = collectionView.get("firstVisibleIndex");
+  var firstVisibleItem = collectionView.firstVisibleIndex;
   floatingSection.set({
     text: getCurrentSection(firstVisibleItem).name,
     transform: {translationY: getSectionTranslationY(firstVisibleItem)}

commit ce44e5b9d189bb8edf830bb8fe39edd9b8f79ae9

Fox local storage example
The example was broken in 

commit f707517aaeda46ab25bc653bd529d0d98957dabc 
"Allow direct widget property access via fields" 

due to a regular expression applied to all examples. 

Change-Id: Ie295038a52b1d3f177784415af98ec2a091fdc82

examples/local-storage/local-storage.js
@@ -32,7 +32,7 @@ new tabris.Button({
   } else if (!valueField.text) {
     console.error("The value cannot be empty.");
   } else {
-    localStorage.setItem(keyField.text"), valueField.get("text);
+    localStorage.setItem(keyField.text, valueField.text);
     valueField.set("text", "");
   }
 }).appendTo(page);