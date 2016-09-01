11696845c2e3424484f97f3a01c4fb4128c1ec7d
Christian Petrov <cpetrov@eclipsesource.com>
2016-09-01 18:06:09 +0200

    Fix layout in collectionview-columncount snippet
    On Android, setting the height of a Slider currently leads to
    a broken layout (see #844). Wrap the slider in a composite.

--- snippets/collectionview-columncount/collectionview-columncount.js
+++ snippets/collectionview-columncount/collectionview-columncount.js
@@ -3,20 +3,6 @@ var page = new tabris.Page({
   topLevel: true
 });
 
-var columnCountTextView = new tabris.TextView({
-  bottom: 16, right: 16, width: 32,
-  font: "bold 14px"
-}).appendTo(page);
-
-var slider = new tabris.Slider({
-  left: 16, bottom: 0, right: [columnCountTextView, 16], height: 48,
-  minimum: 1,
-  maximum: 8
-}).on("change:selection", function(slider, selection) {
-  collectionView.set("columnCount", selection);
-  columnCountTextView.set("text", selection);
-}).appendTo(page);
-
 var collectionView = new tabris.CollectionView({
   left: 0, top: 0, right: 0, bottom: slider,
   items: createItems(),
@@ -37,6 +23,25 @@ var collectionView = new tabris.CollectionView({
     });
   }
 }).appendTo(page);
+
+var sliderBox = new tabris.Composite({
+  left: 0, bottom: 0, right: 0, height: 48,
+  background: "white"
+}).appendTo(page);
+
+var columnCountTextView = new tabris.TextView({
+  bottom: 16, right: 16, width: 32,
+  font: "bold 14px"
+}).appendTo(sliderBox);
+
+var slider = new tabris.Slider({
+  left: 16, right: [columnCountTextView, 16], centerY: 0,
+  minimum: 1,
+  maximum: 8
+}).on("change:selection", function(slider, selection) {
+  collectionView.set("columnCount", selection);
+  columnCountTextView.set("text", selection);
+}).appendTo(sliderBox);
 
 slider.set("selection", 3);
 
