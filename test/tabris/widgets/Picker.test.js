69de4f935732a652ccf9d2306b7787a7c74bb4b8
Christian Petrov <cpetrov@eclipsesource.com>
2016-09-09 17:38:40 +0200

    Test Picker as a module
    As part of the migration of tests to Mocha. Follow-up of @38c876d.
    
    Change-Id: Ib5cf4629f1e478a854c4fd9c626e542a92c8ccfd

--- test/tabris/widgets/Picker.test.js
+++ test/tabris/widgets/Picker.test.js
@@ -1,48 +1,59 @@
+import {expect, stub, spy, restore} from "../../test";
+import ProxyStore from "../../../src/tabris/ProxyStore";
+import NativeBridge from "../../../src/tabris/NativeBridge";
+import NativeBridgeSpy from "../NativeBridgeSpy";
+import Picker from "../../../src/tabris/widgets/Picker";
+
 describe("Picker", function() {
 
-  var nativeBridge, picker;
+  let nativeBridge, picker;
 
   beforeEach(function() {
     nativeBridge = new NativeBridgeSpy();
-    tabris._reset();
-    tabris._init(nativeBridge);
-    picker = new tabris.Picker();
+    global.tabris = {
+      on: () => {},
+      _proxies: new ProxyStore(),
+      _notify: (cid, event, param) => tabris._proxies.find(cid)._trigger(event, param)
+    };
+    global.tabris._nativeBridge = new NativeBridge(nativeBridge);
+    picker = new Picker();
   });
+
+  afterEach(restore);
 
   describe("creation", function() {
 
     it("creates picker", function() {
-      expect(nativeBridge.calls({op: "create"})[0].type).toEqual("tabris.Picker");
+      expect(nativeBridge.calls({op: "create"})[0].type).to.eql("tabris.Picker");
     });
 
     it("initializes selectionIndex", function() {
-      expect(nativeBridge.calls({op: "create"})[0].properties).toEqual({selectionIndex: 0});
+      expect(nativeBridge.calls({op: "create"})[0].properties).to.eql({selectionIndex: 0});
     });
 
   });
 
   describe("events:", function() {
 
-    var listener;
-    var checkEvent = function(value) {
-      expect(listener.calls.count()).toBe(1);
-      expect(listener.calls.argsFor(0)[0]).toBe(picker);
+    let listener;
+    let checkEvent = function(value) {
+      expect(listener).to.have.been.calledOnce;
       if (arguments.length > 0) {
-        expect(listener.calls.argsFor(0)[1]).toEqual(value);
-        expect(listener.calls.argsFor(0)[2]).toEqual(arguments[1] || {});
+        expect(listener).to.have.been.calledWith(picker, value, arguments[1] || {});
       } else {
-        expect(listener.calls.argsFor(0)[1]).toEqual({});
+        expect(listener).to.have.been.calledWith(picker, {});
+        expect(listener.calls.argsFor(0)[1]).to.eql({});
       }
     };
-    var checkListen = function(event) {
-      var listen = nativeBridge.calls({op: "listen", id: picker.cid});
-      expect(listen.length).toBe(1);
-      expect(listen[0].event).toBe(event);
-      expect(listen[0].listen).toBe(true);
+    let checkListen = function(event) {
+      let listen = nativeBridge.calls({op: "listen", id: picker.cid});
+      expect(listen.length).to.equal(1);
+      expect(listen[0].event).to.equal(event);
+      expect(listen[0].listen).to.equal(true);
     };
 
     beforeEach(function() {
-      listener = jasmine.createSpy();
+      listener = spy();
     });
 
     it("select", function() {
@@ -71,8 +82,8 @@ describe("Picker", function() {
 
       picker.set("selection", "foo", {custom: 23});
 
-      expect(listener).toHaveBeenCalledWith(picker, "foo", {custom: 23, index: 0});
-      expect(listener.calls.count()).toBe(1);
+      expect(listener).to.have.been.calledWith(picker, "foo", {custom: 23, index: 0});
+      expect(listener).to.have.been.calledOnce;
     });
 
     it("change:selectionIndex", function() {
@@ -95,30 +106,30 @@ describe("Picker", function() {
     describe("items", function() {
 
       it("initial value is empty array", function() {
-        expect(picker.get("items")).toEqual([]);
+        expect(picker.get("items")).to.eql([]);
       });
 
       it("initial value is a safe copy", function() {
         picker.get("items").push(23);
 
-        expect(picker.get("items")).toEqual([]);
+        expect(picker.get("items")).to.eql([]);
       });
 
       it("converts null to empty array", function() {
-        expect(picker.set("items", null).get("items")).toEqual([]);
+        expect(picker.set("items", null).get("items")).to.eql([]);
       });
 
       it("set SETs items property", function() {
         picker.set("items", ["a", "b", "c"]);
 
-        var call = nativeBridge.calls({op: "set", id: picker.cid})[0];
-        expect(call.properties).toEqual({items: ["a", "b", "c"]});
+        let call = nativeBridge.calls({op: "set", id: picker.cid})[0];
+        expect(call.properties).to.eql({items: ["a", "b", "c"]});
       });
 
       it("get does not GET from client", function() {
         picker.get("items");
 
-        expect(nativeBridge.calls({op: "get", id: picker.cid}).length).toBe(0);
+        expect(nativeBridge.calls({op: "get", id: picker.cid}).length).to.equal(0);
       });
 
     });
@@ -126,22 +137,22 @@ describe("Picker", function() {
     describe("itemText", function() {
 
       it("initial value is function", function() {
-        expect(picker.get("itemText")).toEqual(jasmine.any(Function));
+        expect(picker.get("itemText")).to.be.a("function");
       });
 
       it("initial function translates to string", function() {
-        var fn = picker.get("itemText");
+        let fn = picker.get("itemText");
 
-        expect(fn("foo")).toBe("foo");
-        expect(fn(23)).toBe("23");
-        expect(fn(null)).toBe("");
-        expect(fn()).toBe("");
+        expect(fn("foo")).to.equal("foo");
+        expect(fn(23)).to.equal("23");
+        expect(fn(null)).to.equal("");
+        expect(fn()).to.equal("");
       });
 
       it("does not SET property on client", function() {
         picker.set("itemText", function(item) { return item.name; });
 
-        expect(nativeBridge.calls({op: "set", id: picker.cid}).length).toBe(0);
+        expect(nativeBridge.calls({op: "set", id: picker.cid}).length).to.equal(0);
       });
 
     });
@@ -151,25 +162,25 @@ describe("Picker", function() {
       it("set SETs selectionIndex", function() {
         picker.set("selectionIndex", 23);
 
-        expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties).toEqual({selectionIndex: 23});
+        expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties).to.eql({selectionIndex: 23});
       });
 
     });
 
     it("get GETs selectionIndex", function() {
-      spyOn(nativeBridge, "get").and.returnValue(23);
+      stub(nativeBridge, "get").returns(23);
 
-      expect(picker.get("selectionIndex")).toBe(23);
-      expect(nativeBridge.get).toHaveBeenCalledWith(picker.cid, "selectionIndex");
+      expect(picker.get("selectionIndex")).to.equal(23);
+      expect(nativeBridge.get).to.have.been.calledWith(picker.cid, "selectionIndex");
     });
 
     describe("selection", function() {
 
       it("get returns items entry", function() {
         picker.set("items", ["foo", "bar"]);
-        spyOn(nativeBridge, "get").and.returnValue(1);
+        stub(nativeBridge, "get").returns(1);
 
-        expect(picker.get("selection")).toBe("bar");
+        expect(picker.get("selection")).to.equal("bar");
       });
 
       it("set SETs selectionIndex", function() {
@@ -177,25 +188,25 @@ describe("Picker", function() {
 
         picker.set("selection", "bar");
 
-        expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties.selectionIndex).toBe(1);
+        expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties.selectionIndex).to.equal(1);
       });
 
     });
 
     it("set together with items SETs selectionIndex", function() {
       picker.set({selection: "bar", items: ["foo", "bar"]});
-      expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties).toEqual({
+      expect(nativeBridge.calls({op: "set", id: picker.cid})[0].properties).to.eql({
         selectionIndex: 1,
         items: ["foo", "bar"]
       });
     });
 
     it("set with unknown value prints warning", function() {
-      spyOn(console, "warn");
+      spy(console, "warn");
       picker.set({selection: "bar2"});
 
-      expect(nativeBridge.calls({op: "set", id: picker.cid}).length).toBe(0);
-      expect(console.warn).toHaveBeenCalledWith("Could not set picker selection bar2: item not found");
+      expect(nativeBridge.calls({op: "set", id: picker.cid}).length).to.equal(0);
+      expect(console.warn).to.have.been.calledWith("Could not set picker selection bar2: item not found");
     });
 
   });
