740fdd98f28acc6b9a9f2375f33ae51b4bdb37d5
Christian Petrov <cpetrov@eclipsesource.com>
2016-09-12 10:05:22 +0200

    Test Properties as a module
    As part of the migration of tests to Mocha. Follow-up of @38c876d.
    
    Use ES6's Object.assign instead of the _.extend() utility method.
    
    Change-Id: I58f3ae71450deccde065cb4665dd5f2ef26aff32

--- test/tabris/Properties.test.js
+++ test/tabris/Properties.test.js
@@ -1,8 +1,11 @@
-describe("Properties", function() {
-  /*globals _:false*/
+import {expect, stub, restore} from "../test";
+import Properties from "../../src/tabris/Properties";
+import {types} from "../../src/tabris/property-types";
+import Events from "../../src/tabris/Events";
 
-  var object;
-  var TestType;
+describe("Properties", function() {
+  let object;
+  let TestType;
 
   beforeEach(function() {
     TestType = function() {};
@@ -11,40 +14,42 @@ describe("Properties", function() {
     object.toString = function() {
       return "TestType";
     };
-    _.extend(object, tabris.Properties);
+    Object.assign(object, Properties);
   });
 
+  afterEach(restore);
+
   it ("set returns self", function() {
-    expect(object.set("foo", "bar")).toBe(object);
+    expect(object.set("foo", "bar")).to.equal(object);
   });
 
   it ("set calls _checkDisposed", function() {
-    object._checkDisposed = jasmine.createSpy();
+    object._checkDisposed = stub();
 
     object.set("foo", "bar");
 
-    expect(object._checkDisposed).toHaveBeenCalled();
+    expect(object._checkDisposed).to.have.been.called;
   });
 
   it ("set stores single property", function() {
     object.set("foo", "bar");
-    expect(object.get("foo")).toBe("bar");
+    expect(object.get("foo")).to.equal("bar");
   });
 
   it ("set stores multiple properties", function() {
     object.set({foo: "bar", foo2: "bar2"});
-    expect(object.get("foo")).toBe("bar");
-    expect(object.get("foo2")).toBe("bar2");
+    expect(object.get("foo")).to.equal("bar");
+    expect(object.get("foo2")).to.equal("bar2");
   });
 
   it ("get returns undefined for unset property", function() {
-    expect(object.get("foo")).toBeUndefined();
+    expect(object.get("foo")).to.be.undefined;
   });
 
   it ("get returns default value if nothing was set", function() {
     TestType._properties.foo = {default: "bar"};
 
-    expect(object.get("foo")).toBe("bar");
+    expect(object.get("foo")).to.equal("bar");
   });
 
   it ("get does not return default value if something was set", function() {
@@ -52,62 +57,62 @@ describe("Properties", function() {
 
     object.set("foo", "something else");
 
-    expect(object.get("foo")).toBe("something else");
+    expect(object.get("foo")).to.equal("something else");
   });
 
   it("calls encoding function if present", function() {
     TestType._properties.knownProperty = {type: {
-      encode: jasmine.createSpy().and.returnValue(true)
+      encode: stub().returns(true)
     }};
-    spyOn(console, "warn");
+    stub(console, "warn");
 
     object.set("knownProperty", true);
 
-    expect(TestType._properties.knownProperty.type.encode).toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
+    expect(TestType._properties.knownProperty.type.encode).to.have.been.called;
+    expect(console.warn).to.have.not.been.called;
   });
 
   it("raises a warning if encoding function throws", function() {
     TestType._properties.knownProperty = {type: {
-      encode: jasmine.createSpy().and.throwError("My Error")
+      encode: stub().throws(new Error("My Error"))
     }};
-    spyOn(console, "warn");
+    stub(console, "warn");
 
     object.set("knownProperty", true);
 
-    var message = "TestType: Ignored unsupported value for property \"knownProperty\": My Error";
-    expect(console.warn).toHaveBeenCalledWith(message);
+    let message = "TestType: Ignored unsupported value for property \"knownProperty\": My Error";
+    expect(console.warn).to.have.been.calledWith(message);
   });
 
   it ("get returns value from decoding function if present", function() {
     TestType._properties.foo = {type: {
-      decode: jasmine.createSpy().and.returnValue("bar2")
+      decode: stub().returns("bar2")
     }};
 
     object.set("foo", "bar");
 
-    var value = object.get("foo");
+    let value = object.get("foo");
 
-    expect(TestType._properties.foo.type.decode).toHaveBeenCalledWith("bar");
-    expect(value).toBe("bar2");
+    expect(TestType._properties.foo.type.decode).to.have.been.calledWith("bar");
+    expect(value).to.equal("bar2");
   });
 
   describe("get", function() {
 
     it ("calls custom getter with property name", function() {
-      var getter = jasmine.createSpy();
+      let getter = stub();
       object.set("foo", "bar");
       TestType._properties.foo = {get: getter};
 
       object.get("foo");
 
-      expect(getter).toHaveBeenCalledWith("foo");
+      expect(getter).to.have.been.calledWith("foo");
     });
 
     it ("returns value from custom getter", function() {
-      TestType._properties.foo = {get: jasmine.createSpy().and.returnValue("bar")};
+      TestType._properties.foo = {get: stub().returns("bar")};
 
-      expect(object.get("foo")).toBe("bar");
+      expect(object.get("foo")).to.equal("bar");
     });
 
   });
@@ -115,37 +120,37 @@ describe("Properties", function() {
   describe("set", function() {
 
     it ("calls custom setter", function() {
-      var setter = jasmine.createSpy();
+      let setter = stub();
       TestType._properties.foo = {set: setter};
       object.set("foo", "bar");
 
-      expect(setter).toHaveBeenCalledWith("foo", "bar", {});
+      expect(setter).to.have.been.calledWith("foo", "bar", {});
     });
 
     it ("stores nothing if custom setter exists", function() {
       TestType._properties.foo = {set: function() {}};
       object.set("foo", "bar");
 
-      expect(object.get("foo")).toBe(undefined);
+      expect(object.get("foo")).to.equal(undefined);
     });
 
     it ("calls custom setter with options", function() {
-      var setter = jasmine.createSpy().and.returnValue(true);
+      let setter = stub().returns(true);
       TestType._properties.foo = {set: setter};
       object.set("foo", "bar", {foo2: "bar2"});
 
-      expect(setter).toHaveBeenCalledWith("foo", "bar", {foo2: "bar2"});
+      expect(setter).to.have.been.calledWith("foo", "bar", {foo2: "bar2"});
     });
 
   });
 
   describe("with Events:", function() {
 
-    var listener;
+    let listener;
 
     beforeEach(function() {
-      _.extend(object, tabris.Events);
-      listener = jasmine.createSpy();
+      Object.assign(object, Events);
+      listener = stub();
     });
 
     it ("set triggers change event", function() {
@@ -153,22 +158,18 @@ describe("Properties", function() {
 
       object.set("foo", "bar");
 
-      expect(listener).toHaveBeenCalled();
-      expect(listener.calls.argsFor(0)[0]).toBe(object);
-      expect(listener.calls.argsFor(0)[1]).toBe("bar");
-      expect(listener.calls.argsFor(0)[2]).toEqual({});
+      expect(listener).to.have.been.calledOnce;
+      expect(listener).to.have.been.calledWith(object, "bar", {});
     });
 
     it ("set triggers change event with decoded value", function() {
-      TestType._properties.foo = {type: tabris.PropertyTypes.boolean};
+      TestType._properties.foo = {type: types.boolean};
       object.on("change:foo", listener);
 
       object.set("foo", "bar");
 
-      expect(listener).toHaveBeenCalled();
-      expect(listener.calls.argsFor(0)[0]).toBe(object);
-      expect(listener.calls.argsFor(0)[1]).toBe(true);
-      expect(listener.calls.argsFor(0)[2]).toEqual({});
+      expect(listener).to.have.been.calledOnce;
+      expect(listener).to.have.been.calledWith(object, true, {});
     });
 
     it ("set (two parameters) triggers change event with options", function() {
@@ -176,10 +177,8 @@ describe("Properties", function() {
 
       object.set({foo: "bar"}, {foo2: "bar2"});
 
-      expect(listener).toHaveBeenCalled();
-      expect(listener.calls.argsFor(0)[0]).toBe(object);
-      expect(listener.calls.argsFor(0)[1]).toBe("bar");
-      expect(listener.calls.argsFor(0)[2]).toEqual({foo2: "bar2"});
+      expect(listener).to.have.been.calledOnce;
+      expect(listener).to.have.been.calledWith(object, "bar", {foo2: "bar2"});
     });
 
     it ("set (three parameters) triggers change event with options", function() {
@@ -187,10 +186,8 @@ describe("Properties", function() {
 
       object.set("foo", "bar", {foo2: "bar2"});
 
-      expect(listener).toHaveBeenCalled();
-      expect(listener.calls.argsFor(0)[0]).toBe(object);
-      expect(listener.calls.argsFor(0)[1]).toBe("bar");
-      expect(listener.calls.argsFor(0)[2]).toEqual({foo2: "bar2"});
+      expect(listener).to.have.been.calledOnce;
+      expect(listener).to.have.been.calledWith(object, "bar", {foo2: "bar2"});
     });
 
     it ("set triggers no change event if value is unchanged", function() {
@@ -199,17 +196,17 @@ describe("Properties", function() {
 
       object.set("foo", "bar");
 
-      expect(listener).not.toHaveBeenCalled();
+      expect(listener).to.have.not.been.called;
     });
 
     it ("set triggers no change event if encoded value is unchanged", function() {
-      TestType._properties.foo = {type: tabris.PropertyTypes.boolean};
+      TestType._properties.foo = {type: types.boolean};
       object.set("foo", true);
       object.on("change:foo", listener);
 
       object.set("foo", "bar");
 
-      expect(listener).not.toHaveBeenCalled();
+      expect(listener).to.have.not.been.called;
     });
 
   });
