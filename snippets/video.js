58440c62e74d145d40ba7ec53aeef0ec448f38b4
Christian Petrov <cpetrov@eclipsesource.com>
2016-09-27 17:07:03 +0200

    Improve video snippet
    Make video fit the screen. Add play/pause button.
    
    Change-Id: I3261a2cbb9d56d746bc1495b00cd45f2ddd56682

--- snippets/video.js
+++ snippets/video.js
@@ -1,12 +1,15 @@
-var video = new tabris.Video({
-  left: 16, top: 0, width: 500, height: 500,
+let video = new tabris.Video({
+  left: 0, top: 0, right: 0, bottom: '#button 16',
   url: 'http://peach.themazzone.com/durian/movies/sintel-1280-stereo.mp4',
   controlsVisible: false
+}).on('change:state', function(widget, state) {
+  button.set('text', state !== 'pause' ? '❚❚' : '▶');
 }).appendTo(tabris.ui.contentView);
 
-new tabris.Button({
-  text: 'Play/Pause',
-  left: 16, top: 'prev() 16'
+let button = new tabris.Button({
+  id: 'button',
+  centerX: 0, bottom: 16,
+  text: '❚❚'
 }).on('select', function() {
   video.get('state') === 'play' ? video.pause() : video.play();
 }).appendTo(tabris.ui.contentView);
