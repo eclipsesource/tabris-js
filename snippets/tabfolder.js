27211a8062734443e83a643b6d7875dedb1e2f53
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:54:14 +0100

    Modernize tabfolder snippet
    Don't use utilize the layoutData property, which is not encouraged in
    Tabris.js 2.x anymore.
    
    Change-Id: Idc1023b5f0ef7b2aa5f226af5ef296191a49a1ec

--- snippets/tabfolder.js
+++ snippets/tabfolder.js
@@ -1,7 +1,7 @@
 // Create a swipe enabled tab folder with 3 tabs
 
 var tabFolder = new tabris.TabFolder({
-  layoutData: {left: 0, top: 0, right: 0, bottom: 0},
+  left: 0, top: 0, right: 0, bottom: 0,
   paging: true // enables swiping. To still be able to open the developer console in iOS, swipe from the bottom right.
 }).appendTo(tabris.ui.contentView);
 
@@ -12,7 +12,7 @@ var createTab = function(title, image, seletedImage) {
     selectedImage: {src: seletedImage, scale: 2}
   }).appendTo(tabFolder);
   new tabris.TextView({
-    layoutData: {centerX: 0, centerY: 0},
+    centerX: 0, centerY: 0,
     text: 'Content of Tab ' + title
   }).appendTo(tab);
 };
