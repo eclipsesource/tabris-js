27565528b81b0690ad3654d59725ae578bb16f41
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:16:43 +0100

    Modernize widget-touch snippet
    Don't use practices, which are not encouraged in Tabris 2.x anymore,
    e.g. using property setters/getters instead of fields and utilizing the
    layoutData property.
    
    Change-Id: I38e014b79c7b6fc30a2f1957b426152e31ddb7eb

--- snippets/widget-touch.js
+++ snippets/widget-touch.js
@@ -1,24 +1,24 @@
 var textView = new tabris.TextView({
-  layoutData: {left: 20, top: 20, right: 20},
+  left: 20, top: 20, right: 20,
   text: 'Touch anywhere...'
 }).appendTo(tabris.ui.contentView);
 
 var printXY = function(prefix, event) {
-  textView.set('text', prefix + ': ' + Math.round(event.touches[0].x) + ' X ' + Math.round(event.touches[0].y));
+  textView.text = prefix + ': ' + Math.round(event.touches[0].x) + ' X ' + Math.round(event.touches[0].y);
 };
 
 tabris.ui.contentView.on('touchstart', function(widget, event) {
   printXY('touchstart', event);
-  tabris.ui.contentView.set('background', 'yellow');
+  tabris.ui.contentView.background = 'yellow';
 }).on('touchmove', function(widget, event) {
   printXY('touchmove', event);
 }).on('touchend', function(widget, event) {
   printXY('touchend', event);
-  tabris.ui.contentView.set('background', 'green');
+  tabris.ui.contentView.background = 'green';
 }).on('touchcancel', function(widget, event) {
   printXY('touchcancel', event);
-  tabris.ui.contentView.set('background', 'red');
+  tabris.ui.contentView.background = 'red';
 }).on('longpress', function(widget, event) {
-  tabris.ui.contentView.set('background', 'blue');
+  tabris.ui.contentView.background = 'blue';
   printXY('longpress'  , event);
 });
