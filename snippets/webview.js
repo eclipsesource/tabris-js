fc9969e14ee9c319450ac603f0f448c9565ed0d5
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:21:24 +0100

    Modernize webview snippet
    Don't use practices, which are not encouraged in Tabris 2.x anymore,
    e.g. using property setters/getters instead of fields and utilizing the
    layoutData property.
    
    Change-Id: Ief093f2a663da5fc8049588155a8f962196d9dbb

--- snippets/webview.js
+++ snippets/webview.js
@@ -1,7 +1,7 @@
 // Create a web view to show a web page
 
 var urlInput = new tabris.TextInput({
-  layoutData: {left: 8, right: 8, top: 8},
+  left: 8, right: 8, top: 8,
   message: 'Enter URL...',
   text: 'http://en.wikipedia.org'
 }).on('accept', loadUrl).appendTo(tabris.ui.contentView);
@@ -11,7 +11,7 @@ var webview = new tabris.WebView({
 }).appendTo(tabris.ui.contentView);
 
 function loadUrl() {
-  webview.set('url', urlInput.text);
+  webview.url = urlInput.text;
 }
 
 loadUrl();
