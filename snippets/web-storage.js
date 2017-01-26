ee380077005260080db06fcd8cbaf1f309140542
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:22:30 +0100

    Modernize webstorage snippet
    Don't use the layoutData property, which is not encouraged in Tabris.js
    2.x anymore.
    
    Change-Id: Ie37956f4a0b9c7424b0c7eec515bfa0917139c0e

--- snippets/web-storage.js
+++ snippets/web-storage.js
@@ -3,7 +3,7 @@ var KEY = 'localStorageSnippetCount';
 var startCount = parseInt(localStorage.getItem(KEY) || '0') + 1;
 localStorage.setItem(KEY, startCount.toString());
 new tabris.TextView({
-  layoutData: {left: 10, right: 10, centerY: 0},
+  left: 10, right: 10, centerY: 0,
   alignment: 'center',
   font: '22px sans-serif',
   text: 'This application was started ' + startCount + ' time(s).'
