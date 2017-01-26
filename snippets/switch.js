41b41b33ae0e7bcb6c781cee949730d7f96e9c2a
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:56:58 +0100

    Modernize switch snippet
    Don't use practices, which are not encouraged in Tabris 2.x anymore,
    e.g. using property setters/getters instead of fields and utilizing the
    layoutData property.
    
    Change-Id: I6fe91f0b9c24f69fa250921a63e58e722678d2c6

--- snippets/switch.js
+++ snippets/switch.js
@@ -3,23 +3,23 @@
 var MARGIN = 16;
 
 new tabris.Switch({
+  left: MARGIN, top: MARGIN,
   id: 'switch',
-  selection: true,
-  layoutData: {left: MARGIN, top: MARGIN}
+  selection: true
 }).on('change:selection', function(widget, selection) {
-  tabris.ui.contentView.find('#stateView').set('text', selection ? 'State: checked' : 'State: unchecked');
+  tabris.ui.contentView.find('#stateView').first().text = selection ? 'State: checked' : 'State: unchecked';
 }).appendTo(tabris.ui.contentView);
 
 new tabris.TextView({
+  left: ['#switch', MARGIN], baseline: '#switch',
   id: 'stateView',
-  text: 'State: checked',
-  layoutData: {left: ['#switch', MARGIN], baseline: '#switch'}
+  text: 'State: checked'
 }).appendTo(tabris.ui.contentView);
 
 new tabris.Button({
-  text: 'Toggle Switch',
-  layoutData: {left: MARGIN, top: ['#switch', MARGIN]}
+  left: MARGIN, top: ['#switch', MARGIN],
+  text: 'Toggle Switch'
 }).on('select', function() {
   var checked = tabris.ui.contentView.find('#switch').first().selection;
-  tabris.ui.contentView.find('#switch').set('selection', !checked);
+  tabris.ui.contentView.find('#switch').first().selection = !checked;
 }).appendTo(tabris.ui.contentView);
