33a39380397d971b6f4b0d4b54d185b439bf635f
Christian Petrov <cpetrov@eclipsesource.com>
2017-01-26 14:58:33 +0100

    Modernize slider snippet
    Don't use practices, which are not encouraged in Tabris 2.x anymore,
    e.g. using property setters/getters instead of fields and utilizing the
    layoutData property.
    
    Change-Id: I1108fe7edc17c167a639c5bdc5f6b0a660c379bc

--- snippets/slider.js
+++ snippets/slider.js
@@ -1,17 +1,17 @@
 // Create a slider with a selection handler
 
 var textView = new tabris.TextView({
-  layoutData: {left: 10, right: 10, top: '30%'},
+  left: 10, right: 10, top: '30%',
   alignment: 'center',
   font: '22px sans-serif',
   text: '50'
 }).appendTo(tabris.ui.contentView);
 
 new tabris.Slider({
-  layoutData: {left: 50, top: [textView, 20], right: 50},
+  left: 50, top: [textView, 20], right: 50,
   minimum: -50,
   selection: 50,
   maximum: 150
 }).on('change:selection', function(slider, selection) {
-  textView.set('text', selection);
+  textView.text = selection;
 }).appendTo(tabris.ui.contentView);
