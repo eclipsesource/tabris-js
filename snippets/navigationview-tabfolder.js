892d2069fd9d6b964439df753851c82c39b5742c
Moritz Post <mpost@eclipsesource.com>
2017-01-30 12:36:17 +0100

    Add snippet navigationview-tabfolder.js
    Change-Id: Id615e393f849aa0c5742795b9865c3ad99ae16e2

---
+++ snippets/navigationview-tabfolder.js
@@ -0,0 +1,60 @@
+// demonstrates NavigationViews as children of a TabFolder
+
+var tabFolder = new tabris.TabFolder({
+  left: 0, top: 0, right: 0, bottom: 0,
+  paging: true
+}).appendTo(tabris.ui.contentView);
+
+function createTab(title, image) {
+  var tab = new tabris.Tab({
+    title: title,
+    image: {src: image, scale: 2}
+  }).appendTo(tabFolder);
+  var navigationView = new tabris.NavigationView({
+    id: 'navigationView',
+    left: 0, top: 0, right: 0, bottom: 0,
+    toolbarVisible: tabris.device.platform !== 'Android' // hide toolbar on Android for more native appearance
+  }).appendTo(tab);
+  createPage(navigationView);
+}
+
+function createPage(navigationView) {
+  var text = 'Page ' + (navigationView.pages().length + 1);
+  var page = new tabris.Page({
+    title: text,
+    background: '#eeeeee'
+  }).appendTo(navigationView);
+  var controls = new tabris.Composite({
+    centerX: 0, centerY: 0
+  }).appendTo(page);
+  new tabris.TextView({
+    centerX: 0,
+    text
+  }).appendTo(controls);
+  new tabris.Button({
+    top: 'prev() 16', centerX: 0,
+    text: 'Add Page'
+  }).on('select', function() {
+    createPage(navigationView);
+  }).appendTo(controls);
+  new tabris.Button({
+    top: 'prev() 16', centerX: 0,
+    text: 'Remove Page'
+  }).on('select', function() {
+    page.dispose();
+  }).appendTo(controls);
+}
+
+createTab('Cart', 'images/cart.png');
+createTab('Pay', 'images/card.png');
+createTab('Statistic', 'images/chart.png');
+
+tabris.app.on('backnavigation', function(app, event) {
+  // handle the "physical" back button on Android
+  var navigationView = tabFolder.selection.find('#navigationView').first();
+  var page = navigationView.pages().last();
+  if (page !== undefined) {
+    page.dispose();
+    event.preventDefault();
+  }
+});
