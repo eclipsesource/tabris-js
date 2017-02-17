a6bec8b01f666baf66561178df349337e35aca7a
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-02-17 16:41:25 +0100

    Update documentation of event-related methods
    Adjust the documentation of `on`, `once`, and `off` to the new API,
    i.e. only a single event object is passed to listeners. Rename the
    first parameter `event` to `type` to avoid confusion with the event
    object. `type` is the term used in the DOM events documentation [1].
    
    Also change the documentation for `trigger` and declare only a single
    parameter. Although the trigger method still passes all parameters to
    listeners, we should not advertise this anymore.
    
    The listen function should be adjusted in a separate change.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
    
    Change-Id: Ibe20a78a2d425e12b6f4c746b28523e14bbe344b

--- test/typescript/NativeObject.test.ts
+++ test/typescript/NativeObject.test.ts
@@ -10,21 +10,18 @@ nativeObject.cid = cid;
 cid = nativeObject.cid;
 
 // Methods
-let event: string;
+let type: string;
+let listener: (event: Object) => void;
+let context: NativeObject;
 let property: string;
 let properties: Object;
-let listener: Function;
-let context: NativeObject;
 let thisReturnValue: NativeObject;
-let anyReturnValue: any;
-let paramA: any;
-let paramB: any;
 let value: any;
 
-thisReturnValue = nativeObject.on(event, listener, context);
-thisReturnValue = nativeObject.once(event, listener, context);
-thisReturnValue = nativeObject.off(event, listener, context);
-thisReturnValue = nativeObject.trigger(event, paramA, paramB);
+thisReturnValue = nativeObject.on(type, listener, context);
+thisReturnValue = nativeObject.once(type, listener, context);
+thisReturnValue = nativeObject.off(type, listener, context);
+thisReturnValue = nativeObject.trigger(type, type);
 thisReturnValue = nativeObject.get(property);
 thisReturnValue = nativeObject.set(property, value);
 thisReturnValue = nativeObject.set(properties);
