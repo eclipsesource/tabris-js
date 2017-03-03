0a8ed580deb7e2438d974521bc26a1636f1f6eca
Cam Stuart <cam.asoftware@gmail.com>
2017-03-03 13:13:05 +1100

    Updated syntax to ES6

--- examples/hello/hello.js
+++ examples/hello/hello.js
@@ -1,13 +1,15 @@
-var button = new tabris.Button({
+const {Button, TextView} = require('tabris');
+
+const button = new Button({
   centerX: 0, top: 100,
   text: 'Show message'
 }).appendTo(tabris.ui.contentView);
 
-var textView = new tabris.TextView({
+const textView = new TextView({
   centerX: 0, top: [button, 50],
   font: '24px'
 }).appendTo(tabris.ui.contentView);
 
-button.on('select', function() {
+button.on('select', () => {
   textView.text = 'Tabris.js rocks!';
 });
