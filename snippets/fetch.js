f2e116e0871e5acca6195f4473b8a51a61a16ae3
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-03-06 17:50:53 +0100

    Add a fetch snippet
    Add a small snippet to demonstrate the use of fetch.
    
    Change-Id: I21ac5ddada769925cc1d502ea29ffd0a90493605

---
+++ snippets/fetch.js
@@ -0,0 +1,27 @@
+// Download HTTP content using the fetch API
+// See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+const {Button, TextView, ui} = require('tabris');
+
+new Button({
+  left: 16, right: 16, top: 'prev() 12',
+  text: 'load'
+}).on('select', loadData).appendTo(ui.contentView);
+
+let textView = new TextView({
+  left: 16, right: 16, top: 'prev() 12'
+}).appendTo(ui.contentView);
+
+function loadData() {
+  fetch('https://freegeoip.net/json/')
+    .then(response => checkStatus(response) && response.json())
+    .then(json => textView.text = 'You appear to be in ' + json.city)
+    .catch(err => console.error(err)); // Never forget the final catch!
+}
+
+function checkStatus(response) {
+  if (!response.ok) {
+    throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+  }
+  return response;
+}
