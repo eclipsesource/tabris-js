4ea8b16073a67bc705d2c4854f086ae721d59c20
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-03-06 15:22:46 +0100

    Simplify localStorage example, move to snippets
    The local-storage example only stores a string in the localStorage.
    That does not make an example app, it should rather be a snippet.
    
    Move it to snippets and remove clutter.
    
    Change-Id: Ic966a6cf95f4f7897457d12bc2105cad5c3d26b6

---
+++ snippets/local-storage.js
@@ -0,0 +1,39 @@
+// Tabris.js implements a subset of the W3C Web Storage Recommendation.
+// See https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+const {Button, TextInput, TextView, ui, localStorage} = require('tabris');
+
+const KEY = 'snippet-value';
+
+let valueLabel = new TextView({
+  left: 32, top: 32,
+  text: 'Value:'
+}).appendTo(ui.contentView);
+
+let valueField = new TextInput({
+  left: 'prev() 12', baseline: valueLabel, right: 32,
+  text: localStorage.getItem(KEY) || 'Hello world!'
+}).appendTo(ui.contentView);
+
+new Button({
+  left: 24, right: '66%', top: [valueLabel, 24],
+  text: 'Set'
+}).on('select', () => {
+  localStorage.setItem(KEY, valueField.text);
+  valueField.text = '';
+}).appendTo(ui.contentView);
+
+new Button({
+  left: '33% 12', right: '33% 12', top: [valueLabel, 24],
+  text: 'Get'
+}).on('select', () => {
+  valueField.text = localStorage.getItem(KEY) || '';
+}).appendTo(ui.contentView);
+
+new Button({
+  left: '66%', right: 24, top: [valueLabel, 24],
+  text: 'Remove'
+}).on('select', () => {
+  localStorage.removeItem(KEY);
+  valueField.text = '';
+}).appendTo(ui.contentView);
