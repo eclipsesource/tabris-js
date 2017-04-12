3a2a1e74a0cd8ec71b460a82ec0223cca2d9918f
Moritz Post <mpost@eclipsesource.com>
2017-04-12 09:55:59 +0200

    Modernize webview.js snippet
    The change removes the "layoutData" property in favor of direct property
    access and renames some expressions.

--- snippets/webview.js
+++ snippets/webview.js
@@ -1,17 +1,18 @@
 // Create a web view to show a web page
 
-var urlInput = new tabris.TextInput({
+let urlInput = new tabris.TextInput({
   left: 8, right: 8, top: 8,
   message: 'Enter URL...',
   text: 'http://en.wikipedia.org'
-}).on('accept', loadUrl).appendTo(tabris.ui.contentView);
+}).on('accept', loadUrl)
+  .appendTo(tabris.ui.contentView);
 
-var webview = new tabris.WebView({
-  layoutData: {left: 0, top: [urlInput, 8], right: 0, bottom: 0}
+let webView = new tabris.WebView({
+  left: 0, top: 'prev() 8', right: 0, bottom: 0
 }).appendTo(tabris.ui.contentView);
 
 function loadUrl() {
-  webview.url = urlInput.text;
+  webView.url = urlInput.text;
 }
 
 loadUrl();
