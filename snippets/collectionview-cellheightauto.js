78bd2bf14d6473b6e392315257d20a6bbfea6b72
Moritz Post <mpost@eclipsesource.com>
2017-04-26 12:11:08 +0200

    Add snippet collectionview-cellheightauto.js
    The snippet shows the behavior of 'auto' cellHeight.
    
    Change-Id: Ibc6a74dc36618b909e62c9ac903df4b540aba745

---
+++ snippets/collectionview-cellheightauto.js
@@ -0,0 +1,34 @@
+const MARGIN = 16;
+const TEXT = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem' +
+  ' accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab' +
+  ' illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.' +
+  ' Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,' +
+  ' sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.' +
+  ' Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,' +
+  ' adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et' +
+  ' dolore magnam aliquam quaerat voluptatem.';
+
+const items = [];
+
+for (let i = 0; i < 30; i++) {
+  items[i] = TEXT.substring(0, (i + 1) * 50);
+}
+
+new tabris.CollectionView({
+  left: 0, top: 0, right: 0, bottom: '#controlsComposite',
+  itemCount: items.length,
+  createCell: () => {
+    const composite = new tabris.Composite();
+    new tabris.ImageView({
+      id: 'imageView',
+      left: MARGIN, top: MARGIN,
+      image: {src: 'images/arrow-forward-black-24dp@3x.png', scale: 3}
+    }).appendTo(composite);
+    new tabris.TextView({
+      id: 'textView',
+      left: ['#imageView', MARGIN], top: MARGIN, right: MARGIN
+    }).appendTo(composite);
+    return composite;
+  },
+  updateCell: (cell, index) => cell.find('#textView').set('text', items[index])
+}).appendTo(tabris.ui.contentView);
