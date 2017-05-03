1e338bad1cb4f5788ac8e4ee1702356280128c20
Moritz Post <mpost@eclipsesource.com>
2017-05-03 16:34:26 +0200

    Update gallery example
    - Update code to be in line with Tabris.js 2
    - Reduce number and size of images
    - Create auto hiding filmstrip
    
    Change-Id: If61e6580b695618f50fc8ef820c2d09ecfb96c9a

--- examples/gallery/gallery.js
+++ examples/gallery/gallery.js
@@ -1,49 +1,69 @@
-var imageNames = require('./images/index.json');
+const IMAGES = require('./images/index.json');
+const IMAGE_SIZE = 96;
+
+class FilmStrip extends tabris.ScrollView {
+
+  constructor(properties) {
+    super(properties);
+    this.resetHideTimeout();
+  }
+
+  isShowing() {
+    return this.transform.translationY === 0;
+  }
+
+  show() {
+    this.animate({transform: {translationY: 0}});
+  }
+
+  hide() {
+    this.animate({transform: {translationY: this.bounds.height}});
+  }
+
+  toggleShowing() {
+    if (this.isShowing()) {
+      this.hide();
+    } else {
+      this.show();
+      this.resetHideTimeout();
+    }
+  }
+
+  resetHideTimeout() {
+    clearTimeout(this._timeout);
+    this._timeout = setTimeout(() => filmStrip.hide(), 5000);
+  }
+}
 
 tabris.ui.contentView.background = 'black';
-
-var navigationView = new tabris.NavigationView({
-  left: 0, top: 0, right: 0, bottom: 0,
-}).appendTo(tabris.ui.contentView);
-
-var fullImage = new tabris.ImageView({
-  top: 0, bottom: 0, left: 0, right: 0,
-  image: {src: 'images/' + imageNames[0] + '.jpg'},
-  scaleMode: 'auto'
-}).appendTo(tabris.ui.contentView);
-
-var scrollView = new tabris.ScrollView({
-  left: 0, right: 0, bottom: 0, height: 164,
-  direction: 'horizontal',
-  background: 'rgba(32, 32, 32, 0.6)'
-}).appendTo(tabris.ui.contentView);
-
-imageNames.forEach(function(image, index) {
-  new tabris.ImageView({
-    top: 7, left: index * 157, width: 150, height: 150,
-    image: {src: 'images/' + image + '_thumb.jpg', width: 150, height: 150},
-    highlightOnTouch: true
-  }).on('tap', function() {
-    fullImage.image = {src: 'images/' + image + '.jpg'};
-  }).appendTo(scrollView);
+tabris.ui.statusBar.set({
+  displayMode: 'float',
+  theme: 'dark',
+  background: '#00000044'
 });
 
-var fullscreenAction = new tabris.Action({
-  title: 'Fullscreen',
-  placementPriority: 'high'
-}).on('select', toggleAction)
-  .appendTo(navigationView);
+const fullImage = new tabris.ImageView({
+  top: 0, bottom: 0, left: 0, right: 0,
+  image: {src: `images/${IMAGES[0]}`},
+  scaleMode: 'fill'
+}).on('tap', () => filmStrip.toggleShowing())
+  .appendTo(tabris.ui.contentView);
 
-var thumbnailsAction = new tabris.Action({
-  title: 'Thumbnails',
-  placementPriority: 'high',
-  visible: false
-}).on('select', toggleAction)
-  .appendTo(navigationView);
+const filmStrip = new FilmStrip({
+  left: 0, right: 0, bottom: 0, height: 112,
+  direction: 'horizontal',
+  background: '#00000044'
+}).on('scrollX', () => filmStrip.resetHideTimeout())
+  .appendTo(tabris.ui.contentView);
 
-function toggleAction() {
-  var wasFullscreen = !scrollView.visible;
-  scrollView.visible = wasFullscreen;
-  thumbnailsAction.visible = !wasFullscreen;
-  fullscreenAction.visible = wasFullscreen;
-}
+IMAGES.forEach((image) => {
+  new tabris.ImageView({
+    top: 8, left: 'prev() 8', width: IMAGE_SIZE, height: IMAGE_SIZE,
+    image: {src: `images/${image}`, width: IMAGE_SIZE, height: IMAGE_SIZE},
+    scaleMode: 'fill',
+    highlightOnTouch: true
+  }).on('tap', function() {
+    fullImage.image = {src: `images/${image}`};
+    filmStrip.resetHideTimeout();
+  }).appendTo(filmStrip);
+});
