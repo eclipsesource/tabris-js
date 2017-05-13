7a1ec36d6637a2f90f5767472c3a5ae038f097f2
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-05-13 11:36:24 +0200

    Improve animated canvas example
    Reverse the wheel's rotation direction to let the wheel "create"
    the sine curve instead of swallowing it.
    
    Animate the x axis ticks to mark the correct positions of pi and 2pi.
    
    Update to ES6.
    
    Change-Id: I7c01c79a60410d83088e4588ee8355239d45523f

--- examples/canvas/animation.js
+++ examples/canvas/animation.js
@@ -1,17 +1,17 @@
-var example = new Example();
+let example = new Example();
 
-var page = module.exports = new tabris.Page({
+let page = module.exports = new tabris.Page({
   title: 'Animation',
   autoDispose: false
-}).on('disappear', function() {
+}).on('disappear', () => {
   page.children('#animateCheckBox').set('checked', false);
 });
 
 new tabris.Canvas({
   left: 10, top: 10, right: 10, bottom: '#animateCheckBox 10'
 }).on('resize', function({target, width, height}) {
-  var contextHeight = Math.min(height, Math.floor(width / 2));
-  var ctx = target.getContext('2d', width, contextHeight);
+  let contextHeight = Math.min(height, Math.floor(width / 2));
+  let ctx = target.getContext('2d', width, contextHeight);
   example.init(ctx);
 }).appendTo(page);
 
@@ -19,15 +19,15 @@ new tabris.CheckBox({
   centerX: 0, bottom: 10,
   text: 'Animate',
   id: 'animateCheckBox'
-}).on('checkedChanged', function({value: checked}) {
+}).on('checkedChanged', ({value: checked}) => {
   example.setRunning(checked);
 }).appendTo(page);
 
 function Example() {
-  var ctx, width, height, cx, cy, unit;
-  var angle = 0;
-  var timerId = null;
-  var running = false;
+  let ctx, width, height, cx, cy, unit;
+  let angle = 0;
+  let timerId = null;
+  let running = false;
 
   this.init = function(newCtx) {
     ctx = newCtx;
@@ -65,13 +65,13 @@ function Example() {
 
   function draw() {
     clear();
-    drawAxes();
+    drawAxes(angle);
     drawSine(angle);
     drawCircle(angle);
     drawLever(angle);
     drawFpsLabel();
     speedometer.update();
-    angle += Math.PI / 90;
+    angle = normalizeAngle(angle - Math.PI / 90);
     // re-schedule
     if (running) {
       timerId = setTimeout(draw, 0);
@@ -82,7 +82,13 @@ function Example() {
     ctx.clearRect(0, 0, width, height);
   }
 
-  function drawAxes() {
+  function normalizeAngle(angle) {
+    return angle > Math.PI * 2 ? angle - Math.PI * 2 : angle < 0 ? angle + Math.PI * 2 : angle;
+  }
+
+  function drawAxes(angle) {
+    let p = normalizeAngle(Math.PI * 2 - angle);
+    let p2 = normalizeAngle(Math.PI - angle);
     ctx.strokeStyle = '#aaa';
     ctx.lineWidth = 1;
     ctx.beginPath();
@@ -92,36 +98,38 @@ function Example() {
     ctx.moveTo(cx, cy - 1.5 * unit);
     ctx.lineTo(cx, cy + 1.5 * unit);
     // x axis ticks
-    ctx.moveTo(cx + Math.PI * unit, cy - 5);
-    ctx.lineTo(cx + Math.PI * unit, cy + 5);
-    ctx.moveTo(cx + 2 * Math.PI * unit, cy - 5);
-    ctx.lineTo(cx + 2 * Math.PI * unit, cy + 5);
+    ctx.moveTo(cx + p * unit, cy - 5);
+    ctx.lineTo(cx + p * unit, cy + 5);
+    ctx.moveTo(cx + p2 * unit, cy - 5);
+    ctx.lineTo(cx + p2 * unit, cy + 5);
     // y axis ticks
     ctx.moveTo(cx - 5, cy + unit);
     ctx.lineTo(cx + 5, cy + unit);
     ctx.moveTo(cx - 5, cy - unit);
     ctx.lineTo(cx + 5, cy - unit);
     ctx.stroke();
+    // x axis labels
     ctx.fillStyle = '#aaa';
     ctx.font = '12px sans-serif';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'top';
-    ctx.fillText('π', cx + Math.PI * unit, cy + 8);
-    ctx.fillText('2π', cx + 2 * Math.PI * unit, cy + 8);
+    ctx.fillText('π', cx + p * unit, cy + 8);
+    ctx.fillText('2π', cx + p2 * unit, cy + 8);
+    // y axis labels
     ctx.textAlign = 'left';
     ctx.textBaseline = 'middle';
     ctx.fillText('1', cx + 8, cy - unit);
     ctx.fillText('-1', cx + 8, cy + unit);
   }
 
-  function drawSine(t) {
-    var x, y;
-    var steps = 50;
+  function drawSine(angle) {
+    let x, y;
+    let steps = 50;
     ctx.beginPath();
-    ctx.moveTo(cx, cy + Math.sin(t) * unit);
-    for (var i = 0; i <= steps; i++) {
+    ctx.moveTo(cx, cy + Math.sin(angle) * unit);
+    for (let i = 0; i <= steps; i++) {
       x = i * 2 * Math.PI / steps;
-      y = Math.sin(t + x);
+      y = Math.sin(angle + x);
       ctx.lineTo(cx + x * unit, cy + y * unit);
     }
     ctx.strokeStyle = '#fa0';
@@ -130,7 +138,7 @@ function Example() {
   }
 
   function drawCircle() {
-    var ccx = cx - 1.5 * unit;
+    let ccx = cx - 1.5 * unit;
     ctx.strokeStyle = '#0af';
     ctx.lineWidth = 2;
     ctx.beginPath();
@@ -139,9 +147,9 @@ function Example() {
   }
 
   function drawLever(t) {
-    var ccx = cx - 1.5 * unit;
-    var x = ccx + Math.cos(t) * unit;
-    var y = cy + Math.sin(t) * unit;
+    let ccx = cx - 1.5 * unit;
+    let x = ccx + Math.cos(t) * unit;
+    let y = cy + Math.sin(t) * unit;
     ctx.strokeStyle = '#0af';
     ctx.fillStyle = '#fff';
     ctx.lineWidth = 2;
@@ -155,7 +163,7 @@ function Example() {
     // hinges
     ctx.lineWidth = 1;
     ctx.beginPath();
-    var radius = 3;
+    let radius = 3;
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.moveTo(cx + radius / 2, y);
     ctx.arc(cx, y, radius, 0, 2 * Math.PI);
@@ -168,17 +176,16 @@ function Example() {
     ctx.textBaseline = 'top';
     ctx.fillStyle = '#000';
     ctx.lineWidth = 1;
-    // TODO concat only needed on Rhino
-    ctx.fillText('FPS: '.concat(speedometer.fps.toFixed(1)), 10, 10);
+    ctx.fillText('FPS: ' + speedometer.fps.toFixed(1), 10, 10);
   }
 
-  var speedometer = {
+  let speedometer = {
     start: 0,
     count: 0,
     fps: 0,
     update: function() {
-      var now = Date.now();
-      var time = now - this.start;
+      let now = Date.now();
+      let time = now - this.start;
       this.count++;
       if (this.start === 0) {
         this.start = now;
