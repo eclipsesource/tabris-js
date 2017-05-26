99fd57b17519e4fa73b874e96f72683e65a61cab
Christian Petrov <cpetrov@eclipsesource.com>
2017-05-26 09:42:52 +0200

    Modernize parallax example
      - Destructure tabris import to make the code more compact and to
        avoid using the global tabris object.
      - Use let instead of var.
      - Use const instead of var where sensible.
      - Prefer lambda functions over anonymous functions.
      - Extract texts to constants to make code easier to read.
      - Don't use markupEnabled for the single purpose of making text bold.
        Use bold font weight instead.
      - Inline MARGIN constants, since they may introduce unnecessary
        complexity in the scope of examples and snippets where layouts
        don't have to be exchangeable and the variable name is much longer
        than the primitive number itself.
    
    Change-Id: I4d399560777cc3a2466771f85b90bc4c95c9ec1d

--- examples/parallax/parallax.js
+++ examples/parallax/parallax.js
@@ -1,80 +1,80 @@
-var MARGIN_SMALL = 14;
-var MARGIN = 16;
+const {Composite, ImageView, TextView, ScrollView, ui} = require('tabris');
 
-var INITIAL_TITLE_COMPOSITE_OPACITY = 0.85;
+const INITIAL_TITLE_COMPOSITE_OPACITY = 0.85;
+const TITLE = 'INDIAN SUMMER SALAD';
+const SUBTITLE = 'The perfect side dish';
+const RECIPE = 'Make a dressing of the yolks of 3 hard-boiled eggs pounded fine, equal ' +
+  'quantities of mustard and paprika, a pinch of powdered sugar, 4 ' +
+  'tablespoonfuls of oil, 2 tablespoonfuls of vinegar. Simmer over the fire, ' +
+  'but do not allow to boil. Take the white meat of two chickens, and separate ' +
+  'into flakes; pile it in the middle of a dish, and pour the dressing over ' +
+  'it. Cut up two heads of lettuce, and arrange around the chicken. On top of ' +
+  'the lettuce place the whites of the eggs, cut into rings, and lay so as to ' +
+  'form a chain.';
 
-var titleCompY = 0;
+let titleCompY = 0;
 
-tabris.ui.statusBar.background = rgba(255, 152, 0, 1);
+ui.statusBar.background = rgba(255, 152, 0, 1);
 
-var scrollView = new tabris.ScrollView({
+let scrollView = new ScrollView({
   left: 0, right: 0, top: 0, bottom: 0
-}).appendTo(tabris.ui.contentView);
+}).appendTo(ui.contentView);
 
-var imageView = new tabris.ImageView({
+let imageView = new ImageView({
   left: 0, top: 0, right: 0,
   image: 'images/salad.jpg',
   scaleMode: 'fill'
 }).appendTo(scrollView);
 
-var contentComposite = new tabris.Composite({
+let contentComposite = new Composite({
   left: 0, right: 0, top: '#titleComposite', height: 1000,
   background: 'white'
 }).appendTo(scrollView);
 
-new tabris.TextView({
-  left: MARGIN, right: MARGIN, top: MARGIN,
-  text: 'Make a dressing of the yolks of 3 hard-boiled eggs pounded fine, equal ' +
-    'quantities of mustard and paprika, a pinch of powdered sugar, 4 ' +
-    'tablespoonfuls of oil, 2 tablespoonfuls of vinegar. Simmer over the fire, ' +
-    'but do not allow to boil. Take the white meat of two chickens, and separate ' +
-    'into flakes; pile it in the middle of a dish, and pour the dressing over ' +
-    'it. Cut up two heads of lettuce, and arrange around the chicken. On top of ' +
-    'the lettuce place the whites of the eggs, cut into rings, and lay so as to ' +
-    'form a chain.'
+new TextView({
+  left: 16, right: 16, top: 16,
+  text: RECIPE
 }).appendTo(contentComposite);
 
-var titleComposite = new tabris.Composite({
+let titleComposite = new Composite({
   left: 0, right: 0, height: 78,
   id: 'titleComposite',
   background: rgba(255, 152, 0, INITIAL_TITLE_COMPOSITE_OPACITY)
 }).appendTo(scrollView);
 
-new tabris.TextView({
-  left: MARGIN, top: MARGIN, right: MARGIN,
-  markupEnabled: true,
-  text: '<b>The perfect side dish</b>',
-  font: '16px',
+new TextView({
+  left: 16, top: 16, right: 16,
+  text: SUBTITLE,
+  font: 'bold 16px',
   textColor: 'black'
 }).appendTo(titleComposite);
 
-new tabris.TextView({
-  left: MARGIN, bottom: MARGIN_SMALL, right: MARGIN, top: 'prev()',
-  markupEnabled: true,
-  text: '<b>INDIAN SUMMER SALAD</b>',
-  font: '24px',
+new TextView({
+  left: 16, bottom: 14, right: 16, top: 'prev()',
+  text: TITLE,
+  font: 'bold 24px',
   textColor: 'white'
 }).appendTo(titleComposite);
 
-scrollView.on('resize', function({height}) {
+scrollView.on('resize', ({height}) => {
   imageView.height  = height / 2;
-  var titleCompHeight = titleComposite.height;
+  let titleCompHeight = titleComposite.height;
   // We need the offset of the title composite in each scroll event.
   // As it can only change on resize, we assign it here.
   titleCompY = Math.min(imageView.height - titleCompHeight, height / 2);
   titleComposite.top = titleCompY;
 });
 
-scrollView.on('scrollY', function({offset}) {
+scrollView.on('scrollY', ({offset}) => {
   imageView.transform = {translationY: Math.max(0, offset * 0.4)};
   titleComposite.transform = {translationY: Math.max(0, offset - titleCompY)};
-  var opacity = calculateTitleCompositeOpacity(offset, titleCompY);
+  let opacity = calculateTitleCompositeOpacity(offset, titleCompY);
   titleComposite.background = rgba(255, 152, 0, opacity);
 });
 
 function calculateTitleCompositeOpacity(scrollViewOffsetY, titleCompY) {
-  var titleCompDistanceToTop = titleCompY - scrollViewOffsetY;
-  var opacity = 1 - (titleCompDistanceToTop * (1 - INITIAL_TITLE_COMPOSITE_OPACITY)) / titleCompY;
+  let titleCompDistanceToTop = titleCompY - scrollViewOffsetY;
+  let opacity = 1 - (titleCompDistanceToTop * (1 - INITIAL_TITLE_COMPOSITE_OPACITY)) / titleCompY;
   return opacity <= 1 ? opacity : 1;
 }
 
