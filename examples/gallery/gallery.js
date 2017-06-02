b6df03e6729a12a8c5abf2ece8452e44b4679012
Christian Petrov <cpetrov@eclipsesource.com>
2017-06-02 10:24:14 +0200

    Modernize gallery example
      - Destructure tabris import to make the code more compact and to avoid
        using the global tabris object.
      - Use let instead of const where variables are not "typical" constants
        (e.g. storing primitives). It may be a good practice to use const
        over let when possible, but it is also 2 characters longer and this
        practice may be an overmeasure in the scope of Tabris.js examples. Also
        be consistent with other examples.
      - Use image shorthand when possible. When only source is given
        (without other image properties), it can be given directly as a
        string.
    
    Change-Id: I3c42a9f0d260b723485e1ee43f2a7b8c491cb2e3

--- examples/gallery/gallery.js
+++ examples/gallery/gallery.js
@@ -1,7 +1,9 @@
+const {ImageView, ScrollView, ui} = require('tabris');
+
 const IMAGES = require('./images/index.json');
 const IMAGE_SIZE = 96;
 
-class FilmStrip extends tabris.ScrollView {
+class FilmStrip extends ScrollView {
 
   constructor(properties) {
     super(properties);
@@ -35,35 +37,35 @@ class FilmStrip extends tabris.ScrollView {
   }
 }
 
-tabris.ui.contentView.background = 'black';
-tabris.ui.statusBar.set({
+ui.contentView.background = 'black';
+ui.statusBar.set({
   displayMode: 'float',
   theme: 'dark',
   background: '#00000044'
 });
 
-const fullImage = new tabris.ImageView({
+let fullImage = new ImageView({
   top: 0, bottom: 0, left: 0, right: 0,
-  image: {src: `images/${IMAGES[0]}`},
+  image: `images/${IMAGES[0]}`,
   scaleMode: 'fill'
 }).on('tap', () => filmStrip.toggleShowing())
-  .appendTo(tabris.ui.contentView);
+  .appendTo(ui.contentView);
 
-const filmStrip = new FilmStrip({
+let filmStrip = new FilmStrip({
   left: 0, right: 0, bottom: 0, height: 112,
   direction: 'horizontal',
   background: '#00000044'
 }).on('scrollX', () => filmStrip.resetHideTimeout())
-  .appendTo(tabris.ui.contentView);
+  .appendTo(ui.contentView);
 
 IMAGES.forEach((image) => {
-  new tabris.ImageView({
+  new ImageView({
     top: 8, left: 'prev() 8', width: IMAGE_SIZE, height: IMAGE_SIZE,
     image: {src: `images/${image}`, width: IMAGE_SIZE, height: IMAGE_SIZE},
     scaleMode: 'fill',
     highlightOnTouch: true
   }).on('tap', () => {
-    fullImage.image = {src: `images/${image}`};
+    fullImage.image = `images/${image}`;
     filmStrip.resetHideTimeout();
   }).appendTo(filmStrip);
 });
