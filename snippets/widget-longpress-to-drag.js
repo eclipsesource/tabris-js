3bee35ca1f8eabc0d10ab87ea70b40ea205d0e9d
Christian Petrov <cpetrov@eclipsesource.com>
2017-06-06 18:01:32 +0200

    Introduce longpress to drag snippet
    Long pressing something to put it in draggable mode is a common pattern
    used in e.g. launcher and calendar applications.
    
    Change-Id: If83e3686d388fa63f0df6c473e2b81b869fcee8d

---
+++ snippets/widget-longpress-to-drag.js
@@ -0,0 +1,45 @@
+const {Composite, TextView, ui} = require('tabris');
+
+const LABEL_TEXT = 'Long press the box to drag it';
+
+let previousTouch;
+let drag;
+
+let composite = new Composite({centerX: 0, centerY: 0, width: 100, height: 100, background: 'red'})
+  .appendTo(ui.contentView)
+  .once('longpress', () => label.dispose())
+  .on('longpress', ({state}) => {
+    if (state === 'start') {
+      enableDrag();
+    }
+  })
+  .on('touchStart', ({touches: [touch]}) => previousTouch = touch)
+  .on('touchMove', ({target, touches: [touch]}) => {
+    if (!drag) {
+      return;
+    }
+    target.transform = {
+      translationX: touch.absoluteX - previousTouch.absoluteX + target.transform.translationX,
+      translationY: touch.absoluteY - previousTouch.absoluteY + target.transform.translationY
+    };
+    previousTouch = touch;
+  })
+  .on('touchEnd', () => {
+    previousTouch = null;
+    disableDrag();
+  });
+
+let label = new TextView({
+  centerX: 0, top: [composite, 8],
+  text: LABEL_TEXT
+}).appendTo(ui.contentView);
+
+function enableDrag() {
+  drag = true;
+  composite.background = 'green';
+}
+
+function disableDrag() {
+  drag = false;
+  composite.background = 'red';
+}
