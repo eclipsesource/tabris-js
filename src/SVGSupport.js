46ebd7d4dc991ee290f7c655f6904de1b3e7bf09
dalisoft <dalisoft@mail.ru>
2017-06-23 16:48:55 +0500

    fix(deps): removes dep requirement

--- src/SVGSupport.js
+++ src/SVGSupport.js
@@ -1,6 +1,5 @@
 import Canvas from './widgets/Canvas';
 //import dummyDoc from './dummyDoc';
-const canvg = require('canvg');
 
 const Path2D = Path2D || class Path2D {
   constructor() {
@@ -9,14 +8,6 @@ const Path2D = Path2D || class Path2D {
 };
 
 export default function(target) {
-  target.SVGElement = function ({svgId, width = 100, height = 100, top = 0, left = 0, right = 0, bottom = 0, params = {}
-    , children = []}) {
-    return new Canvas({top, left, width, height, right, bottom})
-      .on('resize', ({target: canvas, widthSVG, heightSVG}) => {
-        return canvg(canvas
-          , `<svg id='${svgId}' width='${widthSVG}' height='${heightSVG}'>${children.join('')}</svg>`, params);
-      });
-  };
   target.SVGPath = function ({d, width = 100, height = 100, top = 0, left = 0, right = 0, bottom = 0}) {
     if (typeof Path2D === 'undefined') {
       console.log('Warn: Path2D is not available');
