9f6b31629517a1f050a557ef37c1e15fde83a680
dalisoft <dalisoft@mail.ru>
2017-06-25 16:49:58 +0500

    * Support/Implement `Add` for virtual-dom-tree

--- src/VChange.js
+++ src/VChange.js
@@ -1,169 +1,167 @@
 // Compare object tree-table
 const _VDiff = {
-  _props: null,
-  _children: null,
-  _layoutData: null
+	_props: null,
+	_children: null,
+	_layoutData: null
 }
 
 // Compare string, object, number, boolean
-function VStringCompare( old, _new ) {
-  return JSON.stringify( old ) !== JSON.stringify( _new );
+function VStringCompare(old, _new) {
+	return JSON.stringify(old) !== JSON.stringify(_new);
 }
 
 // Get objet property as array
-function keys( tree ) {
-  return Object.keys( tree );
+function keys(tree) {
+	return typeof tree === "object" && Object.keys(tree);
 }
 
 // Compares tree and gets result
-function VCompare( old, tree ) {
-  let diff = {},
-    diffAct = {},
-	objP = keys(old).length > keys(tree) ? old : tree;
-  for ( let p in _VDiff ) {
-    if ( p === '_children' && ( old[ p ] || tree[ p ] ) ) {
-      diffAct[ p ] = [];
-      if ( old[ p ] === undefined )
-        old[ p ] = [];
-      if ( tree[ p ] === undefined )
-        tree[ p ] = [];
-      diff[ p ] = tree[ p ].map( ( comp, i ) => {
-        diffAct[ p ] = VAction( old[ p ][ i ], comp );
-        return VCompare( old[ p ][ i ], comp );
-      } );
-    } else if ( VStringCompare( old[ p ], tree[ p ] ) ) {
-      diff[ p ] = tree[ p ];
-      diffAct[ p ] = VAction( old[ p ], tree[ p ] );
-    }
-  }
-  return {
-    diff,
-    diffAct
-  };
+function VCompare(old, tree, own) {
+	let diff = {},
+	diffAct = {},
+	objP = keys(old).length > keys(tree).length ? old : tree,
+	iter = own ? objP : _VDiff;
+	for (let p in iter) {
+		if (old[p] === undefined && tree[p] !== undefined) {
+			diff[p] = tree[p];
+			diffAct[p] = VAction.ACTION_ADD;
+			continue;
+		} else if (old[p] !== undefined && tree[p] === undefined) {
+			diff[p] = old[p];
+			diffAct[p] = VAction.ACTION_REMOVE;
+		} else if (p === '_children' && (old[p] || tree[p])) {
+			diffAct[p] = [];
+			if (old[p] === undefined)
+				old[p] = [];
+			if (tree[p] === undefined) {
+				tree[p] = [];
+			}
+			let largetree = old[p].length > tree[p].length ? old[p] : tree[p];
+			diff[p] = largetree.map((comp, i) => {
+				if (tree[p][i] === undefined && old[p][i] !== undefined) {
+					if (typeof old[p][i].dispose === "function") {
+						old[p][i].dispose();
+					} else {
+						old[p].splice(i, 1);
+					}
+					return false;
+				} else if (tree[p][i] !== undefined && old[p][i] === undefined) {
+					diffAct[p][i] = VAction.ACTION_ADD;
+					return tree[p][i];
+				}
+					diffAct[p][i] = VAction(old[p][i], tree[p][i]);
+					return VCompare(old[p][i], tree[p][i]);
+				}).filter(p => p);
+		} else if (VStringCompare(old[p], tree[p])) {
+			diff[p] = tree[p];
+			diffAct[p] = VAction(old[p], tree[p]);
+		}
+	}
+	return {
+		diff,
+		diffAct
+	};
 }
 
 // Compare two object
-function VObjCompare( old, tree ) {
-  let diff = {},
-    diffAct = {},
-	objP = keys(old).length > keys(tree) ? old : tree;
-  for ( let p in objP ) {
-    if ( p === '_children' && ( old[ p ] || tree[ p ] ) ) {
-      diffAct[ p ] = [];
-      if ( old[ p ] === undefined )
-        old[ p ] = [];
-      if ( tree[ p ] === undefined )
-        tree[ p ] = [];
-      diff[ p ] = tree[ p ].map( ( comp, i ) => {
-        diffAct[ p ] = VAction( old[ p ][ i ], comp );
-        return VCompare( old[ p ][ i ], comp );
-      } );
-    } else if ( VStringCompare( old[ p ], tree[ p ] ) ) {
-      diff[ p ] = tree[ p ];
-      diffAct[ p ] = VAction( old[ p ], tree[ p ] );
-    }
-  }
-  return {
-    diff,
-    diffAct
-  };
+function VObjCompare(old, tree) {
+	return VCompare(old, tree, true);
 }
 
 // Action types
-[ 'STAY', 'ADD', 'REMOVE', 'CHANGE', 'NOTHING', 'UNDEF' ].map( type => {
-  VAction[ 'ACTION_' + type ] = type;
-} );
+['STAY', 'ADD', 'REMOVE', 'CHANGE', 'NOTHING', 'UNDEF'].map(type => {
+	VAction['ACTION_' + type] = type;
+});
 
 // Checks and gives action based on change
-function VAction( hasInOld, hasInTree ) {
-  return ( hasInOld !== undefined && hasInTree !== undefined && hasInOld === hasInTree ) ? VAction.ACTION_STAY : (
-    hasInOld !== undefined && hasInTree === undefined ) ? VAction.ACTION_REMOVE : ( hasInOld === undefined &&
-    hasInTree !== undefined ) ? VAction.ACTION_ADD : ( hasInOld !== undefined && hasInTree !== undefined && hasInOld !==
-    hasInTree ) ? VAction.ACTION_CHANGE : VAction.ACTION_NOTHING;
+function VAction(hasInOld, hasInTree) {
+	return (hasInOld !== undefined && hasInTree !== undefined && hasInOld === hasInTree) ? VAction.ACTION_STAY : (
+		hasInOld !== undefined && hasInTree === undefined) ? VAction.ACTION_REMOVE : (hasInOld === undefined &&
+		hasInTree !== undefined) ? VAction.ACTION_ADD : (hasInOld !== undefined && hasInTree !== undefined && hasInOld !==
+		hasInTree) ? VAction.ACTION_CHANGE : VAction.ACTION_NOTHING;
 }
 
 // Normalizes the comparision (best for Redux, Vuex)
-function VNorm( old, tree, man ) {
-  let VComp = man !== undefined ? man : VCompare( old, tree );
-  let {
-    diff,
-    diffAct
-  } = VComp;
-  let normalizedVTree = {};
-  for ( let p in diff ) {
-    if ( diffAct[ p ] ) {
-      normalizedVTree[ p ] = {
-        property: p,
-        action: diffAct[ p ],
-        value: diff[ p ]
-      };
-    } else {
-      normalizedVTree[ p ] = {
-        property: p,
-        action: VAction.ACTION_UNDEF,
-        value: diff[ p ]
-      }
-    }
-  }
-  return normalizedVTree;
+function VNorm(old, tree, man) {
+	let VComp = man !== undefined ? man : VCompare(old, tree);
+	let {
+		diff,
+		diffAct
+	} = VComp;
+	let normalizedVTree = {};
+	for (let p in diff) {
+		if (diffAct[p]) {
+			normalizedVTree[p] = {
+				property: p,
+				action: diffAct[p],
+				value: diff[p]
+			};
+		} else {
+			normalizedVTree[p] = {
+				property: p,
+				action: VAction.ACTION_UNDEF,
+				value: diff[p]
+			}
+		}
+	}
+	return normalizedVTree;
 }
 
 // Simple VDiff function
-function VDiff( old, tree, man ) {
-  let diff = VNorm( old, tree, man );
-  let key = keys( diff );
-  return key.length ? {
-      diff,
-      key
-    } :
-    null;
+function VDiff(old, tree, man) {
+	let diff = VNorm(old, tree, man);
+	let key = keys(diff);
+	return key.length ? {
+		diff,
+		key
+	}
+	 :
+	null;
 }
 
-function VError( err ) {
-  throw err;
+function VError(err) {
+	throw err;
 }
 
-function VPatch( set, old, tree, action, property, value ) {
-  if ( action === VAction.ACTION_CHANGE || action === VAction.ACTION_ADD ) {
-    set[ property ] = value;
-  } else if ( action === VAction.ACTION_REMOVE ) {
-    delete set[ property ];
-  }
-  return old;
+function VPatch(set, old, tree, action, property, value) {
+	if (action === VAction.ACTION_CHANGE || action === VAction.ACTION_ADD) {
+		set[property] = value;
+	} else if (action === VAction.ACTION_REMOVE) {
+		delete set[property];
+	}
+	return old;
 }
 
-function VChange( set, old, tree, man, diff ) {
-  let checkDiff = diff !== undefined ? diff : VDiff( old, tree, man );
-  if ( checkDiff === null ) {
-    return old;
-  }
+function VChange(set, old, tree, man, diff) {
+	let checkDiff = diff !== undefined ? diff : VDiff(old, tree, man);
+	if (checkDiff === null) {
+		return old;
+	}
 
-  checkDiff.key.map( prop => {
-    let {
-      action,
-      property,
-      value
-    } = checkDiff.diff[ prop ];
-	let ntree = tree[property];
-	let otree = old[property];
-    if ( property === "_children" && Array.isArray( value ) ) {
-	  let longMap = keys(otree).length > keys(ntree).length ? old[property] : ntree;
-      longMap.map( ( vChild, i ) => {
-		if (otree[ i ] && ntree[ i ] === undefined) {
-			otree[ i ].dispose(); // remove from old, because it removed
-			return false;
-		} else if (old[ property ][ i ] === undefined && ntree[ i ]) {
-			otree.push(ntree[ i ]);
+	checkDiff.key.map(prop => {
+		let {
+			action,
+			property,
+			value
+		} = checkDiff.diff[prop];
+		let ntree = tree[property];
+		let otree = old[property];
+		if (property === "_children" && Array.isArray(value)) {
+			let longMap = keys(otree).length > keys(ntree).length ? old[property] : ntree;
+			longMap.map((vChild, i) => {
+				if (otree[i] === undefined && ntree[i] !== undefined) {
+					set.append(ntree[i]);
+					return false;
+				}
+				VChange(otree[i], otree[i], ntree[i], value[i]);
+			});
+		} else if (typeof value === "object") {
+			VChange(set, otree, ntree, VObjCompare(otree, value));
+		} else {
+			VPatch(set, old, tree, action, property, value);
 		}
-        VChange( otree[ i ], otree[ i ], ntree[ i ], value[i] );
-      } );
-    } else if ( typeof value === "object" ) {
-      VChange( set, old[ property ], ntree, VObjCompare( otree, value ) );
-    } else {
-      VPatch( set, old, tree, action, property, value );
-    }
-  } );
-  return old;
+	});
+	return old;
 }
 
 export default VChange;
--- tabris.js
+++ tabris.js
@@ -4089,167 +4089,163 @@ var Styles = function(input) {
 
 // Compare object tree-table
 var _VDiff = {
-  _props: null,
-  _children: null,
-  _layoutData: null
+	_props: null,
+	_children: null,
+	_layoutData: null
 };
 
 // Compare string, object, number, boolean
-function VStringCompare( old, _new ) {
-  return JSON.stringify( old ) !== JSON.stringify( _new );
+function VStringCompare(old, _new) {
+	return JSON.stringify(old) !== JSON.stringify(_new);
 }
 
 // Get objet property as array
-function keys( tree ) {
-  return Object.keys( tree );
+function keys(tree) {
+	return typeof tree === "object" && Object.keys(tree);
 }
 
 // Compares tree and gets result
-function VCompare( old, tree ) {
-  var diff = {},
-    diffAct = {},
-	objP = keys(old).length > keys(tree) ? old : tree;
-  var loop = function ( p ) {
-    if ( p === '_children' && ( old[ p ] || tree[ p ] ) ) {
-      diffAct[ p ] = [];
-      if ( old[ p ] === undefined )
-        { old[ p ] = []; }
-      if ( tree[ p ] === undefined )
-        { tree[ p ] = []; }
-      diff[ p ] = tree[ p ].map( function ( comp, i ) {
-        diffAct[ p ] = VAction( old[ p ][ i ], comp );
-        return VCompare( old[ p ][ i ], comp );
-      } );
-    } else if ( VStringCompare( old[ p ], tree[ p ] ) ) {
-      diff[ p ] = tree[ p ];
-      diffAct[ p ] = VAction( old[ p ], tree[ p ] );
-    }
-  };
+function VCompare(old, tree, own) {
+	var diff = {},
+	diffAct = {},
+	objP = keys(old).length > keys(tree).length ? old : tree,
+	iter = own ? objP : _VDiff;
+	var loop = function ( p ) {
+		if (old[p] === undefined && tree[p] !== undefined) {
+			diff[p] = tree[p];
+			diffAct[p] = VAction.ACTION_ADD;
+			return;
+		} else if (old[p] !== undefined && tree[p] === undefined) {
+			diff[p] = old[p];
+			diffAct[p] = VAction.ACTION_REMOVE;
+		} else if (p === '_children' && (old[p] || tree[p])) {
+			diffAct[p] = [];
+			if (old[p] === undefined)
+				{ old[p] = []; }
+			if (tree[p] === undefined) {
+				tree[p] = [];
+			}
+			var largetree = old[p].length > tree[p].length ? old[p] : tree[p];
+			diff[p] = largetree.map(function (comp, i) {
+				if (tree[p][i] === undefined && old[p][i] !== undefined) {
+					if (typeof old[p][i].dispose === "function") {
+						old[p][i].dispose();
+					} else {
+						old[p].splice(i, 1);
+					}
+					return false;
+				} else if (tree[p][i] !== undefined && old[p][i] === undefined) {
+					diffAct[p][i] = VAction.ACTION_ADD;
+					return tree[p][i];
+				}
+					diffAct[p][i] = VAction(old[p][i], tree[p][i]);
+					return VCompare(old[p][i], tree[p][i]);
+				}).filter(function (p) { return p; });
+		} else if (VStringCompare(old[p], tree[p])) {
+			diff[p] = tree[p];
+			diffAct[p] = VAction(old[p], tree[p]);
+		}
+	};
 
-  for ( var p in _VDiff ) loop( p );
-  return {
-    diff: diff,
-    diffAct: diffAct
-  };
+	for (var p in iter) loop( p );
+	return {
+		diff: diff,
+		diffAct: diffAct
+	};
 }
 
 // Compare two object
-function VObjCompare( old, tree ) {
-  var diff = {},
-    diffAct = {},
-	objP = keys(old).length > keys(tree) ? old : tree;
-  var loop = function ( p ) {
-    if ( p === '_children' && ( old[ p ] || tree[ p ] ) ) {
-      diffAct[ p ] = [];
-      if ( old[ p ] === undefined )
-        { old[ p ] = []; }
-      if ( tree[ p ] === undefined )
-        { tree[ p ] = []; }
-      diff[ p ] = tree[ p ].map( function ( comp, i ) {
-        diffAct[ p ] = VAction( old[ p ][ i ], comp );
-        return VCompare( old[ p ][ i ], comp );
-      } );
-    } else if ( VStringCompare( old[ p ], tree[ p ] ) ) {
-      diff[ p ] = tree[ p ];
-      diffAct[ p ] = VAction( old[ p ], tree[ p ] );
-    }
-  };
-
-  for ( var p in objP ) loop( p );
-  return {
-    diff: diff,
-    diffAct: diffAct
-  };
+function VObjCompare(old, tree) {
+	return VCompare(old, tree, true);
 }
 
 // Action types
-[ 'STAY', 'ADD', 'REMOVE', 'CHANGE', 'NOTHING', 'UNDEF' ].map( function (type) {
-  VAction[ 'ACTION_' + type ] = type;
-} );
+['STAY', 'ADD', 'REMOVE', 'CHANGE', 'NOTHING', 'UNDEF'].map(function (type) {
+	VAction['ACTION_' + type] = type;
+});
 
 // Checks and gives action based on change
-function VAction( hasInOld, hasInTree ) {
-  return ( hasInOld !== undefined && hasInTree !== undefined && hasInOld === hasInTree ) ? VAction.ACTION_STAY : (
-    hasInOld !== undefined && hasInTree === undefined ) ? VAction.ACTION_REMOVE : ( hasInOld === undefined &&
-    hasInTree !== undefined ) ? VAction.ACTION_ADD : ( hasInOld !== undefined && hasInTree !== undefined && hasInOld !==
-    hasInTree ) ? VAction.ACTION_CHANGE : VAction.ACTION_NOTHING;
+function VAction(hasInOld, hasInTree) {
+	return (hasInOld !== undefined && hasInTree !== undefined && hasInOld === hasInTree) ? VAction.ACTION_STAY : (
+		hasInOld !== undefined && hasInTree === undefined) ? VAction.ACTION_REMOVE : (hasInOld === undefined &&
+		hasInTree !== undefined) ? VAction.ACTION_ADD : (hasInOld !== undefined && hasInTree !== undefined && hasInOld !==
+		hasInTree) ? VAction.ACTION_CHANGE : VAction.ACTION_NOTHING;
 }
 
 // Normalizes the comparision (best for Redux, Vuex)
-function VNorm( old, tree, man ) {
-  var VComp = man !== undefined ? man : VCompare( old, tree );
-  var diff = VComp.diff;
-  var diffAct = VComp.diffAct;
-  var normalizedVTree = {};
-  for ( var p in diff ) {
-    if ( diffAct[ p ] ) {
-      normalizedVTree[ p ] = {
-        property: p,
-        action: diffAct[ p ],
-        value: diff[ p ]
-      };
-    } else {
-      normalizedVTree[ p ] = {
-        property: p,
-        action: VAction.ACTION_UNDEF,
-        value: diff[ p ]
-      };
-    }
-  }
-  return normalizedVTree;
+function VNorm(old, tree, man) {
+	var VComp = man !== undefined ? man : VCompare(old, tree);
+	var diff = VComp.diff;
+	var diffAct = VComp.diffAct;
+	var normalizedVTree = {};
+	for (var p in diff) {
+		if (diffAct[p]) {
+			normalizedVTree[p] = {
+				property: p,
+				action: diffAct[p],
+				value: diff[p]
+			};
+		} else {
+			normalizedVTree[p] = {
+				property: p,
+				action: VAction.ACTION_UNDEF,
+				value: diff[p]
+			};
+		}
+	}
+	return normalizedVTree;
 }
 
 // Simple VDiff function
-function VDiff( old, tree, man ) {
-  var diff = VNorm( old, tree, man );
-  var key = keys( diff );
-  return key.length ? {
-      diff: diff,
-      key: key
-    } :
-    null;
+function VDiff(old, tree, man) {
+	var diff = VNorm(old, tree, man);
+	var key = keys(diff);
+	return key.length ? {
+		diff: diff,
+		key: key
+	}
+	 :
+	null;
 }
 
-function VPatch( set, old, tree, action, property, value ) {
-  if ( action === VAction.ACTION_CHANGE || action === VAction.ACTION_ADD ) {
-    set[ property ] = value;
-  } else if ( action === VAction.ACTION_REMOVE ) {
-    delete set[ property ];
-  }
-  return old;
+function VPatch(set, old, tree, action, property, value) {
+	if (action === VAction.ACTION_CHANGE || action === VAction.ACTION_ADD) {
+		set[property] = value;
+	} else if (action === VAction.ACTION_REMOVE) {
+		delete set[property];
+	}
+	return old;
 }
 
-function VChange( set, old, tree, man, diff ) {
-  var checkDiff = diff !== undefined ? diff : VDiff( old, tree, man );
-  if ( checkDiff === null ) {
-    return old;
-  }
+function VChange(set, old, tree, man, diff) {
+	var checkDiff = diff !== undefined ? diff : VDiff(old, tree, man);
+	if (checkDiff === null) {
+		return old;
+	}
 
-  checkDiff.key.map( function (prop) {
-    var ref = checkDiff.diff[ prop ];
-    var action = ref.action;
-    var property = ref.property;
-    var value = ref.value;
-	var ntree = tree[property];
-	var otree = old[property];
-    if ( property === "_children" && Array.isArray( value ) ) {
-	  var longMap = keys(otree).length > keys(ntree).length ? old[property] : ntree;
-      longMap.map( function ( vChild, i ) {
-		if (otree[ i ] && ntree[ i ] === undefined) {
-			otree[ i ].dispose(); // remove from old, because it removed
-			return false;
-		} else if (old[ property ][ i ] === undefined && ntree[ i ]) {
-			otree.push(ntree[ i ]);
+	checkDiff.key.map(function (prop) {
+		var ref = checkDiff.diff[prop];
+		var action = ref.action;
+		var property = ref.property;
+		var value = ref.value;
+		var ntree = tree[property];
+		var otree = old[property];
+		if (property === "_children" && Array.isArray(value)) {
+			var longMap = keys(otree).length > keys(ntree).length ? old[property] : ntree;
+			longMap.map(function (vChild, i) {
+				if (otree[i] === undefined && ntree[i] !== undefined) {
+					set.append(ntree[i]);
+					return false;
+				}
+				VChange(otree[i], otree[i], ntree[i], value[i]);
+			});
+		} else if (typeof value === "object") {
+			VChange(set, otree, ntree, VObjCompare(otree, value));
+		} else {
+			VPatch(set, old, tree, action, property, value);
 		}
-        VChange( otree[ i ], otree[ i ], ntree[ i ], value[i] );
-      } );
-    } else if ( typeof value === "object" ) {
-      VChange( set, old[ property ], ntree, VObjCompare( otree, value ) );
-    } else {
-      VPatch( set, old, tree, action, property, value );
-    }
-  } );
-  return old;
+	});
+	return old;
 }
 
 var Component = (function (Widget$$1) {
