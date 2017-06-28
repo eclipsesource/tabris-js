4fb1737fbb5e4c113fe9a3e4faf3b422ad9d2297
Christian Petrov <cpetrov@eclipsesource.com>
2017-06-28 10:19:43 +0200

    Modernize text page of Canvas example
    - Destructure tabris import to make the code more compact and to avoid
      using the global tabris object.
    - Use let instead of var.
    - Prefer lambda functions over anonymous functions.
    
    Change-Id: If320aa580bd595f4cf2caf0539c6d62476504b34

--- examples/canvas/text.js
+++ examples/canvas/text.js
@@ -1,23 +1,25 @@
-var page = module.exports = new tabris.Page({
+const {Canvas, Page} = require('tabris');
+
+let page = new Page({
   title: 'Text',
   autoDispose: false
 });
 
-var canvas = new tabris.Canvas({
+let canvas = new Canvas({
   left: 10, top: 10, right: 10, bottom: 10
 }).appendTo(page);
 
-var ctx = canvas.getContext('2d', 300, 300);
+let ctx = canvas.getContext('2d', 300, 300);
 ctx.strokeStyle = 'red';
 
-var x = 50;
-var y = 50;
+let x = 50;
+let y = 50;
 
 ctx.moveTo(5, y);
 ctx.lineTo(295, y);
 ctx.stroke();
 ctx.textAlign = 'center';
-['top', 'bottom', 'middle'].forEach(function(mode) {
+['top', 'bottom', 'middle'].forEach((mode) => {
   ctx.textBaseline = mode;
   ctx.fillText(mode, x, y);
   x += 100;
@@ -29,7 +31,7 @@ y = 100;
 ctx.moveTo(5, y);
 ctx.lineTo(295, y);
 ctx.stroke();
-['hanging', 'alphabetic', 'ideographic'].forEach(function(mode) {
+['hanging', 'alphabetic', 'ideographic'].forEach((mode) => {
   ctx.textBaseline = mode;
   ctx.fillText(mode, x, y);
   x += 100;
@@ -42,8 +44,10 @@ ctx.moveTo(x, 150);
 ctx.lineTo(x, 270);
 ctx.stroke();
 ctx.textBaseline = 'middle';
-['start', 'end', 'left', 'right', 'center'].forEach(function(mode) {
+['start', 'end', 'left', 'right', 'center'].forEach((mode) => {
   ctx.textAlign = mode;
   ctx.fillText(mode, x, y);
   y += 20;
 });
+
+module.exports = page;
