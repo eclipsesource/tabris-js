2acb37048fa6b437e3e9506345f6fadc4d124dc3
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-06-28 11:33:18 +0200

    Make version check more strict
    As of Tabris 2.0, the native client's version must match the version
    of the tabris module. This allows us to evolve the protocol during
    the release stream. Patch versions should not be ignored as they
    contain only bug fixes and don't affect the protocol.
    
    The CLI will download the native client and so ensure compatible
    versions.
    
    With this change, major and minor segments must match, so it's now
    sufficient to compare the segments as strings.
    
    Change-Id: I6e7a71dd045816c6b8f1f5402f822e3c1fdd972e

--- src/tabris/version.js
+++ src/tabris/version.js
@@ -3,23 +3,10 @@ export function checkVersion(tabrisVersionString, clientVersionString) {
   if (!clientVersionString) {
     return;
   }
-  let tabrisVersion = tabrisVersionString.split('.').map(toInt);
-  let clientVersion = clientVersionString.split('.').map(toInt);
-  if (tabrisVersion[0] !== clientVersion[0]) {
-    console.error(createVersionMessage(clientVersion, tabrisVersion, 'incompatible with'));
-  } else if (tabrisVersion[1] > clientVersion[1]) {
-    console.warn(createVersionMessage(clientVersion, tabrisVersion, 'newer than'));
+  let tabrisVersion = tabrisVersionString.split('.');
+  let clientVersion = clientVersionString.split('.');
+  if (tabrisVersion[0] !== clientVersion[0] || tabrisVersion[1] !== clientVersion[1]) {
+    console.error(`Version mismatch: JavaScript module "tabris" (version ${tabrisVersionString}) ` +
+    `is incompatible with the native tabris platform (version ${clientVersionString}).`);
   }
-}
-
-function toInt(string) {
-  return parseInt(string);
-}
-
-function createVersionMessage(clientVersion, tabrisVersion, versionComp) {
-  let from = clientVersion[0] + '.0';
-  let to = clientVersion[0] + '.' + clientVersion[1];
-  return `Version mismatch: JavaScript module "tabris" (version ${tabrisVersion.join('.')}) ` +
-         `is ${versionComp} the native tabris platform. ` +
-         `Supported module versions: ${from} to ${to}.`;
 }
--- test/tabris/version.test.js
+++ test/tabris/version.test.js
@@ -5,7 +5,6 @@ describe('version check', function() {
 
   beforeEach(function() {
     stub(console, 'error');
-    stub(console, 'warn');
   });
 
   afterEach(restore);
@@ -13,63 +12,34 @@ describe('version check', function() {
   it('accepts same versions', function() {
     checkVersion('1.2.3', '1.2.3');
 
-    expect(console.warn).not.to.have.been.called;
     expect(console.error).not.to.have.been.called;
   });
 
-  it('ignores smaller tabris patch version', function() {
+  it('ignores mismatching patch version', function() {
     checkVersion('1.2.2', '1.2.3');
-
-    expect(console.warn).not.to.have.been.called;
-    expect(console.error).not.to.have.been.called;
-  });
-
-  it('ignores greater tabris patch version', function() {
     checkVersion('1.2.4', '1.2.3');
 
-    expect(console.warn).not.to.have.been.called;
     expect(console.error).not.to.have.been.called;
   });
 
-  it('ignores smaller tabris minor version', function() {
-    checkVersion('1.1.0', '1.2.3');
+  it('ignores build identifier', function() {
+    checkVersion('1.2.3-dev.20170627+1116', '1.2.3');
 
-    expect(console.warn).not.to.have.been.called;
     expect(console.error).not.to.have.been.called;
   });
 
-  it('compares numerically, not lexicographically', function() {
-    checkVersion('1.9.0', '1.10.0');
+  it('raises error for mismatching minor version', function() {
+    checkVersion('1.1.3', '1.2.3');
 
-    expect(console.warn).not.to.have.been.called;
-    expect(console.error).not.to.have.been.called;
-  });
-
-  it('raises warning for greater tabris minor version', function() {
-    checkVersion('1.3.0', '1.2.3');
-
-    expect(console.warn).to.have.been.calledWith('Version mismatch: JavaScript module "tabris" ' +
-      '(version 1.3.0) is newer than the native tabris platform. ' +
-      'Supported module versions: 1.0 to 1.2.');
-    expect(console.error).not.to.have.been.called;
-  });
-
-  it('raises error for smaller tabris major version', function() {
-    checkVersion('1.2.3', '2.2.3');
-
-    expect(console.warn).not.to.have.been.called;
     expect(console.error).to.have.been.calledWith('Version mismatch: JavaScript module "tabris" ' +
-      '(version 1.2.3) is incompatible with the native tabris platform. ' +
-      'Supported module versions: 2.0 to 2.2.');
+      '(version 1.1.3) is incompatible with the native tabris platform (version 1.2.3).');
   });
 
-  it('raises error for greater tabris major version', function() {
-    checkVersion('2.1.0', '1.2.3');
+  it('raises error for mismatching major version', function() {
+    checkVersion('2.2.3', '1.2.3');
 
-    expect(console.warn).not.to.have.been.called;
     expect(console.error).to.have.been.calledWith('Version mismatch: JavaScript module "tabris" ' +
-      '(version 2.1.0) is incompatible with the native tabris platform. ' +
-      'Supported module versions: 1.0 to 1.2.');
+      '(version 2.2.3) is incompatible with the native tabris platform (version 1.2.3).');
   });
 
 });
