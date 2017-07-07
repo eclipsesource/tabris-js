d9f9fc1b7ff6e6648c6c051ee414e7a48cc6c1d6
David Buschtöns <dbuschtoens@eclipsesource.com>
2017-07-07 16:55:31 +0200

    Change composite snippet to avoid windows client issue
    Change layout of the composite2 to be percentage based rather than
    relative to composite1. See issue #1418
    
    Change-Id: I5e3f189252c4b5585eb7b96dbd0a762df47a6d5e

--- snippets/composite.js
+++ snippets/composite.js
@@ -14,7 +14,7 @@ new TextView({
 }).appendTo(composite1);
 
 let composite2 = new Composite({
-  left: [composite1, 0], top: 0, bottom: 0, right: 0,
+  left: '50%', top: 0, bottom: 0, right: 0,
   background: '#eaeaea'
 }).appendTo(ui.contentView);
 
