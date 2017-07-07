088a6638680e41eb89519345fd724365f24f926e
David Buschtöns <dbuschtoens@eclipsesource.com>
2017-07-07 18:21:22 +0200

    Remove circular layout from statusbar snippet
    The windows client doesn't support circular dependencies in layouts.
    
    Change-Id: I300ee595be191b22ddc5026129d8b7cc6de90a71

--- snippets/statusbar.js
+++ snippets/statusbar.js
@@ -4,37 +4,37 @@ const THEMES = ['default', 'light', 'dark'];
 const DISPLAY_MODES = ['default', 'float', 'hide'];
 const BACKGROUNDS = [ui.statusBar.background, 'rgba(0, 0, 0, 0.25)', 'red', 'green', 'blue'];
 
-createTextView('Theme');
+createTextView('Theme', 'theme');
+createTextView('Display mode', 'displayMode');
+createTextView('Background', 'background');
+createTextView('Height', 'height');
 
 new Picker({
-  left: '#displayMode 16', baseline: 'prev()', right: 16,
+  left: '#displayMode 16', baseline: '#theme', right: 16,
   itemCount: THEMES.length,
   itemText: index => THEMES[index]
 }).on('select', ({index}) => ui.statusBar.theme = THEMES[index])
   .appendTo(ui.contentView);
 
-createTextView('Display mode', 'displayMode');
-
 new Picker({
-  left: '#displayMode 16', baseline: 'prev()', right: 16,
+
+  left: '#displayMode 16', baseline: '#displayMode', right: 16,
   itemCount: DISPLAY_MODES.length,
   itemText: index => DISPLAY_MODES[index]
 }).on('select', ({index}) => ui.statusBar.displayMode = DISPLAY_MODES[index])
   .appendTo(ui.contentView);
 
-createTextView('Background');
 
 new Picker({
-  left: '#displayMode 16', baseline: 'prev()', right: 16,
+  left: '#displayMode 16', baseline: '#background', right: 16,
   itemCount: BACKGROUNDS.length,
   itemText: index => BACKGROUNDS[index]
 }).on('select', ({index}) => ui.statusBar.background = BACKGROUNDS[index])
   .appendTo(ui.contentView);
 
-createTextView('Height');
 
 new TextView({
-  left: '#displayMode 16', baseline: 'prev()', right: 16,
+  left: '#displayMode 16', baseline: '#height', right: 16,
   text: ui.statusBar.height
 }).appendTo(ui.contentView);
 
