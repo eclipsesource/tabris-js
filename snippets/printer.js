2adf0e1efb47d291c8a0a03653b92c8da7e92b5a
Moritz Post <mpost@eclipsesource.com>
2018-03-14 12:02:00 +0100

    Add support for printing images
    The changes adds a new options value `contentType` which has to be set
    to `image/*` when printing an image.

--- snippets/printer.js
+++ snippets/printer.js
@@ -5,10 +5,19 @@ const {Button, printer, ui, app} = require('tabris');
 new Button({
   left: 16, right: 16, top: 16,
   text: 'Print PDF'
-}).on('select', () => {
-  fetch(app.getResourceLocation('resources/example.pdf'))
+}).on('select', () => print('resources/example.pdf', 'application/pdf', 'Example PDF'))
+  .appendTo(ui.contentView);
+
+new Button({
+  left: 16, right: 16, top: 'prev() 16',
+  text: 'Print Image'
+}).on('select', () => print('resources/salad.jpg', 'image/jpg', 'Salad image'))
+  .appendTo(ui.contentView);
+
+function print(file, contentType, jobName) {
+  fetch(app.getResourceLocation(file))
     .then(res => res.arrayBuffer())
-    .then(data => printer.print(data, {jobName: 'tabris print example'}))
+    .then(data => printer.print(data, {jobName: jobName, contentType: contentType}))
     .then(event => console.log('Printing finished', event))
     .catch(err => console.error(err));
-}).appendTo(ui.contentView);
+}
