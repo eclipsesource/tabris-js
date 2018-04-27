8bff1a504ea00f5b95c06b9cb9aba3b1e7cee874
Tim Buschtoens <tbuschto@eclipsesource.com>
2018-04-27 14:18:32 +0200

    Extract select function from WidgetCollection in to separate module
    The selector will become more complex soon and would inflate the
    WidgetCollection module.
    
    Change-Id: I4a8d21573377bb263d7907a3248c727e6c800186

--- src/tabris/WidgetCollection.js
+++ src/tabris/WidgetCollection.js
@@ -1,4 +1,4 @@
-import NativeObject from './NativeObject';
+import {select} from './util-widget-select';
 
 export default class WidgetCollection {
 
@@ -125,80 +125,4 @@ export default class WidgetCollection {
     };
   }
 
-}
-
-function select(array, selector, deep) {
-  if (!array || array.length === 0) {
-    return [];
-  }
-  if (selector === '*' && !deep) {
-    return array.concat();
-  }
-  let filter = getFilter(selector);
-  if (deep) {
-    return deepSelect([], array, filter);
-  }
-  return array.filter(filter);
-}
-
-function deepSelect(result, iterable, filter) {
-  for (let widget of iterable) {
-    if (filter(widget)) {
-      result.push(widget);
-    }
-    let children = widget.children();
-    if (children instanceof WidgetCollection && children.length) {
-      deepSelect(result, children, filter);
-    }
-  }
-  return result;
-}
-
-function getFilter(selector) {
-  let matches = {};
-  let filter = isFilter(selector) ? selector : createMatcher(selector);
-  return (widget) => {
-    if (matches[widget.cid]) {
-      return false;
-    }
-    if (filter(widget)) {
-      matches[widget.cid] = true;
-      return true;
-    }
-    return false;
-  };
-}
-
-function createMatcher(selector) {
-  if (selector instanceof Function) {
-    return widget => widget instanceof selector;
-  }
-  if (selector.charAt(0) === '#') {
-    let expectedId = selector.slice(1);
-    return widget => expectedId === widget.id;
-  }
-  if (selector.charAt(0) === '.') {
-    let expectedClass = selector.slice(1);
-    return widget => widget.classList.indexOf(expectedClass) !== -1;
-  }
-  if (selector === '*') {
-    return () => true;
-  }
-  return widget => selector === widget.constructor.name;
-}
-
-function isFilter(selector) {
-  return selector instanceof Function && !isWidgetConstructor(selector);
-}
-
-function isWidgetConstructor(fn) {
-  let proto = fn.prototype;
-  while (proto) {
-    // Use NativeObject since importing Widget would causes circulary module dependency issues
-    if (proto === NativeObject.prototype) {
-      return true;
-    }
-    proto = Object.getPrototypeOf(proto);
-  }
-  return false;
 }
