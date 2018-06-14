28c972e4c132e0e05f82297f872f9277131cdc48
Moritz Post <mpost@eclipsesource.com>
2018-06-14 11:06:14 +0200

    Show more properties in device snippet

--- snippets/device.js
+++ snippets/device.js
@@ -2,13 +2,15 @@ const {TextView, device, ui} = require('tabris');
 
 // Display available device information
 
-['platform', 'version', 'model', 'vendor', 'name', 'language', 'orientation'].forEach((property) => {
-  new TextView({
-    id: property,
-    left: 10, right: 10, top: 'prev() 10',
-    text: property + ': ' + device[property]
-  }).appendTo(ui.contentView);
-});
+['platform', 'version', 'model', 'vendor', 'name', 'language',
+  'orientation', 'screenWidth', 'screenHeight', 'scaleFactor']
+  .forEach((property) => {
+    new TextView({
+      id: property,
+      left: 10, right: 10, top: 'prev() 10',
+      text: property + ': ' + device[property]
+    }).appendTo(ui.contentView);
+  });
 
 device.on('orientationChanged', ({value: orientation}) => {
   ui.contentView.find('#orientation').set('text', 'orientation: ' + orientation);
