3ab39d48955986cc5471e4fcc1efc5279d243d28
Tim Buschtoens <tbuschto@eclipsesource.com>
2018-11-02 18:06:33 +0100

    Add Console TypeScript test
    Change-Id: I11de472da11026cceadbbc7fc208288781d71f05

---
+++ test/typescript/console.test.ts
@@ -0,0 +1,14 @@
+console.log(1, 'foo', true, {});
+console.debug(1, 'foo', true, {});
+console.info(1, 'foo', true, {});
+console.warn(1, 'foo', true, {});
+console.error(1, 'foo', true, {});
+console.group(1, 'foo', true, {});
+console.groupEnd();
+console.assert(true, 'foo', true, {});
+console.assert(false, 'foo', true, {});
+console.count();
+console.count('foo');
+console.countReset();
+console.trace();
+console.dirxml({});
