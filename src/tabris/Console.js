8625429d8c2dc105270957d9c8ce0395cce4084b
Tim Buschtoens <tbuschto@eclipsesource.com>
2018-11-06 11:17:18 +0100

    Extract stack trace code from Console.js to util-stacktrace
    Change-Id: Id449d8f5ab80b09d2b61a60468ed39c843115da1

--- src/tabris/Console.js
+++ src/tabris/Console.js
@@ -1,4 +1,5 @@
 import {format} from './Formatter';
+import {getStackTrace} from './util-stacktrace';
 
 export default class Console {
 
@@ -9,10 +10,7 @@ export default class Console {
   }
 
   trace() {
-    let stack = new Error().stack.split('\n');
-    stack = stack.filter(filterStackLine);
-    stack = stack.map(normalizeStackLine);
-    this.log(stack.join('\n'));
+    this.log(getStackTrace(new Error));
   }
 
   assert(expression, ...args) {
@@ -116,39 +114,3 @@ export const log = function(...args) { defaultConsole.log(...args); };
 export const warn = function(...args) { defaultConsole.warn(...args); };
 export const error = function(...args) { defaultConsole.error(...args); };
 
-const androidStackLineRegex = /^ +at +(.+) +\((.*):([0-9]+):([0-9]+)\)/;
-const androidStackLineNoNameRegex = /^ +at +(.*):([0-9]+):([0-9]+)/;
-const iosStackLineRegex = /^(.+)@(.*):([0-9]+):([0-9]+)/;
-const iosStackLineNoNameRegex = /(.*):([0-9]+):([0-9]+)/;
-const urlBaseRegEx = /^[a-z]+:\/\/[^/]+\//;
-
-function filterStackLine(line) {
-  if (tabris.device.platform === 'android' && !androidStackLineNoNameRegex.test(line)) {
-    return false;
-  }
-  return line.indexOf('tabris/tabris.min.js:') === -1 && line.indexOf('@[native code]') === -1;
-}
-
-function normalizeStackLine(line) {
-  const regex = tabris.device.platform === 'android' ? androidStackLineRegex : iosStackLineRegex;
-  const noNameRegex = tabris.device.platform === 'android' ? androidStackLineNoNameRegex : iosStackLineNoNameRegex;
-  const fullMatch = line.match(regex);
-  const noNameMatch = line.match(noNameRegex);
-  if (fullMatch && fullMatch.length === 5) {
-    let [, fn, url, line, column] = fullMatch;
-    return `${fn.split('.').pop()} (${fixUrl(url)}:${line}:${column})`;
-  } else if (noNameMatch && noNameMatch.length === 4) {
-    let [, url, line, column] = noNameMatch;
-    return `${fixUrl(url)}:${line}:${column}`;
-  } else {
-    return line;
-  }
-}
-
-function fixUrl(url) {
-  const urlBase = url.match(urlBaseRegEx);
-  if (urlBase) {
-    return './' + url.slice(urlBase[0].length);
-  }
-  return url;
-}
---
+++ src/tabris/util-stacktrace.js
@@ -0,0 +1,43 @@
+const androidStackLineRegex = /^ +at +(.+) +\((.*):([0-9]+):([0-9]+)\)/;
+const androidStackLineNoNameRegex = /^ +at +(.*):([0-9]+):([0-9]+)/;
+const iosStackLineRegex = /^(.+)@(.*):([0-9]+):([0-9]+)/;
+const iosStackLineNoNameRegex = /(.*):([0-9]+):([0-9]+)/;
+const urlBaseRegEx = /^[a-z]+:\/\/[^/]+\//;
+
+export function getStackTrace(error) {
+  let stack = error.stack.split('\n');
+  stack = stack.filter(filterStackLine);
+  stack = stack.map(normalizeStackLine);
+  return stack.join('\n');
+}
+
+function filterStackLine(line) {
+  if (tabris.device.platform === 'android' && !androidStackLineNoNameRegex.test(line)) {
+    return false;
+  }
+  return line.indexOf('tabris/tabris.min.js:') === -1 && line.indexOf('@[native code]') === -1;
+}
+
+function normalizeStackLine(line) {
+  const regex = tabris.device.platform === 'android' ? androidStackLineRegex : iosStackLineRegex;
+  const noNameRegex = tabris.device.platform === 'android' ? androidStackLineNoNameRegex : iosStackLineNoNameRegex;
+  const fullMatch = line.match(regex);
+  const noNameMatch = line.match(noNameRegex);
+  if (fullMatch && fullMatch.length === 5) {
+    let [, fn, url, line, column] = fullMatch;
+    return `${fn.split('.').pop()} (${fixUrl(url)}:${line}:${column})`;
+  } else if (noNameMatch && noNameMatch.length === 4) {
+    let [, url, line, column] = noNameMatch;
+    return `${fixUrl(url)}:${line}:${column}`;
+  } else {
+    return line;
+  }
+}
+
+function fixUrl(url) {
+  const urlBase = url.match(urlBaseRegEx);
+  if (urlBase) {
+    return './' + url.slice(urlBase[0].length);
+  }
+  return url;
+}
