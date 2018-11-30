766325157ed4f13d4ea936d2601d2c5e4c6ffc02
Tim Buschtoens <tbuschto@eclipsesource.com>
2018-11-30 16:10:27 +0100

    Fix snippet datedialog.js
    Change-Id: I75bf362ca69da766a12be6bfad2da7a0fafaf38f

--- snippets/datedialog.js
+++ snippets/datedialog.js
@@ -14,12 +14,13 @@ const selectionTextView = new TextView({
 }).appendTo(ui.contentView);
 
 function showDateDialog() {
+  const date = new Date();
   new DateDialog({
-    date: new Date(),
+    date,
     minDate: new Date(date.getTime() - FIVE_DAYS),
     maxDate: new Date(date.getTime() + FIVE_DAYS)
   }).on({
-    select: ({date}) => selectionTextView.text = date,
+    select: ev => selectionTextView.text = ev.date,
     close: () => console.log('DateDialog closed')
   }).open();
 }
