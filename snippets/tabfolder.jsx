0848b2622df3a50dcff2f687bc660ef1b5989221
Tim Buschtoens <tbuschto@eclipsesource.com>
2019-03-15 15:08:55 +0100

    Modernize TabFolder snippet
    * Use JSX
    * Repeat code instead of extracting to a function
    * Do not log events
    * Rename used images to include image scale
    
    Change-Id: I7f9ad81f6fbc6642aee1e9a8edcddc04e6ba1934

---
+++ snippets/tabfolder.jsx
@@ -0,0 +1,15 @@
+import {Tab, TabFolder, TextView, contentView} from 'tabris';
+
+contentView.append(
+  <TabFolder paging layoutData='fill'>
+    <Tab title='Cart' image='resources/cart@2x.png' selectedImage='resources/cart-filled@2x.png'>
+      <TextView centerX centerY>Content of Tab Cart</TextView>
+    </Tab>
+    <Tab title='Pay' image='resources/card@2x.png' selectedImage='resources/card-filled@2x.png'>
+      <TextView centerX centerY>Content of Tab Cart</TextView>
+    </Tab>
+    <Tab title='Statistic' image='resources/chart@2x.png' selectedImage='resources/chart-filled@2x.png'>
+      <TextView centerX centerY>Content of Tab Cart</TextView>
+    </Tab>
+  </TabFolder>
+);
