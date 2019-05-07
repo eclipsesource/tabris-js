e21ea2b2a6314d7b1c20e3ac50aca8a039920964
Moritz Post <mpost@eclipsesource.com>
2019-05-07 10:45:20 +0200

    Correct tab content in tabfolder.jsx snippet
    Change-Id: I472fa102f35a221d205944cef41cca2b8b574d64

--- snippets/tabfolder.jsx
+++ snippets/tabfolder.jsx
@@ -3,13 +3,13 @@ import {contentView, Tab, TabFolder, TextView} from 'tabris';
 contentView.append(
   <TabFolder paging stretch selectionIndex={1} tabBarLocation='bottom'>
     <Tab title='Cart' image='resources/cart@2x.png' selectedImage='resources/cart-filled@2x.png'>
-      <TextView centerX centerY>Content of Tab Cart</TextView>
+      <TextView center>Content of Tab Cart</TextView>
     </Tab>
     <Tab title='Pay' image='resources/card@2x.png' selectedImage='resources/card-filled@2x.png' badge={5}>
-      <TextView centerX centerY>Content of Tab Cart</TextView>
+      <TextView center>Content of Tab Pay</TextView>
     </Tab>
     <Tab title='Statistic' image='resources/chart@2x.png' selectedImage='resources/chart-filled@2x.png'>
-      <TextView centerX centerY>Content of Tab Cart</TextView>
+      <TextView center>Content of Tab Statistic</TextView>
     </Tab>
   </TabFolder>
 );
