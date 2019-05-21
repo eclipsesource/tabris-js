d619b65f16c990b1a508741df3988f18b58a2f3b
Tim Buschtoens <tbuschto@eclipsesource.com>
2019-05-21 11:16:30 +0200

    Adjust console snippet for better stack trace on iOS
    Unfortunately iOS stack traces skip arrow functions, which is why the
    cleaned-up stack trace was empty in this snippet. (The trace function
    falls back to the original stack trace.)
    
    Add a intermediate named function to the function so we get a clean
    trace on iOS.
    
    Change-Id: I86db8a64cc4bb7bfa2c2587a701d251608ca91c5

--- snippets/console.jsx
+++ snippets/console.jsx
@@ -8,7 +8,7 @@ contentView.append(
     <Button text='info' onSelect={() => console.info(input.text)}/>
     <Button text='warn' onSelect={() => console.warn(input.text)}/>
     <Button text='error' onSelect={() => console.error(input.text)}/>
-    <Button text='trace' onSelect={() => console.trace()}/>
+    <Button text='trace' onSelect={printTrace}/>
     <TextView top={16} font='18px'/>
   </Stack>
 );
@@ -19,3 +19,8 @@ const output = $(TextView).only();
 tabris.onLog(ev =>
   output.text = ev.level + ': ' + ev.message
 );
+
+function printTrace() {
+  // This line should appear in the trace:
+  console.trace();
+}
