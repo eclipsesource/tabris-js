eclipsesource/tabris-js

commit e0bb314bf3cc8ddb6971fe3dcd1e13ab9a88efbb

Fix logging of contentView causing an exception
The contentView object is proxified to better support unit testing. The
proxy uses a fake target object and re-directs any access to itself to
the current contentView. In some cases this causes exceptions because
the VM checks if the fake target behaves the same way the proxy does.

Change-Id: I160f70d876936f4b667cda487af23f06542f550a

src/tabris/util.js
@@ -132,11 +132,17 @@ const traps = {
  */
 export function proxify(getTarget) {
   const handler = {};
+  const fakeTarget = {};
   Object.keys(traps).forEach(trap => handler[trap] = (_, ...args) => {
     const result = Reflect[trap](getTarget(), ...args);
+    if (trap === 'getOwnPropertyDescriptor') {
+      // The VM throws if a property is configurable or non-existing
+      // on fakeTarget, but not reported as such here
+      return Object.assign({}, result, {configurable: true});
+    }
     return traps[trap] ? true : result;
   });
-  return new Proxy({}, handler);
+  return new Proxy(fakeTarget, handler);
 }
 
 export function isReadable(value) {

test/tabris/util.test.js
@@ -9,6 +9,9 @@ import {
   checkNumber,
   proxify
 } from '../../src/tabris/util';
+import Composite from '../../src/tabris/widgets/Composite';
+import {mockTabris} from '../tabris-mock';
+import ClientMock from './ClientMock';
 
 describe('util', function() {
 
@@ -224,6 +227,13 @@ describe('util', function() {
       expect(proxy).to.deep.equal(org);
       expect(proxy).not.to.deep.equal(backup);
       expect(proxy instanceof Test).to.be.false;
+    });
+
+    it('Does not crash Object.keys', function() {
+      mockTabris(new ClientMock());
+      const original = new Composite({left: 23});
+      proxy = proxify(() => original);
+      expect(() => Object.keys(proxy)).not.to.throw(Error);
     });
 
   });

snippets/format.js
@@ -19,5 +19,6 @@ log({foo: 1, bar: 2, baz: 3});
 log('Formats other stuff...');
 log('Dates: ', new Date());
 log('Functions:', () => 23);
-log('Widgets:', contentView.children().children());
+log('Widgets:', contentView);
+log('WidgetCollection:', contentView.children().children());
 log('Errors:', new TypeError('error message'));

commit f6483111485dd4bf3330125b85a35ccb93f5c0bc

Fix proxified service classes not lazy-initializing event interface
This issue was introduced by e0bb314bf3cc8ddb6971fe3dcd1e13ab9a88efbb.
The root cause was that the Object.assign method used in that commit did
not actually copy the given object as expected. The fix is to modify the
original, as it is a safe copy already.

Change-Id: Iabf5a439358f4bcdf3551937d43b3646c2c8b29d

src/tabris/util.js
@@ -135,10 +135,11 @@ export function proxify(getTarget) {
   const fakeTarget = {};
   Object.keys(traps).forEach(trap => handler[trap] = (_, ...args) => {
     const result = Reflect[trap](getTarget(), ...args);
-    if (trap === 'getOwnPropertyDescriptor') {
+    if (trap === 'getOwnPropertyDescriptor' && result) {
       // The VM throws if a property is configurable or non-existing
       // on fakeTarget, but not reported as such here
-      return Object.assign({}, result, {configurable: true});
+      result.configurable = true;
+      return result;
     }
     return traps[trap] ? true : result;
   });

test/tabris/util.test.js
@@ -1,4 +1,4 @@
-import {expect} from '../test';
+import {expect, spy} from '../test';
 import {
   pick,
   omit,
@@ -12,6 +12,7 @@ import {
 import Composite from '../../src/tabris/widgets/Composite';
 import {mockTabris} from '../tabris-mock';
 import ClientMock from './ClientMock';
+import NativeObject from '../../src/tabris/NativeObject';
 
 describe('util', function() {
 
@@ -229,11 +230,27 @@ describe('util', function() {
       expect(proxy instanceof Test).to.be.false;
     });
 
-    it('Does not crash Object.keys', function() {
+    it('does not crash Object.keys', function() {
       mockTabris(new ClientMock());
       const original = new Composite({left: 23});
       proxy = proxify(() => original);
       expect(() => Object.keys(proxy)).not.to.throw(Error);
+    });
+
+    it('does support lazy initialized event API', function() {
+      mockTabris(new ClientMock());
+      const TestClass = class extends NativeObject {
+        get _nativeType() { return 'bar'; }
+      };
+      NativeObject.defineEvent(TestClass.prototype, 'foo', {});
+      const instance = new TestClass();
+      const testProxy = proxify(() => instance);
+      const listener = spy();
+
+      testProxy.onFoo(listener);
+      testProxy.onFoo.trigger();
+
+      expect(listener).to.have.been.calledOnce;
     });
 
   });