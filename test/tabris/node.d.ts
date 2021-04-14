27310e609ce76efdb1a94b9552caaf1cf30604a7
Tim Buschtoens <tbuschto@eclipsesource.com>
2021-04-14 15:59:10 +0200

    Fix unit tests picking up build output
    The tsconfig.json used for all unit tests auto-included all type
    declarations in the project, including those in "build". The only way
    to prevent that is to very explicitly only include type declarations
    actually needed.
    
    Change-Id: Iae0ba78aa26f5958bea85d3756ee80a3479e4675

---
+++ test/tabris/node.d.ts
@@ -0,0 +1,5 @@
+// Including the entire node type declarations in the tests
+// can cause conflicts. Declare only the few actually used
+// global variables
+
+declare function setTimeout(fn: Function, time?: number): any;
